c3db52d85affa7edb984422339112a19bd0781b0
Burcu Dogan <jbd@google.com>
2014-06-01 18:36:09 +0200

    Extend test coverage for proto to entity conversions.

--- test/datastore.entity.js
+++ test/datastore.entity.js
@@ -145,3 +145,69 @@ describe('isKeyComplete', function() {
   });
 
 });
+
+describe('entityFromEntityProto', function() {
+
+  it('should support boolean, integer, double, string, entity and list values', function(done) {
+    var obj = entity.entityFromEntityProto(entityProto);
+    assert.strictEqual(obj.createdAt.getTime(), new Date('2001-01-01').getTime());
+    assert.strictEqual(obj.linkedTo.ns, undefined);
+    assert.strictEqual(obj.linkedTo.kind, 'Kind');
+    assert.strictEqual(obj.linkedTo.id, '4790047639339008');
+    assert.strictEqual(obj.name, 'Name');
+    assert.strictEqual(obj.flagged, true);
+    assert.strictEqual(obj.count, 5);
+    assert.strictEqual(obj.total, 5.42);
+    assert.strictEqual(obj.author.name, 'Burcu Dogan');
+    assert.strictEqual(obj.list[0], 6);
+    assert.strictEqual(obj.list[1], false);
+    done();
+  });
+
+});
+
+var keyProto = {
+  "partitionId":{
+     "datasetId":"s~bamboo-shift-xxx",
+     "namespace":"default"
+  },
+  "path":[
+     {
+        "kind":"Kind",
+        "id":"4790047639339008"
+     }
+  ]
+};
+
+var entityProto = {
+   "properties":{
+      "createdAt":{
+         "dateTimeValue":"2001-01-01T00:00:00.000Z"
+      },
+      "linkedTo": {
+         "keyValue": keyProto
+      },
+      "name":{
+         "stringValue":"Name"
+      },
+      "flagged":{
+         "booleanValue":true
+      },
+      "count":{
+         "integerValue":"5"
+      },
+      "total":{
+         "doubleValue": "5.42"
+      },
+      "author": {
+         "entityValue": {
+             "properties": {
+                "name": { "stringValue": "Burcu Dogan" }
+             }
+         }
+      },
+      "list": {
+         "listValue": [{ "integerValue": "6" }, { "booleanValue": false }]
+      }
+   }
+};
