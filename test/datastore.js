423e5ec9d1a09150d3f61817dcedc7ab34a001e2
Burcu Dogan <jbd@google.com>
2014-06-01 18:57:33 +0200

    Fixing dataset ID prefix prepending.

--- test/datastore.js
+++ test/datastore.js
@@ -6,5 +6,13 @@ describe('Keys', function() {
 });
 
 describe('Dataset', function() {
-
+  it('should append ~s if ~s or ~e are not presented', function(done) {
+    var dsWithNoPrefix = new datastore.Dataset({ projectId: 'test' });
+    var dsWithSPrefix = new datastore.Dataset({ projectId: 's~name' });
+    var dsWithEPrefix = new datastore.Dataset({ projectId: 'e~name' });
+    assert.equal(dsWithNoPrefix.id, 's~test');
+    assert.equal(dsWithSPrefix.id, 's~name');
+    assert.equal(dsWithEPrefix.id, 'e~name');
+    done();
+  });
 });
