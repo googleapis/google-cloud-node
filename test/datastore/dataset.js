c977820c412874af8049e1e75f58844643b6c5f6
Burcu Dogan <jbd@google.com>
2014-08-06 14:37:18 -0700

    datastore: Key partition should only include namespace and should be optional.

--- test/datastore/dataset.js
+++ test/datastore/dataset.js
@@ -24,16 +24,6 @@ var mockRespGet = require('../testdata/response_get.json');
 var Transaction = require('../../lib/datastore/transaction.js');
 
 describe('Dataset', function() {
-  it('should append ~s if ~s or ~e are not presented', function(done) {
-    var dsWithNoPrefix = new datastore.Dataset({ projectId: 'test' });
-    var dsWithSPrefix = new datastore.Dataset({ projectId: 's~name' });
-    var dsWithEPrefix = new datastore.Dataset({ projectId: 'e~name' });
-    assert.equal(dsWithNoPrefix.id, 's~test');
-    assert.equal(dsWithSPrefix.id, 's~name');
-    assert.equal(dsWithEPrefix.id, 'e~name');
-    done();
-  });
-
   it('should get by key', function(done) {
     var ds = new datastore.Dataset({ projectId: 'test' });
     ds.transaction.makeReq = function(method, proto, callback) {
@@ -126,9 +116,6 @@ describe('Dataset', function() {
       assert.equal(method, 'allocateIds');
       assert.equal(proto.keys.length, 1);
       assert.deepEqual(proto.keys[0], {
-        partitionId:{
-          datasetId: 's~test',
-        },
         path :[{kind:'Kind'}]
       });
       callback(null, {
