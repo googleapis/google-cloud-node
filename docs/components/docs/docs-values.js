9917742e143da88a8e6dd3e142a63e75a5dda88e
Stephen Sawchuk <sawchuk@gmail.com>
2014-10-09 20:10:43 -0400

    docs: re-structure versioning.

--- docs/components/docs/docs-values.js
+++ docs/components/docs/docs-values.js
@@ -1,5 +1,20 @@
 angular.module('gcloud.docs')
   .value('pages', {
+
+    //---------------------------------------------
+    // Link schema:
+    //---------------------------------------------
+    // key: {
+    //   title: 'Display Name for Link',
+    //   _url: '{baseUrl}/module-name',
+    //
+    //   pages: [
+    //     title: 'Display Name for Sub-Page Link',
+    //     url: '/path-relevant-to-parent-url'
+    //   ]
+    // }
+    //---------------------------------------------
+
     gcloud: {
       title: 'gcloud',
       _url: '{baseUrl}'
@@ -62,8 +77,20 @@ angular.module('gcloud.docs')
     VERSIONS: {
       // Give a version with/without a comparator, anything semver:
       //   https://github.com/npm/node-semver#versions
-      // List should be in ascending order.
-      '<=0.7.1': ['gcloud', 'datastore', 'storage'],
-      '>0.7.1': ['gcloud', 'datastoreWithTransaction', 'pubsub', 'storage']
+      //
+      // Multiple keys may be used to match a version.
+      //
+      // Example:
+      // A user is browsing the docs for 0.7.0. If the list below contains the
+      // keys: "*", ">0.4.0", and "0.7.0", all of the contents will be joined.
+      //
+      // Notes on ordering:
+      // These are sorted alphabetically by `module`.title.
+      //
+      // To keep the documentation for the main module, `gcloud`, on top of the
+      // link list, **make sure the title is `gcloud`**
+      '*': ['gcloud', 'storage'],
+      '<0.8.0': ['datastore'],
+      '>=0.8.0': ['datastoreWithTransaction', 'pubsub']
     }
   });
