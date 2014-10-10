d35f86a5945dbbfc5b3f52759e86186a028447a5
Stephen Sawchuk <sawchuk@gmail.com>
2014-10-09 20:10:43 -0400

    docs: re-structure versioning.

--- docs/components/docs/docs-services.js
+++ docs/components/docs/docs-services.js
@@ -1,24 +1,33 @@
 angular.module('gcloud.docs')
-  .factory('getLinks', function(pages) {
+  .factory('getLinks', function(versions, pages) {
     'use strict';
 
+    // `version` is the current version being browsed.
     return function(version) {
       var baseUrl = '#/docs/' + version;
       var VERSIONS = pages.VERSIONS;
-      var versions;
-      var match;
-      if (!version || version === 'master') {
-        versions = Object.keys(VERSIONS);
-        match = versions[versions.length - 1];
-      } else {
-        match = Object.keys(VERSIONS).filter(semver.satisfies.bind(null, version))[0];
+
+      if (version === 'master') {
+        // Use the most recent release.
+        version = versions[0];
       }
-      return VERSIONS[match]
-        .map(function(module) {
-          if (pages[module]._url) {
-            pages[module].url = pages[module]._url.replace('{baseUrl}', baseUrl);
+
+      // Use semver matching to put all matching modules together.
+      return Object.keys(VERSIONS)
+        .reduce(function(acc, potentialVersion) {
+          if (semver.satisfies(version, potentialVersion)) {
+            acc = acc.concat(VERSIONS[potentialVersion].map(function(module) {
+              if (pages[module]._url) {
+                pages[module].url = pages[module]._url.replace('{baseUrl}', baseUrl);
+              }
+              return pages[module];
+            }));
           }
-          return pages[module];
+          return acc;
+        }, [])
+        .sort(function(moduleA, moduleB) {
+          // A title matching `gcloud` will come first in the list.
+          return moduleA.title === 'gcloud' ? -1 : moduleA.title > moduleB.title;
         });
     };
   });
