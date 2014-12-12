7b963bf17f0da8dbad1d64585d52c083c38380f5
Stephen Sawchuk <sawchuk@gmail.com>
2014-12-12 14:38:13 -0500

    datastore: commit transactions after queueing modifcations

--- test/datastore/request.js
+++ test/datastore/request.js
@@ -149,6 +149,43 @@ describe('Request', function() {
       request.save({ key: key, data: {} }, done);
     });
 
+    it('should set the ID on incomplete key objects', function(done) {
+      var key = new entity.Key({ namespace: 'ns', path: ['Company'] });
+      var id = 50714372;
+
+      var mockCommitResponse = {
+        mutation_result: {
+          insert_auto_id_key: [
+            {
+              partition_id: {
+                dataset_id: 's~project-id',
+                namespace: 'ns'
+              },
+              path_element: [
+                {
+                  kind: 'Company',
+                  id: id,
+                  name: null
+                }
+              ]
+            }
+          ]
+        }
+      };
+
+      request.makeReq_ = function(method, req, callback) {
+        callback(null, mockCommitResponse);
+      };
+
+      request.save({ key: key, data: {} }, function(err) {
+        assert.ifError(err);
+
+        assert.equal(key.path[1], id);
+
+        done();
+      });
+    });
+
     it('should save with keys', function(done) {
       request.makeReq_ = function(method, req, callback) {
         assert.equal(method, 'commit');
@@ -164,61 +201,50 @@ describe('Request', function() {
       ], done);
     });
 
+    it('should not set an indexed value by default', function(done) {
+      request.makeReq_ = function(method, req) {
+        var property = req.mutation.upsert[0].property[0];
+        assert.equal(property.name, 'name');
+        assert.equal(property.value.string_value, 'value');
+        assert.strictEqual(property.value.indexed, undefined);
+        done();
+      };
+      request.save({
+        key: key,
+        data: [{ name: 'name', value: 'value' }]
+      }, assert.ifError);
+    });
+
+    it('should allow setting the indexed value of property', function(done) {
+      request.makeReq_ = function(method, req) {
+        var property = req.mutation.upsert[0].property[0];
+        assert.equal(property.name, 'name');
+        assert.equal(property.value.string_value, 'value');
+        assert.strictEqual(property.value.indexed, false);
+        done();
+      };
+      request.save({
+        key: key,
+        data: [{ name: 'name', value: 'value', excludeFromIndexes: true }]
+      }, assert.ifError);
+    });
+
     describe('transactions', function() {
       beforeEach(function() {
         // Trigger transaction mode.
         request.id = 'transaction-id';
+        request.requestCallbacks_ = [];
+        request.requests_ = [];
       });
 
-      it('should mark transaction as finalized', function(done) {
-        assert.strictEqual(request.isFinalized, undefined);
-        request.makeReq_ = function(method, req, callback) {
-          callback(null, { mutation_result: {} });
-        };
-        request.save({ key: key, data: {} }, function(err) {
-          assert.ifError(err);
-          assert.strictEqual(request.isFinalized, true);
-          done();
-        });
-      });
-
-      it('should not mark as finalized if an error occurred', function(done) {
-        assert.strictEqual(request.isFinalized, undefined);
-        request.makeReq_ = function(method, req, callback) {
-          callback(new Error('Error.'));
-        };
-        request.save({ key: key, data: {} }, function() {
-          assert.strictEqual(request.isFinalized, undefined);
-          done();
-        });
-      });
-
-      it('should not set an indexed value by default', function(done) {
-        request.makeReq_ = function(method, req) {
-          var property = req.mutation.upsert[0].property[0];
-          assert.equal(property.name, 'name');
-          assert.equal(property.value.string_value, 'value');
-          assert.strictEqual(property.value.indexed, undefined);
-          done();
-        };
+      it('should queue request & callback', function() {
         request.save({
           key: key,
           data: [{ name: 'name', value: 'value' }]
-        }, assert.ifError);
-      });
+        });
 
-      it('should allow setting the indexed value of property', function(done) {
-        request.makeReq_ = function(method, req) {
-          var property = req.mutation.upsert[0].property[0];
-          assert.equal(property.name, 'name');
-          assert.equal(property.value.string_value, 'value');
-          assert.strictEqual(property.value.indexed, false);
-          done();
-        };
-        request.save({
-          key: key,
-          data: [{ name: 'name', value: 'value', excludeFromIndexes: true }]
-        }, assert.ifError);
+        assert.equal(typeof request.requestCallbacks_[0], 'function');
+        assert.equal(typeof request.requests_[0], 'object');
       });
     });
   });
@@ -246,29 +272,13 @@ describe('Request', function() {
       beforeEach(function() {
         // Trigger transaction mode.
         request.id = 'transaction-id';
+        request.requests_ = [];
       });
 
-      it('should mark transaction as finalized', function(done) {
-        assert.strictEqual(request.isFinalized, undefined);
-        request.makeReq_ = function(method, req, callback) {
-          callback(null, { mutation_result: {} });
-        };
-        request.delete(key, function(err) {
-          assert.ifError(err);
-          assert.strictEqual(request.isFinalized, true);
-          done();
-        });
-      });
+      it('should queue request', function() {
+        request.delete(key);
 
-      it('should not mark as finalized if an error occurred', function(done) {
-        assert.strictEqual(request.isFinalized, undefined);
-        request.makeReq_ = function(method, req, callback) {
-          callback(new Error('Error.'));
-        };
-        request.delete(key, function() {
-          assert.strictEqual(request.isFinalized, undefined);
-          done();
-        });
+        assert.equal(typeof request.requests_[0].mutation.delete, 'object');
       });
     });
   });
