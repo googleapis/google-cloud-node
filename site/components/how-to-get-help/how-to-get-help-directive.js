9926167fe660eab7d6a69439d5e8d501f99b8511
travis-ci <travis@travis-ci.org>
2015-05-12 23:05:25 +0000

    Update docs after merge to master

---
+++ site/components/how-to-get-help/how-to-get-help-directive.js
@@ -0,0 +1,9 @@
+angular
+  .module('gcloud.how-to-get-help', [])
+  .directive('howToGetHelp', function() {
+    'use strict';
+
+    return {
+      templateUrl: 'site/components/how-to-get-help/how-to-get-help.html',
+    };
+  });
