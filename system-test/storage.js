9d9863e0d9c98c65c31a10e276c78c8f734d2e96
Stephen Sawchuk <sawchuk@gmail.com>
2015-09-09 11:55:10 -0400

    storage: support simple uploads

--- system-test/storage.js
+++ system-test/storage.js
@@ -593,18 +593,23 @@ describe('storage', function() {
       it('should write without chunked transfer-encoding', function(done) {
         var file = bucket.file('LargeFile');
 
-        fs.stat(files.big.path, function(err, metadata) {
+        fs.stat(files.big.path, function(err, fd) {
+          var metadata = {
+            contentLength: fd.size,
+            contentType: 'image/png'
+          };
+
           var ws = file.createWriteStream({
-            metadata: {
-              contentLength: metadata.size
-            }
+            simple: true,
+            metadata: metadata
           });
 
           fs.createReadStream(files.big.path)
             .pipe(ws)
             .on('error', done)
             .on('finish', function() {
-              assert.equal(file.metadata.size, metadata.size);
+              assert.equal(file.metadata.size, metadata.contentLength);
+              assert.equal(file.metadata.contentType, metadata.contentType);
               file.delete(done);
             });
         });
