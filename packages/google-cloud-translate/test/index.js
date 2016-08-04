8dcc140bdc37493512d18c4d2566843fda330a27
Stephen Sawchuk <sawchuk@gmail.com>
2016-08-04 15:21:59 -0400

    modular code style

--- packages/google-cloud-translate/test/index.js
+++ packages/google-cloud-translate/test/index.js
@@ -20,8 +20,8 @@ var assert = require('assert');
 var extend = require('extend');
 var prop = require('propprop');
 var proxyquire = require('proxyquire');
-
 var util = require('@google-cloud/common').util;
+
 var PKG = require('../package.json');
 
 var makeRequestOverride;
