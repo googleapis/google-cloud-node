6f622c53f30eaf782b4a761b29a4c17b85e0269f
Jun Mukai <mukai@google.com>
2016-08-16 13:55:44 -0700

    Revise generated code.
    - Move API service class to a toplevel object
    - index.js creates a auth context (through GAX) and apply it to
      the generated code. This way, the same context will be shared
      among multiple instances if an API consists of multiple service
      classes.

--- packages/language/src/v1beta1/language_service_api.js
+++ packages/language/src/v1beta1/language_service_api.js
@@ -31,10 +31,8 @@ var arguejs = require('arguejs');
 var configData = require('./language_service_client_config');
 var gax = require('google-gax');
 
-/** The default address of the service. */
 var SERVICE_ADDRESS = 'language.googleapis.com';
 
-/** The default port of the service. */
 var DEFAULT_SERVICE_PORT = 443;
 
 var CODE_GEN_NAME_VERSION = 'gapic/0.1.0';
@@ -49,201 +47,195 @@ var ALL_SCOPES = [
   'https://www.googleapis.com/auth/cloud-platform'
 ];
 
-module.exports = function build(options) {
-  options = options || {};
-  if (!('scopes' in options)) {
-    options.scopes = ALL_SCOPES;
-  }
-  var gaxGrpc = gax.grpc(options);
+/**
+ * Provides text analysis operations such as sentiment analysis and entity
+ * recognition.
+ *
+ * @class
+ * @param {Object=} opts - The optional parameters.
+ * @param {String=} opts.servicePath
+ *   The domain name of the API remote host.
+ * @param {number=} opts.port
+ *   The port on which to connect to the remote host.
+ * @param {grpc.ClientCredentials=} opts.sslCreds
+ *   A ClientCredentials for use with an SSL-enabled channel.
+ * @param {Object=} opts.clientConfig
+ *   The customized config to build the call settings. See
+ *   {@link gax.constructSettings} for the format.
+ * @param {number=} opts.timeout
+ *   The default timeout, in seconds, for calls made through this client.
+ * @param {number=} opts.appName
+ *   The codename of the calling service.
+ * @param {String=} opts.appVersion
+ *   The version of the calling service.
+ */
+function LanguageServiceApi(gaxGrpc, grpcClient, opts) {
+  opts = opts || {};
+  var servicePath = opts.servicePath || SERVICE_ADDRESS;
+  var port = opts.port || DEFAULT_SERVICE_PORT;
+  var sslCreds = opts.sslCreds || null;
+  var clientConfig = opts.clientConfig || {};
+  var timeout = opts.timeout || DEFAULT_TIMEOUT;
+  var appName = opts.appName || 'gax';
+  var appVersion = opts.appVersion || gax.Version;
 
+
+  var googleApiClient = [
+    appName + '/' + appVersion,
+    CODE_GEN_NAME_VERSION,
+    'nodejs/' + process.version].join(' ');
+
+  var defaults = gaxGrpc.constructSettings(
+      'google.cloud.language.v1beta1.LanguageService',
+      configData,
+      clientConfig,
+      timeout,
+      null,
+      null,
+      {'x-goog-api-client': googleApiClient});
+
+  var stub = gaxGrpc.createStub(
+      servicePath,
+      port,
+      grpcClient.google.cloud.language.v1beta1.LanguageService,
+      {sslCreds: sslCreds});
+  var methods = [
+    'analyzeSentiment',
+    'analyzeEntities',
+    'annotateText'
+  ];
+  methods.forEach(function(methodName) {
+    this['_' + methodName] = gax.createApiCall(
+        stub.then(function(stub) { return stub[methodName].bind(stub); }),
+        defaults[methodName]);
+  }.bind(this));
+}
+
+// Callback types
+
+/**
+ * @callback APICallback
+ * @param {?Error} error - the error object if something goes wrong.
+ *   Null if API succeeds.
+ * @param {?T} response
+ *   The response object when API succeeds.
+ * @template T
+ */
+
+/**
+ * @callback EmptyCallback
+ * @param {?Error} error - the error object if something goes wrong.
+ *   Null if API succeeds.
+ */
+
+// Service calls
+
+/**
+ * Analyzes the sentiment of the provided text.
+ *
+ * @param {google.cloud.language.v1beta1.Document} document
+ *   Input document. Currently, `analyzeSentiment` only supports English text
+ *   ({@link Document.language}="EN").
+ * @param {gax.CallOptions=} options
+ *   Overrides the default settings for this call, e.g, timeout,
+ *   retries, etc.
+ * @param {APICallback<google.cloud.language.v1beta1.AnalyzeSentimentResponse>=} callback
+ *   The function which will be called with the result of the API call.
+ * @returns {gax.EventEmitter} - the event emitter to handle the call
+ *   status.
+ * @throws an error if the RPC is aborted.
+ */
+LanguageServiceApi.prototype.analyzeSentiment = function analyzeSentiment() {
+  var args = arguejs({
+    document: Object,
+    options: [gax.CallOptions],
+    callback: [Function]
+  }, arguments);
+  var req = {
+    document: args.document
+  };
+  return this._analyzeSentiment(req, args.options, args.callback);
+};
+
+/**
+ * Finds named entities (currently finds proper names) in the text,
+ * entity types, salience, mentions for each entity, and other properties.
+ *
+ * @param {google.cloud.language.v1beta1.Document} document
+ *   Input document.
+ * @param {google.cloud.language.v1beta1.EncodingType} encodingType
+ *   The encoding type used by the API to calculate offsets.
+ * @param {gax.CallOptions=} options
+ *   Overrides the default settings for this call, e.g, timeout,
+ *   retries, etc.
+ * @param {APICallback<google.cloud.language.v1beta1.AnalyzeEntitiesResponse>=} callback
+ *   The function which will be called with the result of the API call.
+ * @returns {gax.EventEmitter} - the event emitter to handle the call
+ *   status.
+ * @throws an error if the RPC is aborted.
+ */
+LanguageServiceApi.prototype.analyzeEntities = function analyzeEntities() {
+  var args = arguejs({
+    document: Object,
+    encodingType: Number,
+    options: [gax.CallOptions],
+    callback: [Function]
+  }, arguments);
+  var req = {
+    document: args.document,
+    encoding_type: args.encodingType
+  };
+  return this._analyzeEntities(req, args.options, args.callback);
+};
+
+/**
+ * Advanced API that analyzes the document and provides a full set of text
+ * annotations, including semantic, syntactic, and sentiment information. This
+ * API is intended for users who are familiar with machine learning and need
+ * in-depth text features to build upon.
+ *
+ * @param {google.cloud.language.v1beta1.Document} document
+ *   Input document.
+ * @param {google.cloud.language.v1beta1.AnnotateTextRequest.Features} features
+ *   The enabled features.
+ * @param {google.cloud.language.v1beta1.EncodingType} encodingType
+ *   The encoding type used by the API to calculate offsets.
+ * @param {gax.CallOptions=} options
+ *   Overrides the default settings for this call, e.g, timeout,
+ *   retries, etc.
+ * @param {APICallback<google.cloud.language.v1beta1.AnnotateTextResponse>=} callback
+ *   The function which will be called with the result of the API call.
+ * @returns {gax.EventEmitter} - the event emitter to handle the call
+ *   status.
+ * @throws an error if the RPC is aborted.
+ */
+LanguageServiceApi.prototype.annotateText = function annotateText() {
+  var args = arguejs({
+    document: Object,
+    features: Object,
+    encodingType: Number,
+    options: [gax.CallOptions],
+    callback: [Function]
+  }, arguments);
+  var req = {
+    document: args.document,
+    features: args.features,
+    encoding_type: args.encodingType
+  };
+  return this._annotateText(req, args.options, args.callback);
+};
+
+module.exports = function build(gaxGrpc) {
   var grpcClient = gaxGrpc.load([{
     root: require('google-proto-files')('..'),
     file: 'google/cloud/language/v1beta1/language_service.proto'
   }]);
   var built = grpcClient.google.cloud.language.v1beta1;
-  /**
-   * Provides text analysis operations such as sentiment analysis and entity
-   * recognition.
-   *
-   * @class
-   * @param {?Object} opts - The optional parameters.
-   * @param {String} opts.servicePath
-   *   The domain name of the API remote host.
-   * @param {number} opts.port
-   *   The port on which to connect to the remote host.
-   * @param {grpc.ClientCredentials} opts.sslCreds
-   *   A ClientCredentials for use with an SSL-enabled channel.
-   * @param {Object} opts.clientConfig
-   *   The customized config to build the call settings. See
-   *   {@link gax.constructSettings} for the format.
-   * @param {number} opts.timeout
-   *   The default timeout, in seconds, for calls made through this client.
-   * @param {number} opts.appName
-   *   The codename of the calling service.
-   * @param {String} opts.appVersion
-   *   The version of the calling service.
-   */
-  function LanguageServiceApi(opts) {
-    if (!(this instanceof LanguageServiceApi)) {
-      return new LanguageServiceApi(opts);
-    }
-    opts = opts || {};
-    var servicePath = opts.servicePath || SERVICE_ADDRESS;
-    var port = opts.port || DEFAULT_SERVICE_PORT;
-    var sslCreds = opts.sslCreds || null;
-    var clientConfig = opts.clientConfig || {};
-    var timeout = opts.timeout || DEFAULT_TIMEOUT;
-    var appName = opts.appName || 'gax';
-    var appVersion = opts.appVersion || gax.Version;
 
-
-    var googleApiClient = [
-      appName + '/' + appVersion,
-      CODE_GEN_NAME_VERSION,
-      'nodejs/' + process.version].join(' ');
-
-    var defaults = gaxGrpc.constructSettings(
-        'google.cloud.language.v1beta1.LanguageService',
-        configData,
-        clientConfig,
-        timeout,
-        null,
-        null,
-        {'x-goog-api-client': googleApiClient});
-
-    var stub = gaxGrpc.createStub(
-        servicePath,
-        port,
-        grpcClient.google.cloud.language.v1beta1.LanguageService,
-        {sslCreds: sslCreds});
-    var methods = [
-      'analyzeSentiment',
-      'analyzeEntities',
-      'annotateText'
-    ];
-    methods.forEach(function(methodName) {
-      this['_' + methodName] = gax.createApiCall(
-          stub.then(function(stub) { return stub[methodName].bind(stub); }),
-          defaults[methodName]);
-    }.bind(this));
-  }
-  built.LanguageServiceApi = LanguageServiceApi;
-
-  // Callback types
-
-  /**
-   * @callback APICallback
-   * @param {?Error} error - the error object if something goes wrong.
-   *   Null if API succeeds.
-   * @param {?T} response
-   *   The response object when API succeeds.
-   * @template T
-   */
-
-  /**
-   * @callback EmptyCallback
-   * @param {?Error} error - the error object if something goes wrong.
-   *   Null if API succeeds.
-   */
-
-  // Service calls
-
-  /**
-   * Analyzes the sentiment of the provided text.
-   *
-   * @param {google.cloud.language.v1beta1.Document} document
-   *   Input document. Currently, `analyzeSentiment` only supports English text
-   *   ({@link Document.language}="EN").
-   * @param {?gax.CallOptions} options
-   *   Overrides the default settings for this call, e.g, timeout,
-   *   retries, etc.
-   * @param {?APICallback<google.cloud.language.v1beta1.AnalyzeSentimentResponse>} callback
-   *   The function which will be called with the result of the API call.
-   * @returns {gax.EventEmitter} - the event emitter to handle the call
-   *   status.
-   * @throws an error if the RPC is aborted.
-   */
-  LanguageServiceApi.prototype.analyzeSentiment = function analyzeSentiment() {
-    var args = arguejs({
-      document: Object,
-      options: [gax.CallOptions],
-      callback: [Function]
-    }, arguments);
-    var req = {
-      document: args.document
-    };
-    return this._analyzeSentiment(req, args.options, args.callback);
-  };
-
-  /**
-   * Finds named entities (currently finds proper names) in the text,
-   * entity types, salience, mentions for each entity, and other properties.
-   *
-   * @param {google.cloud.language.v1beta1.Document} document
-   *   Input document.
-   * @param {google.cloud.language.v1beta1.EncodingType} encodingType
-   *   The encoding type used by the API to calculate offsets.
-   * @param {?gax.CallOptions} options
-   *   Overrides the default settings for this call, e.g, timeout,
-   *   retries, etc.
-   * @param {?APICallback<google.cloud.language.v1beta1.AnalyzeEntitiesResponse>} callback
-   *   The function which will be called with the result of the API call.
-   * @returns {gax.EventEmitter} - the event emitter to handle the call
-   *   status.
-   * @throws an error if the RPC is aborted.
-   */
-  LanguageServiceApi.prototype.analyzeEntities = function analyzeEntities() {
-    var args = arguejs({
-      document: Object,
-      encodingType: Number,
-      options: [gax.CallOptions],
-      callback: [Function]
-    }, arguments);
-    var req = {
-      document: args.document,
-      encoding_type: args.encodingType
-    };
-    return this._analyzeEntities(req, args.options, args.callback);
-  };
-
-  /**
-   * Advanced API that analyzes the document and provides a full set of text
-   * annotations, including semantic, syntactic, and sentiment information. This
-   * API is intended for users who are familiar with machine learning and need
-   * in-depth text features to build upon.
-   *
-   * @param {google.cloud.language.v1beta1.Document} document
-   *   Input document.
-   * @param {google.cloud.language.v1beta1.AnnotateTextRequest.Features} features
-   *   The enabled features.
-   * @param {google.cloud.language.v1beta1.EncodingType} encodingType
-   *   The encoding type used by the API to calculate offsets.
-   * @param {?gax.CallOptions} options
-   *   Overrides the default settings for this call, e.g, timeout,
-   *   retries, etc.
-   * @param {?APICallback<google.cloud.language.v1beta1.AnnotateTextResponse>} callback
-   *   The function which will be called with the result of the API call.
-   * @returns {gax.EventEmitter} - the event emitter to handle the call
-   *   status.
-   * @throws an error if the RPC is aborted.
-   */
-  LanguageServiceApi.prototype.annotateText = function annotateText() {
-    var args = arguejs({
-      document: Object,
-      features: Object,
-      encodingType: Number,
-      options: [gax.CallOptions],
-      callback: [Function]
-    }, arguments);
-    var req = {
-      document: args.document,
-      features: args.features,
-      encoding_type: args.encodingType
-    };
-    return this._annotateText(req, args.options, args.callback);
+  built.languageServiceApi = function(opts) {
+    return new LanguageServiceApi(gaxGrpc, grpcClient, opts);
   };
   return built;
-};
-
+}
 module.exports.SERVICE_ADDRESS = SERVICE_ADDRESS;
 module.exports.ALL_SCOPES = ALL_SCOPES;
