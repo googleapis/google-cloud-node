552055eafd998f7a015cd6382e54ed2e1ea699c3
Jun Mukai <jun.mukai@gmail.com>
2016-09-26 15:33:40 -0700

    Introduce auto-generated Gapic client for speech API (#1614)

---
+++ packages/google-cloud-speech/src/v1beta1/index.js
@@ -0,0 +1,31 @@
+/*!
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+var speechApi = require('./speech_api');
+var gax = require('google-gax');
+var extend = require('extend');
+
+function v1beta1(options) {
+  options = extend({
+    scopes: v1beta1.ALL_SCOPES
+  }, options);
+  var gaxGrpc = gax.grpc(options);
+  return speechApi(gaxGrpc);
+}
+v1beta1.SERVICE_ADDRESS = speechApi.SERVICE_ADDRESS;
+v1beta1.ALL_SCOPES = speechApi.ALL_SCOPES;
+module.exports = v1beta1;
