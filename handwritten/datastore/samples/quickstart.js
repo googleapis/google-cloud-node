6fff1f904c182565e0dcc7699647e259272cccae
Jason Dobry <jmdobry@users.noreply.github.com>
2016-10-03 14:44:10 -0700

    New quickstarts. (#226)
    * New quickstarts.
    
    * Address comments.

--- handwritten/datastore/samples/quickstart.js
+++ handwritten/datastore/samples/quickstart.js
@@ -1,15 +1,17 @@
-// Copyright 2016, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * Copyright 2016, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 'use strict';
 
@@ -34,8 +36,11 @@ const taskKey = datastoreClient.key([kind, id]);
 
 // Retrieves the entity
 datastoreClient.get(taskKey, (err, entity) => {
-  if (!err) {
-    // The entity was retrieved successfully
+  if (err) {
+    console.error(err);
+    return;
   }
+
+  console.log(`Entity: ${entity.key.id}`);
 });
 // [END datastore_quickstart]
