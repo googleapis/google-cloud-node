c4538d08c335304194b0af848ec363d1dca18d23
Jason Dobry <jmdobry@users.noreply.github.com>
2016-10-03 14:44:10 -0700

    New quickstarts. (#226)
    * New quickstarts.
    
    * Address comments.

--- handwritten/pubsub/samples/system-test/quickstart.test.js
+++ handwritten/pubsub/samples/system-test/quickstart.test.js
@@ -1,15 +1,17 @@
-// Copyright 2015-2016, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * Copyright 2016, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 'use strict';
 
@@ -35,14 +37,18 @@ describe(`pubsub:quickstart`, () => {
     const expectedTopicName = `my-new-topic`;
 
     pubsubMock = {
-      createTopic: (_topicName) => {
+      createTopic: (_topicName, _callback) => {
         assert.equal(_topicName, expectedTopicName);
+        assert.equal(typeof _callback, 'function');
 
         pubsub.createTopic(topicName, (err, topic, apiResponse) => {
+          _callback(err, topic, apiResponse);
           assert.ifError(err);
           assert.notEqual(topic, undefined);
           assert.equal(topic.name, fullTopicName);
           assert.notEqual(apiResponse, undefined);
+          assert.equal(console.log.calledOnce, true);
+          assert.deepEqual(console.log.firstCall.args, [`Topic ${topic.name} created.`]);
           done();
         });
       }
