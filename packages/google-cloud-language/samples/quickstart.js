2e6fd2710c049203862ce85e8dd9defd1eb82e2e
Jason Dobry <jmdobry@users.noreply.github.com>
2016-10-03 14:44:10 -0700

    New quickstarts. (#226)
    * New quickstarts.
    
    * Address comments.

---
+++ packages/google-cloud-language/samples/quickstart.js
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2016, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+// [START language_quickstart]
+// Imports the Google Cloud client library
+const Language = require('@google-cloud/language');
+
+// Your Google Cloud Platform project ID
+const projectId = 'YOUR_PROJECT_ID';
+
+// Instantiates a client
+const languageClient = Language({
+  projectId: projectId
+});
+
+// The text to analyze
+const text = 'Hello, world!';
+
+// Detects the sentiment of the text
+languageClient.detectSentiment(text, { verbose: true }, (err, sentiment) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log('Text: %s', text);
+  console.log('Sentiment: %j', sentiment);
+});
+// [END language_quickstart]
