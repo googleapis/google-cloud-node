4f2598d8fda76f79129797920e5dc6399dddf854
Thomas Rognon <tcrognon@gmail.com>
2016-10-08 15:14:23 -0500

    docs: datastore: maxApiCalls should be number not boolean

--- packages/datastore/src/request.js
+++ packages/datastore/src/request.js
@@ -231,7 +231,7 @@ DatastoreRequest.prototype.delete = function(keys, callback) {
  *     If not specified, default values are chosen by Datastore for the
  *     operation. Learn more about strong and eventual consistency
  *     [here](https://cloud.google.com/datastore/docs/articles/balancing-strong-and-eventual-consistency-with-google-cloud-datastore).
- * @param {boolean} options.maxApiCalls - Maximum API calls to make.
+ * @param {number} options.maxApiCalls - Maximum API calls to make.
  * @param {function} callback - The callback function.
  * @param {?error} callback.err - An error returned while making this request
  * @param {object|object[]} callback.entity - The entity object(s) which match
