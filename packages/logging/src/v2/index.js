32649dbcd81bdb1cce9d70aba12e04f4bc8f82cb
Jun Mukai <mukai@google.com>
2017-02-28 14:40:33 -0800

    Regenerate logging API client

--- packages/logging/src/v2/index.js
+++ packages/logging/src/v2/index.js
@@ -1,11 +1,11 @@
-/*!
- * Copyright 2016 Google Inc. All Rights Reserved.
+/*
+ * Copyright 2016 Google Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,11 +15,12 @@
  */
 'use strict';
 
-var configServiceV2Client = require('./config_service_v2_client');
 var loggingServiceV2Client = require('./logging_service_v2_client');
+var configServiceV2Client = require('./config_service_v2_client');
 var metricsServiceV2Client = require('./metrics_service_v2_client');
-var extend = require('extend');
 var gax = require('google-gax');
+var extend = require('extend');
+var union = require('lodash.union');
 
 function v2(options) {
   options = extend({
@@ -27,12 +28,17 @@ function v2(options) {
   }, options);
   var gaxGrpc = gax.grpc(options);
   var result = {};
-  extend(result, configServiceV2Client(gaxGrpc));
   extend(result, loggingServiceV2Client(gaxGrpc));
+  extend(result, configServiceV2Client(gaxGrpc));
   extend(result, metricsServiceV2Client(gaxGrpc));
   return result;
 }
 
 v2.SERVICE_ADDRESS = loggingServiceV2Client.SERVICE_ADDRESS;
-v2.ALL_SCOPES = loggingServiceV2Client.ALL_SCOPES;
+v2.ALL_SCOPES = union(
+  loggingServiceV2Client.ALL_SCOPES,
+  configServiceV2Client.ALL_SCOPES,
+  metricsServiceV2Client.ALL_SCOPES
+);
+
 module.exports = v2;
