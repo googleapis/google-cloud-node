104af3ba497559e3937cb8e51e9a0a3a281c12e9
Stephen Sawchuk <sawchuk@gmail.com>
2017-03-23 14:43:15 -0400

    convert log.js [ci skip]

--- packages/logging/src/log.js
+++ packages/logging/src/log.js
@@ -67,52 +67,17 @@ var Metadata = require('./metadata.js');
 function Log(logging, name, options) {
   options = options || {};
 
+  this.api = logging.api;
+
   this.formattedName_ = Log.formatName_(logging.projectId, name);
   this.name = this.formattedName_.split('/').pop();
   this.removeCircular_ = !!options.removeCircular;
 
   this.metadata_ = new Metadata(logging);
 
-  var methods = {
-    /**
-     * Delete the log.
-     *
-     * @resource [projects.logs.delete API Documentation]{@link https://cloud.google.com/logging/docs/reference/v2/rest/v2/projects.logs/delete}
-     *
-     * @param {function=} callback - The callback function.
-     * @param {?error} callback.err - An error returned while making this
-     *     request.
-     * @param {object} callback.apiResponse - The full API response.
-     *
-     * @example
-     * log.delete(function(err, apiResponse) {
-     *   if (!err) {
-     *     // The log was deleted.
-     *   }
-     * });
-     *
-     * //-
-     * // If the callback is omitted, we'll return a Promise.
-     * //-
-     * log.delete().then(function(data) {
-     *   var apiResponse = data[0];
-     * });
-     */
-    delete: {
-      protoOpts: {
-        service: 'LoggingServiceV2',
-        method: 'deleteLog'
-      },
-      reqOpts: {
-        logName: this.formattedName_
-      }
-    }
-  };
-
   commonGrpc.ServiceObject.call(this, {
     parent: logging,
-    id: this.name,
-    methods: methods
+    id: this.name
   });
 }
 
@@ -229,6 +194,40 @@ Log.prototype.critical = function(entry, options, callback) {
  */
 Log.prototype.debug = function(entry, options, callback) {
   this.write(Log.assignSeverityToEntries_(entry, 'DEBUG'), options, callback);
+};
+
+/**
+ * Delete the log.
+ *
+ * @resource [projects.logs.delete API Documentation]{@link https://cloud.google.com/logging/docs/reference/v2/rest/v2/projects.logs/delete}
+ *
+ * @param {object=} gaxOptions - Request configuration options, outlined
+ *     here: https://googleapis.github.io/gax-nodejs/global.html#CallOptions.
+ * @param {function=} callback - The callback function.
+ * @param {?error} callback.err - An error returned while making this
+ *     request.
+ * @param {object} callback.apiResponse - The full API response.
+ *
+ * @example
+ * log.delete(function(err, apiResponse) {
+ *   if (!err) {
+ *     // The log was deleted.
+ *   }
+ * });
+ *
+ * //-
+ * // If the callback is omitted, we'll return a Promise.
+ * //-
+ * log.delete().then(function(data) {
+ *   var apiResponse = data[0];
+ * });
+ */
+Log.prototype.delete = function(gaxOptions, callback) {
+  var reqOpts = {
+    logName: this.formattedName_
+  };
+
+  this.api.Logging.deleteLog(reqOpts, gaxOptions, callback);
 };
 
 /**
@@ -523,6 +522,8 @@ Log.prototype.warning = function(entry, options, callback) {
  * @param {module:logging/entry|module:logging/entry[]} entry - A log entry, or
  *     array of entries, to write.
  * @param {object=} options - Configuration object.
+ * @param {object} options.gaxOptions - Request configuration options, outlined
+ *     here: https://googleapis.github.io/gax-nodejs/global.html#CallOptions.
  * @param {object[]} options.labels - Labels to set on the log.
  * @param {object} options.resource - A default monitored resource for entries
  *     where one isn't specified.
@@ -592,25 +593,17 @@ Log.prototype.write = function(entry, options, callback) {
     options = {};
   }
 
-  var protoOpts = {
-    service: 'LoggingServiceV2',
-    method: 'writeLogEntries'
-  };
-
-  var reqOpts = extend({
-    logName: this.formattedName_
-  }, options);
-
-  var entries = arrify(entry);
-
-  this.decorateEntries_(entries, function(err, decoratedEntries) {
+  this.decorateEntries_(arrify(entry), function(err, decoratedEntries) {
     // Ignore errors (the API will speak up if it has an issue).
 
-    reqOpts.entries = decoratedEntries;
+    var reqOpts = extend({
+      logName: self.formattedName_,
+      entries: decoratedEntries
+    }, options);
 
-    self.request(protoOpts, reqOpts, function(err, resp) {
-      callback(err, resp);
-    });
+    delete reqOpts.gaxOptions;
+
+    self.api.Logging.writeLogEntries(reqOpts, options.gaxOptions, callback);
   });
 };
 
