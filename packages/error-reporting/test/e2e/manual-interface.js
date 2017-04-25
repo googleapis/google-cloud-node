207db1a07655123883d0644b187728cee3c8d09c
Dominic Kramer <DominicDKramer@google.com>
2017-04-25 15:11:07 -0700

    Simplify the tests
    Most of the checks can be done in the system tests.

--- packages/error-reporting/test/e2e/manual-interface.js
+++ packages/error-reporting/test/e2e/manual-interface.js
@@ -22,26 +22,7 @@ var isObject = lodash.isObject;
 
 var ErrorsApiTransport = require('../../utils/errors-api-transport.js');
 
-var TIMEOUT = 20000;
-
-function verifyPublishing(nodeEnv, ignoreEnvCheck, shouldSucceed, cb) {
-  if (nodeEnv) {
-    process.env.NODE_ENV = nodeEnv;
-  }
-  else {
-    delete process.env.NODE_ENV;
-  }
-
-  var errors = require('../../src/index.js')({
-    ignoreEnvironmentCheck: ignoreEnvCheck
-  });
-  var transport = new ErrorsApiTransport(errors._config, errors._logger);
-  deleteAllEvents(transport, function() {
-    publish(errors, transport, nodeEnv, ignoreEnvCheck, shouldSucceed, function() {
-      deleteAllEvents(transport, cb);
-    });
-  });
-}
+var TIMEOUT = 30000;
 
 function deleteAllEvents(transport, cb) {
   transport.deleteAllEvents(function(err) {
@@ -51,100 +32,46 @@ function deleteAllEvents(transport, cb) {
         assert.ifError(err);
         assert.ok(groups);
         assert.strictEqual(groups.length, 0);
-        cb();
+        setTimeout(cb, TIMEOUT);
       });
     }, TIMEOUT);
   });
 }
 
-function publish(errors, transport, nodeEnv, ignoreEnvCheck, shouldSucceed, cb) {
+function verifyPublish(errors, transport, cb) {
   var errorId = [Date.now(), 'manual-interface-e2e-fixture'].join('_');
   errors.report(new Error(errorId), function(err, response, body) {
-    if (shouldSucceed) {
-      assert.ifError(err);
-      assert(isObject(response));
-      assert.deepEqual(body, {});
-    }
-    else {
-      // The callback should have the error set in the situation where
-      // errors should not be reported to the console.
-      assert.ok(err);
-      assert.equal(response, null);
-      assert.equal(body, null);
-    }
+    assert.ifError(err);
+    assert(isObject(response));
+    assert.deepEqual(body, {});
 
     setTimeout(function() {
       transport.getAllGroups(function(err, groups) {
         assert.ifError(err);
         assert.ok(groups);
-        assert.strictEqual(groups.length, shouldSucceed ? 1 : 0);
-        cb();
+        assert.strictEqual(groups.length, 1);
+        setTimeout(cb, TIMEOUT);
       });
     }, TIMEOUT);
   });
 }
 
 describe('End-to-end test of manual interface', function() {
-  var originalNodeEnv;
-  beforeEach(function() {
-    originalNodeEnv = process.env.NODE_ENV;
+  var errors;
+  var transport;
+  before(function(done) {
+    errors = require('../../src/index.js')({
+      ignoreEnvironmentCheck: true
+    });
+    transport = new ErrorsApiTransport(errors._config, errors._logger);
+    deleteAllEvents(transport, done);
   });
 
-  afterEach(function() {
-    if (originalNodeEnv) {
-      process.env.NODE_ENV = originalNodeEnv;
-    }
-    else {
-      delete process.env.NODE_ENV;
-    }
+  after(function(done) {
+    deleteAllEvents(transport, done);
   });
 
-  it('should report errors with NODE_ENV=production and ignore env off', function(done) {
-    var nodeEnv = 'production';
-    var ignoreEnvCheck = false;
-    var shouldSucceed = true;
-    verifyPublishing(nodeEnv, ignoreEnvCheck, shouldSucceed, done);
+  it('should report errors', function(done) {
+    verifyPublish(errors, transport, done);
   });
-
-  it('should report errors with NODE_ENV=production and ignore env on', function(done) {
-    var nodeEnv = 'production';
-    var ignoreEnvCheck = true;
-    var shouldSucceed = true;
-    verifyPublishing(nodeEnv, ignoreEnvCheck, shouldSucceed, done);
-  });
-
-  it('should report errors with NODE_ENV=development and ignore env on', function(done) {
-    var nodeEnv = 'development';
-    var ignoreEnvCheck = true;
-    var shouldSucceed = true;
-    verifyPublishing(nodeEnv, ignoreEnvCheck, shouldSucceed, done);
-  });
-
-  it('should report errors without NODE_ENV set and ignore env on', function(done) {
-    var nodeEnv = null;
-    var ignoreEnvCheck = true;
-    var shouldSucceed = true;
-    verifyPublishing(nodeEnv, ignoreEnvCheck, shouldSucceed, done);
-  });
-
-  it('should not report errors with NODE_ENV=development and ignore env off', function(done) {
-    var nodeEnv = 'development';
-    var ignoreEnvCheck = false;
-    var shouldSucceed = false;
-    verifyPublishing(nodeEnv, ignoreEnvCheck, shouldSucceed, done);
-  });
-
-  it('should not report errors without NODE_ENV set and ignore env off', function(done) {
-    var nodeEnv = null;
-    var ignoreEnvCheck = false;
-    var shouldSucceed = false;
-    verifyPublishing(nodeEnv, ignoreEnvCheck, shouldSucceed, done);
-  });
-
-  it('should not report errors with NODE_ENV != production and ignore env off', function(done) {
-    var nodeEnv = 'some-other-value';
-    var ignoreEnvCheck = false;
-    var shouldSucceed = false;
-    verifyPublishing(nodeEnv, ignoreEnvCheck, shouldSucceed, done);
-  });
-}).timeout(6*TIMEOUT);
+}).timeout(5*TIMEOUT);
