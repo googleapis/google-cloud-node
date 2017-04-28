436c28f85207922faf9ef406f827fab93ae5bd24
Stephen Sawchuk <sawchuk@gmail.com>
2017-04-28 15:32:24 -0400

    common-grpc: update to grpc@1.3.0

--- packages/datastore/src/entity.js
+++ packages/datastore/src/entity.js
@@ -165,7 +165,7 @@ entity.Key = Key;
  * // <Buffer 68 65 6c 6c 6f>
  */
 function decodeValueProto(valueProto) {
-  var valueType = valueProto.value_type;
+  var valueType = valueProto.valueType;
   var value = valueProto[valueType];
 
   switch (valueType) {
@@ -328,7 +328,7 @@ entity.encodeValue = encodeValue;
  *     map: {
  *       name: {
  *         value: {
- *           value_type: 'stringValue',
+ *           valueType: 'stringValue',
  *           stringValue: 'Stephen'
  *         }
  *       }
@@ -469,9 +469,9 @@ function keyFromKeyProto(keyProto) {
   keyProto.path.forEach(function(path, index) {
     keyOptions.path.push(path.kind);
 
-    var id = path[path.id_type];
+    var id = path[path.idType];
 
-    if (path.id_type === 'id') {
+    if (path.idType === 'id') {
       id = new entity.Int(id);
     }
 
