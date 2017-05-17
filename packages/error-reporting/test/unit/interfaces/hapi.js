8b43f0a643725abfdb19c43159a04dc5e02c69cb
Dominic Kramer <DominicDKramer@google.com>
2017-05-17 13:25:22 -0700

    Fix indentation

--- packages/error-reporting/test/unit/interfaces/hapi.js
+++ packages/error-reporting/test/unit/interfaces/hapi.js
@@ -114,31 +114,31 @@ describe('Hapi interface', function() {
       fakeServer.removeAllListeners();
     });
     it('Should call continue when a boom is emitted if reply is an object',
-    function(done) {
-      plugin.register(fakeServer, null, function() {});
-      fakeServer.emit(EVENT, {response: {isBoom: true}},
-        {
-          continue: function() {
-            // The continue function should be called
-            done();
+      function(done) {
+        plugin.register(fakeServer, null, function() {});
+        fakeServer.emit(EVENT, {response: {isBoom: true}},
+          {
+            continue: function() {
+              // The continue function should be called
+              done();
+            }
           }
-        }
-     );
+       );
     });
     it('Should call continue when a boom is emitted if reply is a function',
-    function(done) {
-      // Manually testing has shown that in actual usage the `reply` object
-      // provided to listeners of the `onPreResponse` event can be a function
-      // that has a `continue` property that is a function.
-      // If `reply.continue()` is not invoked in this situation, the Hapi
-      // app will become unresponsive.
-      plugin.register(fakeServer, null, function() {});
-      var reply = function() {};
-      reply.continue = function() {
-        // The continue function should be called
-        done();
-      };
-      fakeServer.emit(EVENT, {response: {isBoom: true}}, reply);
+      function(done) {
+        // Manually testing has shown that in actual usage the `reply` object
+        // provided to listeners of the `onPreResponse` event can be a function
+        // that has a `continue` property that is a function.
+        // If `reply.continue()` is not invoked in this situation, the Hapi
+        // app will become unresponsive.
+        plugin.register(fakeServer, null, function() {});
+        var reply = function() {};
+        reply.continue = function() {
+          // The continue function should be called
+          done();
+        };
+        fakeServer.emit(EVENT, {response: {isBoom: true}}, reply);
     });
     it('Should call sendError when a boom is received', function(done) {
       var fakeClient = {
