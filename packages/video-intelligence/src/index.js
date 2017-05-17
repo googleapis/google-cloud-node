75c0b64492f415bcfc3e6379a7e51ff36a7c680e
Luke Sneeringer <luke@sneeringer.com>
2017-05-17 13:49:05 -0700

    Fix lint.

--- packages/video-intelligence/src/index.js
+++ packages/video-intelligence/src/index.js
@@ -14,41 +14,41 @@
  * limitations under the License.
  */
 
- 'use strict';
+'use strict';
 
- var gapic = {
-   v1beta1: require('./v1beta1/index.js'),
- };
+var gapic = {
+  v1beta1: require('./v1beta1/index.js'),
+};
 
- const VERSION = require('../package.json').version;
+const VERSION = require('../package.json').version;
 
 
- /**
-  * Create an imageAnnotatorClientfor the appropriate version.
-  *
-  * @param {Object=} opts - The optional parameters.
-  * @param {String=} opts.servicePath
-  *   The domain name of the API remote host.
-  * @param {number=} opts.port
-  *   The port on which to connect to the remote host.
-  * @param {grpc.ClientCredentials=} opts.sslCreds
-  *   A ClientCredentials for use with an SSL-enabled channel.
-  * @param {Object=} opts.clientConfig
-  *   The customized config to build the call settings. See
-  *   {@link gax.constructSettings} for the format.
-  */
- function videointelligence_v1beta1(opts) {
-   // Define the header options.
-   opts = opts || {};
-   opts.libName = 'gccl';
-   opts.libVersion = VERSION;
+/**
+ * Create an imageAnnotatorClientfor the appropriate version.
+ *
+ * @param {Object=} opts - The optional parameters.
+ * @param {String=} opts.servicePath
+ *   The domain name of the API remote host.
+ * @param {number=} opts.port
+ *   The port on which to connect to the remote host.
+ * @param {grpc.ClientCredentials=} opts.sslCreds
+ *   A ClientCredentials for use with an SSL-enabled channel.
+ * @param {Object=} opts.clientConfig
+ *   The customized config to build the call settings. See
+ *   {@link gax.constructSettings} for the format.
+ */
+function videointelligenceV1beta1(opts) {
+  // Define the header options.
+  opts = opts || {};
+  opts.libName = 'gccl';
+  opts.libVersion = VERSION;
 
-   // Create the image annotator client with the provided options.
-   var client = gapic.v1beta1(opts).videoIntelligenceServiceClient(opts);
-   return client;
- }
+  // Create the image annotator client with the provided options.
+  var client = gapic.v1beta1(opts).videoIntelligenceServiceClient(opts);
+  return client;
+}
 
 // The default export should be the latest version.
 // Assign all versions as version properties on the default.
 module.exports = videointelligence_v1beta1;
-module.exports.v1beta1 = videointelligence_v1beta1;
+module.exports.v1beta1 = videointelligenceV1beta1;
