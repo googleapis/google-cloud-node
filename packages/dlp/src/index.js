950d332476f40c93f7157ea469af60d4236d1712
Ernest Landrito <landrito@google.com>
2017-05-25 13:32:34 -0700

    Regen

--- packages/dlp/src/index.js
+++ packages/dlp/src/index.js
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016 Google Inc. All rights reserved.
+ * Copyright 2017, Google Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,8 +15,9 @@
  */
 'use strict';
 
+var extend = require('extend');
 var gapic = {
- v2beta1: require('./v2beta1'),
+  v2beta1: require('./v2beta1')
 };
 
 const VERSION = require('../package.json').version;
@@ -25,27 +26,28 @@ const VERSION = require('../package.json').version;
  * Create an dlpServiceClient with additional helpers for common
  * tasks.
  *
- * @param {Object=} opts - The optional parameters.
- * @param {String=} opts.servicePath
- *   The domain name of the API remote host.
- * @param {number=} opts.port
- *   The port on which to connect to the remote host.
- * @param {grpc.ClientCredentials=} opts.sslCreds
- *   A ClientCredentials for use with an SSL-enabled channel.
- * @param {Object=} opts.clientConfig
- *   The customized config to build the call settings. See
- *   {@link gax.constructSettings} for the format.
+ * The DLP API is a service that allows clients
+ * to detect the presence of Personally Identifiable Information (PII) and other
+ * privacy-sensitive data in user-supplied, unstructured data streams, like text
+ * blocks or images.
+ * The service also includes methods for sensitive data redaction and
+ * scheduling of data scans on Google Cloud Platform based data sets.
+ *
+ * @param {object=} options - [Configuration object](#/docs).
+ * @param {number=} options.port - The port on which to connect to the remote host.
+ * @param {string=} options.servicePath - The domain name of the API remote host.
  */
-function dlp_v2beta1(opts) {
+function dlpV2beta1(options) {
   // Define the header options.
-  opts = opts || {};
-  opts.libName = 'gccl';
-  opts.libVersion = VERSION;
+  options = extend({}, options, {
+    libName: 'gccl',
+    libVersion: VERSION
+  });
 
   // Create the image annotator client with the provided options.
-  var client = gapic.v2beta1(opts).dlpServiceClient(opts);
+  var client = gapic.v2beta1(options).dlpServiceClient(options);
   return client;
 }
 
-module.exports = dlp_v2beta1;
-module.exports.v2beta1 = dlp_v2beta1;
+module.exports = dlpV2beta1;
+module.exports.v2beta1 = dlpV2beta1;
