b747c3b88d5223be9b3996feec34463aa8d6deeb
Dominic Kramer <DominicDKramer@google.com>
2017-06-13 14:52:39 -0700

    Update buildStackTrace to use captureStackTrace
    The `Error.captureStackTrace` method is now used in the
    `buildStackTrace` function to create a stack trace.

--- packages/error-reporting/src/build-stack-trace.js
+++ packages/error-reporting/src/build-stack-trace.js
@@ -22,7 +22,6 @@ var is = require('is');
  * Constructs a string representation of the stack trace at the point where
  * this function was invoked.  Note that the stack trace will not include any
  * references to this function itself.
- * @function buildStackTrace
  * @param {String?} message - The message that should appear as the first line
  *   of the stack trace.  This value defaults to the empty string.
  * @param {Number?} numSkip - The number of additional lines to not display
@@ -33,13 +32,16 @@ var is = require('is');
  *   where this method was invoked.
  */
 function buildStackTrace(message, numSkip) {
-  var fauxError = new Error('');
-  var fullStack = fauxError.stack.split('\n');
+  var target = {};
+  // Build a stack trace without the frames associated with `buildStackTrace`.
+  // The stack is located at `target.stack`.
+  Error.captureStackTrace(target, buildStackTrace);
+  // Ignore the first line, which contains the error message, because we will
+  // add our own error message later.
+  var fullStack = target.stack.split('\n').slice(1);
   var prefix = message ? message + '\n' : '';
-  // Two additional lines are skipped to skip the default empty message
-  // and the line referencing this function itself.
-  var totalNumSkip = (is.number(numSkip) ? numSkip : 0) + 2;
-  return prefix + fullStack.slice(totalNumSkip).join('\n');
+  var realNumSkip = is.number(numSkip) ? numSkip : 0;
+  return prefix + fullStack.slice(realNumSkip).join('\n');
 }
 
 module.exports = buildStackTrace;
