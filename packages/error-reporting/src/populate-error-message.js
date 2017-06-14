3df370b98ebc651e5bb8c0ad83b0ffc60a1006f2
Dominic Kramer <DominicDKramer@google.com>
2017-06-13 17:09:54 -0700

    Consolidate code in populate-error-message.js

--- packages/error-reporting/src/populate-error-message.js
+++ packages/error-reporting/src/populate-error-message.js
@@ -18,9 +18,6 @@
 var has = require('lodash.has');
 var is = require('is');
 var isObject = is.object;
-var isString = is.string;
-var isNumber = is.number;
-var isFunction = is.function;
 var buildStackTrace = require('./build-stack-trace.js');
 
 /**
@@ -35,17 +32,13 @@ var buildStackTrace = require('./build-stack-trace.js');
  */
 function populateErrorMessage(ob, em) {
   if (ob === null || ob === undefined) {
-    populateFromUnknown(ob, em);
-  } else if (ob instanceof Error) {
+    em.setMessage(buildStackTrace('' + ob, 3));
+  } else if (ob.stack) {
     populateFromError(ob, em);
   } else if (typeof ob === 'object' && isObject(ob)) {
     populateFromObject(ob, em);
-  } else if (typeof ob === 'string' && isString(ob)) {
-    populateFromString(ob, em);
-  } else if (typeof ob === 'number') {
-    populateFromNumber(ob, em);
   } else {
-    populateFromUnknown(ob, em);
+    em.setMessage(buildStackTrace(ob.toString(), 3));
   }
 
   return em;
@@ -127,53 +120,6 @@ function populateFromObject(ob, errorMessage) {
     errorMessage.setServiceContext(ob.serviceContext.service,
                                    ob.serviceContext.version);
   }
-}
-
-/**
- * Handles validation of an error which has been indicated to be of type String.
- * This function will create a new instance of the Error class to produce a
- * stack trace for submission to the API and check to confirm that the given
- * value is of type string.
- * @function populateFromString
- * @param {String} str - the String indicated as the content of the error
- * @param {ErrorMessage} errorMessage - the error message instance to marshal
- *  error information into.
- * @returns {Undefined} - does not return anything
- */
-function populateFromString(str, errorMessage) {
-  errorMessage.setMessage(buildStackTrace(str, 3));
-}
-
-/**
- * Handles routing and validation for parsing an error which has been indicated
- * to be of type Number. This handler will manufacture a new Error to create
- * a stack-trace for submission to the Error API and will attempt to caste the
- * given number to a string for submission to the Error API.
- * @function populateFromNumber
- * @param {Number} num - the number submitted as content for the error message
- * @param {ErrorMessage} errorMessage - the error messag instance to marshall
- *  error information into.
- * @returns {Undefined} - does not return anything
- */
-function populateFromNumber(num, errorMessage) {
-  var message = isNumber(num) && isFunction(num.toString) ? num.toString() : '';
-  errorMessage.setMessage(buildStackTrace(message, 3));
-}
-
-/**
- * Handles unknown/unsupported input as the content of the error message. Since
- * the problem-space is not defined for this path the library only attempts to
- * manufacture a stack trace for submission to the API and discards the input
- * that was given as the error content.
- * @function populateFromUnknown
- * @param {Any} ob - the unknown/unsupported input indicated as the content of
- *  the error.
- * @param {ErrorMessage} errorMessage - the error message instance to marshal
- *  error information into.
- * @returns {Undefined} - does not return anything
- */
-function populateFromUnknown(ob, errorMessage) {
-  errorMessage.setMessage(buildStackTrace('' + ob, 3));
 }
 
 module.exports = populateErrorMessage;
