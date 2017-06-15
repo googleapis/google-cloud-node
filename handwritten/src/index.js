fd3cf31a05318f58c29a1cb121675cb9c5433912
Dominic Kramer <DominicDKramer@google.com>
2017-06-15 10:31:19 -0700

    errors-reporting: Unhandled rejections are reported (#2360)

--- handwritten/src/index.js
+++ handwritten/src/index.js
@@ -154,6 +154,17 @@ function Errors(initConfiguration) {
    * app.use(errors.koa);
    */
   this.koa = koa(this._client, this._config);
+
+  if (this._config.getReportUnhandledRejections()) {
+    var that = this;
+    process.on('unhandledRejection', function(reason) {
+      console.log('UnhandledPromiseRejectionWarning: ' +
+        'Unhandled promise rejection: ' + reason +
+        '.  This rejection has been reported to the ' +
+        'Google Cloud Platform error-reporting console.');
+      that.report(reason);
+    });
+  }
 }
 
 module.exports = Errors;
