73eec6d9771c8d873092f84b14813183a713fb15
Dave Gramlich <callmehiphop@gmail.com>
2017-06-21 17:20:32 -0400

    add comment explaining error handling

--- packages/pubsub/src/subscription.js
+++ packages/pubsub/src/subscription.js
@@ -563,7 +563,13 @@ Subscription.prototype.openConnection_ = function() {
     self.connection = requestFn();
 
     self.connection.on('error', function(err) {
-      var retryCodes = [2, 4, 14];
+      // in the event of a connection error, we will attempt to re-establish
+      // a new connection if the grpc error code is something safe to retry on
+      var retryCodes = [
+        2, // Unknown Error
+        4, // DeadlineExceeded
+        14 // Unavailable
+      ];
 
       if (retryCodes.indexOf(err.code) > -1) {
         self.openConnection_();
