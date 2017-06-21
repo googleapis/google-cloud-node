db200cc26f877bbb39b2c8396a7ec0907de83a3d
Dave Gramlich <callmehiphop@gmail.com>
2017-06-21 13:14:03 -0400

    update license year

--- packages/pubsub/src/message.js
+++ packages/pubsub/src/message.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2014 Google Inc. All Rights Reserved.
+ * Copyright 2017 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,6 +63,7 @@ Message.prototype.modifyAckDeadline = function(milliseconds, callback) {
     if (is.fn(callback)) {
       setImmediate(callback);
     }
+
     return;
   }
 
