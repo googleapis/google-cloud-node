648d6f7efa76d75c01db2e6b82dafa3d76cac74a
Stephen Sawchuk <sawchuk@gmail.com>
2017-07-20 12:00:27 -0400

    speech:tests bring coverage back for helpers.js

--- packages/speech/test/helpers.test.js
+++ packages/speech/test/helpers.test.js
@@ -22,33 +22,122 @@ var stream = require('stream');
 
 var Speech = require('../');
 
-
 describe('Speech helper methods', () => {
   var sandbox = sinon.sandbox.create();
+  var speech;
+
+  beforeEach(() => {
+    speech = Speech.v1();
+  });
 
   afterEach(() => {
     sandbox.restore();
   });
 
   describe('streamingRecognize', () => {
-    it('writes the config to the resulting stream', () => {
-      var speech = Speech.v1();
+    var CONFIG = {config: {languageCode: 'en-us'}};
+    var OPTIONS = {timeout: Infinity};
 
+    it('writes the config to the resulting stream', (done) => {
       // Stub the underlying _streamingRecognize method to just return
       // a bogus stream.
-      var writable = stream.Writable();
-      var sr = sandbox.stub(speech, '_streamingRecognize').returns(writable);
+      var requestStream = new stream.PassThrough({ objectMode: true });
+      var sr = sandbox.stub(speech, '_streamingRecognize')
+        .returns(requestStream);
 
       // Call the new helper method and establish that the config was
       // forwarded as expected.
-      var config = {config: {languageCode: 'en-us'}};
-      var options = {timeout: Infinity};
-      speech.streamingRecognize(config, options);
+      var userStream = speech.streamingRecognize(CONFIG, OPTIONS);
 
       // Establish that the underlying streamingRecognize was called with
       // the options.
       assert(sr.calledOnce);
-      assert(sr.calledWithExactly(options));
+      assert(sr.calledWithExactly(OPTIONS));
+
+      requestStream._write = (data, enc, next) => {
+        assert.deepStrictEqual(data, {
+          streamingConfig: CONFIG
+        });
+        setImmediate(done);
+        next(null, data);
+      };
+
+      userStream.emit('writing');
+    });
+
+    it('does not require options', () => {
+      // Stub the underlying _streamingRecognize method to just return
+      // a bogus stream.
+      var requestStream = new stream.PassThrough({ objectMode: true });
+      var sr = sandbox.stub(speech, '_streamingRecognize')
+        .returns(requestStream);
+
+      var userStream = speech.streamingRecognize(CONFIG);
+
+      userStream.emit('writing');
+
+      assert(sr.calledOnce);
+      assert(sr.calledWithExactly({}));
+    });
+
+    it('destroys the user stream when the request stream errors', (done) => {
+      // Stub the underlying _streamingRecognize method to just return
+      // a bogus stream.
+      var requestStream = new stream.PassThrough({ objectMode: true });
+      sandbox.stub(speech, '_streamingRecognize').returns(requestStream);
+
+      var userStream = speech.streamingRecognize(CONFIG, OPTIONS);
+
+      var error = new Error('Request stream error');
+
+      userStream.once('error', (err) => {
+        assert.strictEqual(err, error);
+        done();
+      });
+
+      userStream.emit('writing');
+
+      requestStream.emit('error', error);
+    });
+
+    it('re-emits response from the request stream', (done) => {
+      // Stub the underlying _streamingRecognize method to just return
+      // a bogus stream.
+      var requestStream = new stream.PassThrough({ objectMode: true });
+      sandbox.stub(speech, '_streamingRecognize').returns(requestStream);
+
+      var userStream = speech.streamingRecognize(CONFIG, OPTIONS);
+
+      var response = {};
+
+      userStream.on('response', (_response) => {
+        assert.strictEqual(_response, response);
+        done();
+      });
+
+      userStream.emit('writing');
+      requestStream.emit('response', response);
+    });
+
+    it('wraps incoming audio data', (done) => {
+      // Stub the underlying _streamingRecognize method to just return
+      // a bogus stream.
+      var requestStream = new stream.PassThrough({ objectMode: true });
+      sandbox.stub(speech, '_streamingRecognize').returns(requestStream);
+
+      var userStream = speech.streamingRecognize(CONFIG, OPTIONS);
+      var audioContent = new Buffer('audio content');
+
+      requestStream._write = (data, enc, next) => {
+        if (data && data.streamingConfig !== CONFIG) {
+          assert.deepStrictEqual(data, { audioContent });
+          setImmediate(done);
+        }
+
+        next(null, data);
+      };
+
+      userStream.end(audioContent);
     });
   });
 });
