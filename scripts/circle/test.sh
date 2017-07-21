1ea7a8149768dbde47beb7ae74d778f3151be872
Stephen Sawchuk <sawchuk@gmail.com>
2017-07-20 22:18:56 -0400

    test

--- scripts/circle/test.sh
+++ scripts/circle/test.sh
@@ -39,10 +39,10 @@ fi
 git config --global user.name "circle-ci"
 git config --global user.email "circle-ci@circleci.com"
 
-# nvm use v4
-# npm install
-# npm run lint
-# node ./scripts/build.js
+nvm use v4
+npm install
+npm run lint
+node ./scripts/build.js
 
 export COVERALLS_REPO_TOKEN="kEhKxvu3L9Z4GiClA9LcKYk7G9i9dl83k"
 export CIRCLE_ARTIFACTS="$(pwd)/.coverage"
