ae8ee2ae690fb98c8890c149cac6470fb211872b
Evawere Ogbe <eoogbe@gmail.com>
2017-08-15 11:39:00 -0700

    Update videointelligence v1beta1 client (#2524)

--- packages/google-cloud-videointelligence/src/index.js
+++ packages/google-cloud-videointelligence/src/index.js
@@ -1,11 +1,11 @@
 /*
- * Copyright 2017 Google Inc. All rights reserved.
+ * Copyright 2017, Google Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,57 +16,52 @@
 
 /*!
  * @module video-intelligence
+ * @name VideoIntelligence
  */
 
 'use strict';
 
-var v1beta1 = require('./v1beta1/index.js');
+var extend = require('extend');
+var gapic = {
+  v1beta1: require('./v1beta1')
+};
+var gaxGrpc = require('google-gax').grpc();
 
 const VERSION = require('../package.json').version;
 
 /**
- * [Google Cloud Video Intelligence API](https://cloud.google.com/video-intelligence)
- * makes videos searchable, and discoverable, by extracting metadata with an
- * easy to use REST API. You can now search every moment of every video file in
- * your catalog and find every occurrence as well as its significance. It
- * quickly annotates videos stored in Google Cloud Storage, and helps you
- * identify key nouns entities of your video, and when they occur within the
- * video. Separate signal from noise, by retrieving relevant information at the
- * video, shot or per frame.
+ * Create a videoIntelligenceServiceClient with additional helpers for common
+ * tasks.
  *
- * <h2>This is an auto-generated API</h2>
+ * Use this service to interact with the Google Cloud Video Intelligence API.
  *
- * It does not follow the conventions you're familiar with from other parts of
- * our library. A handwritten layer is not yet available.
- *
- * The example below shows you how to instantiate the generated client. For
- * further documentation, please browse the
- * [Video Intelligence .proto files](https://github.com/googleapis/googleapis/blob/master/google/cloud/videointelligence/v1beta1/video_intelligence.proto)
- * on GitHub.
- *
- * @constructor
- * @alias module:video-intelligence
- *
+ * @param {object=} options - [Configuration object](#/docs).
+ * @param {number=} options.port - The port on which to connect to
+ *     the remote host.
+ * @param {string=} options.servicePath - The domain name of the
+ *     API remote host.
  * @resource [Cloud Video Intelligence](https://cloud.google.com/video-intelligence)
- *
- * @param {object} options - [Configuration object](#/docs).
- * @param {number=} options.port - The port on which to connect to the remote
- *     host.
- * @param {string=} options.servicePath - The domain name of the API remote
- *     host.
  */
-function VideoIntelligence(options) {
+function videoIntelligenceV1beta1(options) {
   // Define the header options.
-  options = options || {};
-  options.libName = 'gccl';
-  options.libVersion = VERSION;
+  options = extend({}, options, {
+    libName: 'gccl',
+    libVersion: VERSION
+  });
 
-  // Create the image annotator client with the provided options.
-  var client = v1beta1(options).videoIntelligenceServiceClient(options);
+  // Create the client with the provided options.
+  var client = gapic.v1beta1(options).videoIntelligenceServiceClient(options);
   return client;
 }
 
-// The default export should be the latest version.
-// Assign all versions as version properties on the default.
-module.exports = VideoIntelligence;
-module.exports.v1beta1 = v1beta1;
+var v1beta1Protos = {};
+
+extend(v1beta1Protos, gaxGrpc.load([{
+  root: require('google-proto-files')('..'),
+  file: 'google/cloud/videointelligence/v1beta1/video_intelligence.proto'
+}]).google.cloud.videointelligence.v1beta1);
+
+module.exports = videoIntelligenceV1beta1;
+module.exports.types = v1beta1Protos;
+module.exports.v1beta1 = videoIntelligenceV1beta1;
+module.exports.v1beta1.types = v1beta1Protos;
