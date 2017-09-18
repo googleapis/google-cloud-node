04e65b2e762e6a10290063fba9432590e9c616d6
Luke Sneeringer <luke@sneeringer.com>
2017-09-18 15:19:30 -0700

    Update Natural Language (#2612)

--- packages/google-cloud-language/src/index.js
+++ packages/google-cloud-language/src/index.js
@@ -14,35 +14,49 @@
  * limitations under the License.
  */
 
-/*!
- * @module language
- * @name Language
- */
-
+ /*!
+  * @module language
+  * @name Language
+  */
 'use strict';
 
 var extend = require('extend');
 var gapic = {
   v1: require('./v1'),
-  v1beta2: require('./v1beta2')
+  v1beta2: require('./v1beta2'),
 };
 var gaxGrpc = require('google-gax').grpc();
+var path = require('path');
 
 const VERSION = require('../package.json').version;
 
 /**
- * Create a V1 languageServiceClient with additional helpers for common
+ * Create an languageServiceClient with additional helpers for common
  * tasks.
  *
  * Provides text analysis operations such as sentiment analysis and entity
  * recognition.
  *
- * @constructor
- * @alias module:language
- *
  * @param {object=} options - [Configuration object](#/docs).
+ * @param {object=} options.credentials - Credentials object.
+ * @param {string=} options.credentials.client_email
+ * @param {string=} options.credentials.private_key
+ * @param {string=} options.email - Account email address. Required when using a
+ *     .pem or .p12 keyFilename.
+ * @param {string=} options.keyFilename - Full path to the a .json, .pem, or
+ *     .p12 key downloaded from the Google Developers Console. If you provide
+ *     a path to a JSON file, the projectId option above is not necessary.
+ *     NOTE: .pem and .p12 require you to specify options.email as well.
  * @param {number=} options.port - The port on which to connect to
  *     the remote host.
+ * @param {string=} options.projectId - The project ID from the Google
+ *     Developer's Console, e.g. 'grape-spaceship-123'. We will also check
+ *     the environment variable GCLOUD_PROJECT for your project ID. If your
+ *     app is running in an environment which supports
+ *     {@link https://developers.google.com/identity/protocols/application-default-credentials Application Default Credentials},
+ *     your project ID will be detected automatically.
+ * @param {function=} options.promise - Custom promise module to use instead
+ *     of native Promises.
  * @param {string=} options.servicePath - The domain name of the
  *     API remote host.
  */
@@ -58,16 +72,41 @@ function languageV1(options) {
   return client;
 }
 
+var v1Protos = {};
+
+extend(v1Protos, gaxGrpc.loadProto(
+  path.join(__dirname, '..', 'protos',
+    'google/cloud/language/v1/language_service.proto')
+).google.cloud.language.v1);
+
+
 /**
- * Create a V1beta2 languageServiceClient with additional helpers for common
+ * Create an languageServiceClient with additional helpers for common
  * tasks.
  *
  * Provides text analysis operations such as sentiment analysis and entity
  * recognition.
  *
  * @param {object=} options - [Configuration object](#/docs).
+ * @param {object=} options.credentials - Credentials object.
+ * @param {string=} options.credentials.client_email
+ * @param {string=} options.credentials.private_key
+ * @param {string=} options.email - Account email address. Required when using a
+ *     .pem or .p12 keyFilename.
+ * @param {string=} options.keyFilename - Full path to the a .json, .pem, or
+ *     .p12 key downloaded from the Google Developers Console. If you provide
+ *     a path to a JSON file, the projectId option above is not necessary.
+ *     NOTE: .pem and .p12 require you to specify options.email as well.
  * @param {number=} options.port - The port on which to connect to
  *     the remote host.
+ * @param {string=} options.projectId - The project ID from the Google
+ *     Developer's Console, e.g. 'grape-spaceship-123'. We will also check
+ *     the environment variable GCLOUD_PROJECT for your project ID. If your
+ *     app is running in an environment which supports
+ *     {@link https://developers.google.com/identity/protocols/application-default-credentials Application Default Credentials},
+ *     your project ID will be detected automatically.
+ * @param {function=} options.promise - Custom promise module to use instead
+ *     of native Promises.
  * @param {string=} options.servicePath - The domain name of the
  *     API remote host.
  */
@@ -83,19 +122,13 @@ function languageV1beta2(options) {
   return client;
 }
 
-var v1Protos = {};
-
-extend(v1Protos, gaxGrpc.load([{
-  root: require('google-proto-files')('..'),
-  file: 'google/cloud/language/v1/language_service.proto'
-}]).google.cloud.language.v1);
-
 var v1beta2Protos = {};
 
-extend(v1beta2Protos, gaxGrpc.load([{
-  root: require('google-proto-files')('..'),
-  file: 'google/cloud/language/v1beta2/language_service.proto'
-}]).google.cloud.language.v1beta2);
+extend(v1beta2Protos, gaxGrpc.loadProto(
+  path.join(__dirname, '..', 'protos',
+    'google/cloud/language/v1beta2/language_service.proto')
+).google.cloud.language.v1beta2);
+
 
 module.exports = languageV1;
 module.exports.types = v1Protos;
