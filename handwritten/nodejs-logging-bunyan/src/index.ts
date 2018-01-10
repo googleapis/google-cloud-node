226ecd5f55b8589b95baac1d9d05e784e5286bb7
Dominic Kramer <DominicDKramer@google.com>
2018-01-10 10:19:09 -0800

    chore: migrate the code to typescript (#18)
    BREAKING CHANGE:  As a result of the migration, the way in which this package is required and used has changed slightly.
    
    That is, the package is required and a `LoggingBuyan` object is created as follows on Node 6+.  In particular, note the `{}` braces used in the require statement and the use of `new` which is now mandatory.
    
    ```javascript
    const {LoggingBunyan} = require('@google-cloud/logging-bunyan');
    
    // Creates a Bunyan Stackdriver Logging client
    const loggingBunyan = new LoggingBunyan();
    ```
    
    On Node 4, the  require syntax is slightly different, but the use of `new` is still required.
    
    ```javascript
    const LoggingBunyan = require('@google-cloud/logging-bunyan').LoggingBunyan;
    
    // Creates a Bunyan Stackdriver Logging client
    const loggingBunyan = new LoggingBunyan();
    ```

---
+++ handwritten/nodejs-logging-bunyan/src/index.ts
@@ -0,0 +1,231 @@
+/*!
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Writable} from 'stream';
+import * as util from 'util';
+const logging = require('@google-cloud/logging');
+
+// Map of Stackdriver logging levels.
+const BUNYAN_TO_STACKDRIVER: Map<number, string> = new Map([
+  [60, 'CRITICAL'],
+  [50, 'ERROR'],
+  [40, 'WARNING'],
+  [30, 'INFO'],
+  [20, 'DEBUG'],
+  [10, 'DEBUG'],
+]);
+
+/**
+ * Key to use in the Bunyan payload to allow users to indicate a trace for the
+ * request, and to store as an intermediate value on the log entry before it
+ * gets written to the Stackdriver logging API.
+ */
+export const LOGGING_TRACE_KEY = 'logging.googleapis.com/trace';
+
+/**
+ * Gets the current fully qualified trace ID when available from the
+ * @google-cloud/trace-agent library in the LogEntry.trace field format of:
+ * "projects/[PROJECT-ID]/traces/[TRACE-ID]".
+ */
+function getCurrentTraceFromAgent() {
+  const agent = global._google_trace_agent;
+  if (!agent || !agent.getCurrentContextId || !agent.getWriterProjectId) {
+    return null;
+  }
+
+  const traceId = agent.getCurrentContextId();
+  if (!traceId) {
+    return null;
+  }
+
+  const traceProjectId = agent.getWriterProjectId();
+  if (!traceProjectId) {
+    return null;
+  }
+
+  return `projects/${traceProjectId}/traces/${traceId}`;
+}
+
+export class LoggingBunyan extends Writable {
+  private logName: string;
+  private resource: MonitoredResource|undefined;
+  private serviceContext: ServiceContext|undefined;
+  private stackdriverLog:
+      StackdriverLog;  // TODO: add type for @google-cloud/logging
+  constructor(options: Options) {
+    options = options || {};
+    super({objectMode: true});
+    this.logName = options.logName || 'bunyan_log';
+    this.resource = options.resource;
+    this.serviceContext = options.serviceContext;
+    this.stackdriverLog = logging(options).log(this.logName, {
+      removeCircular: true,
+    });
+  }
+
+  /**
+   * Convenience method that Builds a bunyan stream object that you can put in
+   * the bunyan streams list.
+   */
+  stream(level: string|number): StreamResponse {
+    return {
+      level,
+      type: 'raw',
+      stream: this,
+    };
+  }
+
+  /**
+   * Format a bunyan record into a Stackdriver log entry.
+   */
+  private formatEntry_(record: string|BunyanLogRecord) {
+    if (typeof record === 'string') {
+      throw new Error(
+          '@google-cloud/logging-bunyan only works as a raw bunyan stream type.');
+    }
+    // Stackdriver Log Viewer picks up the summary line from the 'message' field
+    // of the payload. Unless the user has provided a 'message' property also,
+    // move the 'msg' to 'message'.
+    if (!record.message) {
+      // If this is an error, report the full stack trace. This allows
+      // Stackdriver Error Reporting to pick up errors automatically (for
+      // severity 'error' or higher). In this case we leave the 'msg' property
+      // intact.
+      // https://cloud.google.com/error-reporting/docs/formatting-error-messages
+      //
+      if (record.err && record.err.stack) {
+        record.message = record.err.stack;
+        record.serviceContext = this.serviceContext;
+      } else if (record.msg) {
+        // Simply rename `msg` to `message`.
+        record.message = record.msg;
+        delete record.msg;
+      }
+    }
+
+    const entryMetadata: StackdriverEntryMetadata = {
+      resource: this.resource,
+      timestamp: record.time,
+      // BUNYAN_TO_STACKDRIVER does not have index signature.
+      // tslint:disable-next-line:no-any
+      severity: (BUNYAN_TO_STACKDRIVER as any)[record.level as string],
+    };
+
+    // If the record contains a httpRequest property, provide it on the entry
+    // metadata. This allows Stackdriver to use request log formatting.
+    // https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#HttpRequest
+    // Note that the httpRequest field must properly validate as a HttpRequest
+    // proto message, or the log entry would be rejected by the API. We do no
+    // validation here.
+    if (record.httpRequest) {
+      entryMetadata.httpRequest = record.httpRequest;
+      delete record.httpRequest;
+    }
+
+    // record does not have index signature.
+    // tslint:disable-next-line:no-any
+    if ((record as any)[LOGGING_TRACE_KEY]) {
+      // tslint:disable-next-line:no-any
+      entryMetadata.trace = (record as any)[LOGGING_TRACE_KEY];
+      // tslint:disable-next-line:no-any
+      delete (record as any)[LOGGING_TRACE_KEY];
+    }
+
+    return this.stackdriverLog.entry(entryMetadata, record);
+  }
+
+  /**
+   * Intercept log entries as they are written so we can attempt to add the
+   * trace ID in the same continuation as the function that wrote the log,
+   * because the trace agent currently uses continuation local storage for the
+   * trace context.
+   *
+   * By the time the Writable stream buffer gets flushed and _write gets called
+   * we may well be in a different continuation.
+   */
+  write(record: BunyanLogRecord, callback?: Function): boolean;
+  write(record: BunyanLogRecord, encoding?: string, callback?: Function):
+      boolean;
+  // Writable.write used 'any' in function signature.
+  // tslint:disable-next-line:no-any
+  write(...args: any[]): boolean {
+    let record = args[0];
+    let encoding: string|null = null;
+    let callback: Function;
+    if (typeof args[1] === 'string') {
+      encoding = args[1];
+      callback = args[2];
+    } else {
+      callback = args[1];
+    }
+    record = Object.assign({}, record);
+    // record does not have index signature.
+    // tslint:disable-next-line:no-any
+    if (!(record as any)[LOGGING_TRACE_KEY]) {
+      const trace = getCurrentTraceFromAgent();
+      if (trace) {
+        // tslint:disable-next-line:no-any
+        (record as any)[LOGGING_TRACE_KEY] = trace;
+      }
+    }
+    if (encoding !== null) {
+      return super.write.call(this, record, encoding, callback);
+    } else {
+      return super.write.call(this, record, callback);
+    }
+  }
+
+  /**
+   * Relay a log entry to the logging agent. This is called by bunyan through
+   * Writable#write.
+   */
+  _write(record: BunyanLogRecord, encoding?: string, callback?: Function) {
+    const entry = this.formatEntry_(record);
+    this.stackdriverLog.write(entry, callback);
+  }
+
+  /**
+   * Relay an array of log entries to the logging agent. This is called by
+   * bunyan through Writable#write.
+   */
+  // Writable._write used 'any' in function signature.
+  _writev(
+      chunks: Array<{
+        // tslint:disable-next-line:no-any
+        chunk: any;
+        encoding: string;
+      }>,
+      callback?: Function) {
+    const entries = chunks.map((request: {
+                                 // tslint:disable-next-line:no-any
+                                 chunk: any; encoding: string;
+                               }) => {
+      return this.formatEntry_(request.chunk);
+    });
+
+    this.stackdriverLog.write(entries, callback);
+  }
+}
+
+module.exports.BUNYAN_TO_STACKDRIVER = BUNYAN_TO_STACKDRIVER;
+
+/**
+ * Value: `logging.googleapis.com/trace`
+ *
+ * @name LoggingBunyan.LOGGING_TRACE_KEY
+ * @type {string}
+ */
+module.exports.LOGGING_TRACE_KEY = LOGGING_TRACE_KEY;
---
+++ handwritten/nodejs-logging-bunyan/src/types.d.ts
@@ -0,0 +1,197 @@
+/*!
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare namespace NodeJS {
+  export interface Global {
+    _google_trace_agent: any;
+  }
+}
+
+interface Options {
+  /**
+   * The name of the log that will receive messages written to this bunyan
+   * stream. Default: `bunyan_Log`.
+   */
+  logName?: string,
+  /**
+   * The monitored resource that the log stream corresponds to. On Google Cloud
+   * Platform, this is detected automatically, but you may optionally specify a
+   * specific monitored resource. For more information, see the
+   * [official documentation]{@link https://cloud.google.com/logging/docs/api/reference/rest/v2/MonitoredResource}
+   */
+  resource?: MonitoredResource,
+  /**
+   * For logged errors, we provide this as the service context. For more
+   * information see [this guide]{@link https://cloud.google.com/error-reporting/docs/formatting-error-messages}
+   * and the [official documentation]{@link https://cloud.google.com/error-reporting/reference/rest/v1beta1/ServiceContext}.
+   */
+  serviceContext?: ServiceContext,
+  /**
+   * The project ID from the Google Cloud
+ *     Console, e.g. 'grape-spaceship-123'. We will also check the environment
+ *     variable `GCLOUD_PROJECT` for your project ID. If your app is running in
+ *     an environment which supports {@link https://cloud.google.com/docs/authentication/production#providing_credentials_to_your_application Application Default Credentials},
+ *     your project ID will be detected automatically.
+   */
+  projectId?: string,
+  /**
+   * Full path to the a .json, .pem, or .p12 key downloaded from the Google
+   * Cloud Console. If you provide a path to a JSON file, the `projectId` option
+   * above is not necessary. NOTE: .pem and .p12 require you to specify the
+   * `email` option as well.
+   */
+  keyFilename?: string,
+  /**
+   * Account email address. Required when using a .pem or .p12 keyFilename.
+   */
+  email?: string,
+  /**
+   * Credentials object.
+   */
+  credentials?: Credentials,
+  /**
+   * Automatically retry requests if the response is related to rate limits or
+   * certain intermittent server errors. We will exponentially backoff
+   * subsequent requests by default.
+   */
+  autoRetry?: boolean,
+  /**
+   * Maximum number of automatic retries attempted before returning the error.
+   */
+  maxRetries?: number,
+  /**
+   * Custom promise module to use instead of native Promises.
+   */
+  // TODO: address the correct type of promise.
+  promise?: {}
+}
+
+interface MonitoredResource {
+  type?: string;
+  labels?: {[key: string]: string};
+}
+
+interface ServiceContext {
+  /**
+   * An identifier of the service, such as the name of the executable, job, or
+   * Google App Engine service name.
+   */
+  service?: string;
+  /**
+   * Represents the version of the service.
+   */
+  version?: string;
+}
+
+interface StackdriverLog {
+  critical: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  debug: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  emergency: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  error: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  info: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  notice: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  warning: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  write: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  alert: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  entry: (metadata: {}, data: {}|string) => StackdriverEntry
+}interface Credentials {
+  client_email?: string;
+  private_key?: string;
+}
+
+interface StackdriverEntryMetadata {
+  resource?: MonitoredResource,
+  timestamp?: Date,
+  severity?: string, // figure out the correct type later
+  httpRequest?: HttpRequest,
+  trace?: {}
+}
+
+interface StackdriverLog {
+  critical: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  debug: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  emergency: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  error: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  info: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  notice: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  warning: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  write: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  alert: (entry: StackdriverEntry|StackdriverEntry[], options?: {}, callback?: (err: Error, apiResponse: {}) => void) => Promise<LogWriteResponse>,
+  entry: (metadata: {}, data: {}|string) => StackdriverEntry
+}
+
+interface StackdriverLogging {
+  Entry?: StackdriverEntry,
+  Log?: StackdriverLog,
+  Logging?: StackdriverLogging,
+  entry?: (resource?: MonitoredResource, data?: {message: string}|string) => StackdriverEntry
+  // define additional properties and methods.
+}
+
+interface StackdriverEntry {
+  constructor: (metadata?: StackdriverEntryMetadata, data?: {message: string}| string) => StackdriverEntry,
+  data?: StackdriverData|string,
+  metadata?: StackdriverEntryMetadata
+}
+
+interface StackdriverData {
+  serviceContext?: ServiceContext;
+  message?: string;
+  metadata?: Metadata;
+  pid?: string;
+  test?: {circular?: string};
+}
+
+interface Metadata {
+  stack?: string;
+  httpRequest?: HttpRequest;
+}
+
+type LogWriteResponse = {}[];
+
+interface HttpRequest {
+  requestMethod: string,
+  requestUrl: string,
+  requestSize: string,
+  status: number,
+  responseSize: string,
+  userAgent: string,
+  remoteIp: string,
+  serverIp: string,
+  referer: string,
+  latency: string,
+  cacheLookup: boolean,
+  cacheHit: boolean,
+  cacheValidatedWithOriginServer: boolean,
+  cacheFillBytes: string,
+  protocol: string,
+}
+
+interface BunyanLogRecord {
+  message?: string,
+  msg?: string,
+  err?: Error,
+  serviceContext?: ServiceContext,
+  level?: string,
+  time?: Date,
+  httpRequest?: HttpRequest
+}
+
+interface StreamResponse {
+  level: string|number,
+  type: string,
+  stream: {}
+}
