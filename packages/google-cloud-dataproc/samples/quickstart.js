0fe72b26d4958e2e64195062926e79c55f24da8c
Jason Dobry <jason.dobry@gmail.com>
2018-03-16 13:01:10 -0700

    Upgrade repo-tools and regenerate scaffolding.

--- packages/google-cloud-dataproc/samples/quickstart.js
+++ packages/google-cloud-dataproc/samples/quickstart.js
@@ -1,19 +1,21 @@
-// Copyright 2017, Google LLC All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * Copyright 2017, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 'use strict';
 
+// [START dataproc_quickstart]
 if (
   !process.env.GCLOUD_PROJECT ||
   !process.env.GOOGLE_APPLICATION_CREDENTIALS
@@ -22,24 +24,24 @@ if (
     'Usage: GCLOUD_PROJECT=<project_id> GOOGLE_APPLICATION_CREDENTIALS=<path to json key> node #{$0}'
   );
 }
-var projectId = process.env.GCLOUD_PROJECT;
 
-const dataproc = require('../src');
+const dataproc = require('@google-cloud/dataproc');
 
-var client = new dataproc.v1.ClusterControllerClient({
+const client = new dataproc.v1.ClusterControllerClient({
   // optional auth parameters.
 });
 
+const projectId = process.env.GCLOUD_PROJECT;
+
 // Iterate over all elements.
-var projectId2 = projectId;
-var region = 'global';
-var request = {
-  projectId: projectId2,
+const region = 'global';
+const request = {
+  projectId: projectId,
   region: region,
 };
 
 client.listClusters(request).then(responses => {
-  var resources = responses[0];
+  const resources = responses[0];
   console.log('Total resources:', resources.length);
   for (let i = 0; i < resources.length; i += 1) {
     console.log(resources[i]);
@@ -47,14 +49,14 @@ client.listClusters(request).then(responses => {
 });
 
 // Or obtain the paged response.
-var options = {autoPaginate: false};
-var callback = responses => {
+const options = {autoPaginate: false};
+const callback = responses => {
   // The actual resources in a response.
-  var resources = responses[0];
+  const resources = responses[0];
   // The next request if the response shows that there are more responses.
-  var nextRequest = responses[1];
+  const nextRequest = responses[1];
   // The actual response object, if necessary.
-  // var rawResponse = responses[2];
+  // const rawResponse = responses[2];
   for (let i = 0; i < resources.length; i += 1) {
     console.log(resources[i]);
   }
@@ -68,3 +70,4 @@ client.listClusters(request, options).then(callback);
 client.listClustersStream(request).on('data', element => {
   console.log(element);
 });
+// [END dataproc_quickstart]
