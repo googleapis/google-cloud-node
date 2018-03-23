904948fd23d4c379e3ea3759b887c078fb67dc77
Rebecca Taylor <rebeccataylor@google.com>
2018-03-23 14:15:45 -0700

    Samples: add quickstart

---
+++ packages/google-cloud-texttospeech/samples/quickstart.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2018, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+// [START vision_quickstart]
+const fs = require('fs');
+
+// Imports the Google Cloud client library
+const textToSpeech = require('@google-cloud/text-to-speech');
+
+// Creates a client
+const client = new textToSpeech.TextToSpeechClient();
+
+// The text to synthesize
+const text = 'Hello, world!';
+
+// Construct the request
+const request = {
+  input: { text: text },
+  // Select the language and SSML Voice Gender (optional)
+  voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
+  // Select the type of audio encoding
+  audioConfig: { audioEncoding: 'MP3' }
+};
+
+// Performs the Text-to-Speech request
+client.synthesizeSpeech(request)
+  .then(results => {
+    // The binary audio content returned from the API
+    const audioContent = results[0].audioContent;
+
+    // Write the audio content to a local file
+    fs.writeFileSync('output.mp3', audioContent, 'binary');
+    console.log('Audio content written to file: output.mp3');
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
+// [END vision_quickstart]
