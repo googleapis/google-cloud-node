ca9de9c8d2235394ee8d1c4fcc75fb26e2b941d7
Dominic Kramer <DominicDKramer@google.com>
2018-03-23 14:51:53 -0700

    chore: convert test servers to TypeScript (#87)

--- handwritten/nodejs-error-reporting/test/test-servers/manual_scaffold_server.ts
+++ handwritten/nodejs-error-reporting/test/test-servers/manual_scaffold_server.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-'use strict';
 
 const errors = require('../../src/index.js')();
 errors.report('Sample test string', err => {
