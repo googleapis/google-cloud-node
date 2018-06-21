03f3751d98f083ccc33b788faa18d225aa0f3e5e
Justin Beckwith <justin.beckwith@gmail.com>
2018-06-21 16:42:08 -0700

    chore(package): update to the latest gts and typescript (#100)

--- handwritten/nodejs-logging-winston/system-test/logging-winston.ts
+++ handwritten/nodejs-logging-winston/system-test/logging-winston.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import * as assert from 'assert';
+import assert from 'assert';
 import * as winston from 'winston';
 
 import * as types from '../src/types/core';
