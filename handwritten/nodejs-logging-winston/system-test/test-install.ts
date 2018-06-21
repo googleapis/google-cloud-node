b82d4a17b8c878c4a55b63377de10c9a3306c16c
Dominic Kramer <DominicDKramer@google.com>
2018-06-21 13:12:32 -0700

    fix: fix broken install tests (#96)
    The tests were failing because they were testing the library
    against Winston@3 that is not supported.  Now the tests are
    testing against Winston@2.
    
    fixes: #94

--- handwritten/nodejs-logging-winston/system-test/test-install.ts
+++ handwritten/nodejs-logging-winston/system-test/test-install.ts
@@ -14,24 +14,22 @@
  * limitations under the License.
  */
 
-import * as assert from 'assert';
-import * as path from 'path';
+import * as check from 'post-install-check';
 
-import {globP, mkdirP, ncpP, rimrafP, spawnP, tmpDirP, writeFileP} from './utils';
-
-const INDEX_TS = 'index.ts';
-const INDEX_JS = 'index.js';
-
-const TS_CODE_ARRAY: CodeSample[] = [
+const TS_CODE_SAMPLES: check.CodeSample[] = [
   {
     code: `import * as loggingWinston from '@google-cloud/logging-winston';
 new loggingWinston.LoggingWinston();`,
-    description: 'imports the module using * syntax'
+    description: 'imports the module using * syntax',
+    dependencies: ['winston@2'],
+    devDependencies: ['@types/winston@2']
   },
   {
     code: `import {LoggingWinston} from '@google-cloud/logging-winston';
 new LoggingWinston();`,
-    description: 'imports the module with {} syntax'
+    description: 'imports the module with {} syntax',
+    dependencies: ['winston@2'],
+    devDependencies: ['@types/winston@2']
   },
   {
     code: `import {LoggingWinston} from '@google-cloud/logging-winston';
@@ -40,7 +38,10 @@ new LoggingWinston({
     service: 'some service'
   }
 });`,
-    description: 'imports the module and starts with a partial `serviceContext`'
+    description:
+        'imports the module and starts with a partial `serviceContext`',
+    dependencies: ['winston@2'],
+    devDependencies: ['@types/winston@2']
   },
   {
     code: `import {LoggingWinston} from '@google-cloud/logging-winston';
@@ -52,7 +53,9 @@ new LoggingWinston({
   }
 });`,
     description:
-        'imports the module and starts with a complete `serviceContext`'
+        'imports the module and starts with a complete `serviceContext`',
+    dependencies: ['winston@2'],
+    devDependencies: ['@types/winston@2']
   },
   {
     code: `import {LoggingWinston} from '@google-cloud/logging-winston';
@@ -64,16 +67,20 @@ new LoggingWinston({
     version: 'some-version'
   }
 });`,
-    description: 'imports the module with a prefix and labels specified'
+    description: 'imports the module with a prefix and labels specified',
+    dependencies: ['winston@2'],
+    devDependencies: ['@types/winston@2']
   }
 ];
 
-const JS_CODE_ARRAY: CodeSample[] = [
+const JS_CODE_SAMPLES: check.CodeSample[] = [
   {
     code:
         `const LoggingWinston = require('@google-cloud/logging-winston').LoggingWinston;
 new LoggingWinston();`,
-    description: 'requires the module using Node 4+ syntax'
+    description: 'requires the module using Node 4+ syntax',
+    dependencies: ['winston@2'],
+    devDependencies: []
   },
   {
     code:
@@ -84,7 +91,9 @@ new LoggingWinston({
   }
 });`,
     description:
-        'requires the module and starts with a partial `serviceContext`'
+        'requires the module and starts with a partial `serviceContext`',
+    dependencies: ['winston@2'],
+    devDependencies: []
   },
   {
     code:
@@ -97,7 +106,9 @@ new LoggingWinston({
   }
 });`,
     description:
-        'requires the module and starts with a complete `serviceContext`'
+        'requires the module and starts with a complete `serviceContext`',
+    dependencies: ['winston@2'],
+    devDependencies: []
   },
   {
     code:
@@ -110,112 +121,11 @@ new LoggingWinston({
     version: 'some-version'
   }
 });`,
-    description: 'imports the module with a prefix and labels specified'
+    description: 'imports the module with a prefix and labels specified',
+    dependencies: ['winston@2'],
+    devDependencies: []
   }
 ];
 
-const TIMEOUT_MS = 2 * 60 * 1000;
-
-const DEBUG = false;
-function log(txt: string): void {
-  if (DEBUG) {
-    console.log(txt);
-  }
-}
-
-const stdio = DEBUG ? 'inherit' : 'ignore';
-
-interface CodeSample {
-  code: string;
-  description: string;
-}
-
-describe('Installation', () => {
-  let installDir: string|undefined;
-  before(async () => {
-    const tgz = await globP(`${process.cwd()}/*.tgz`);
-    assert.deepStrictEqual(
-        tgz.length, 0,
-        `Expected zero tgz files in the current working directory before ` +
-            `running the test but found files: ${tgz.map(file => {
-              const parts = file.split(path.sep);
-              return parts[parts.length - 1];
-            })}`);
-  });
-
-  beforeEach(async function() {
-    this.timeout(TIMEOUT_MS);
-    // This script assumes that you don't already have a TGZ file
-    // in your current working directory.
-    installDir = await tmpDirP();
-    log(`Using installation directory: ${installDir}`);
-    await spawnP('npm', ['install'], {stdio}, log);
-    await spawnP('npm', ['run', 'compile'], {stdio}, log);
-    await spawnP('npm', ['pack'], {stdio}, log);
-    const tgz = await globP(`${process.cwd()}/*.tgz`);
-    if (tgz.length !== 1) {
-      throw new Error(
-          `Expected 1 tgz file in current directory, but found ${tgz.length}`);
-    }
-    await spawnP('npm', ['init', '-y'], {cwd: installDir, stdio}, log);
-    await spawnP(
-        'npm', ['install', 'typescript', '@types/node', tgz[0]],
-        {cwd: installDir, stdio}, log);
-  });
-
-  afterEach(async function() {
-    this.timeout(TIMEOUT_MS);
-    if (installDir) {
-      await rimrafP(installDir);
-    }
-  });
-
-  describe('When used with Typescript code', () => {
-    TS_CODE_ARRAY.forEach((sample) => {
-      it(`should install and work with code that ${sample.description}`,
-         async function() {
-           this.timeout(TIMEOUT_MS);
-           assert(installDir);
-           const srcDir = path.join(installDir!, 'src');
-           await mkdirP(srcDir);
-           await spawnP(
-               'npm', ['install', '--save', 'winston'],
-               {cwd: installDir, stdio}, log);
-           await spawnP(
-               'npm', ['install', '--save-dev', '@types/winston'],
-               {cwd: installDir, stdio}, log);
-           await writeFileP(path.join(srcDir, INDEX_TS), sample.code, 'utf-8');
-           await spawnP(
-               'npm', ['install', '--save-dev', 'gts', 'typescript@2.x'],
-               {cwd: installDir, stdio}, log);
-           await spawnP(
-               'gts', ['init', '--yes'], {cwd: installDir, stdio}, log);
-           await spawnP(
-               'npm', ['run', 'compile'], {cwd: installDir, stdio}, log);
-           const buildDir = path.join(installDir!, 'build');
-           await spawnP(
-               'node', [path.join(buildDir, 'src', INDEX_JS)],
-               {cwd: installDir, stdio}, log);
-         });
-    });
-  });
-
-  describe('When used with Javascript code', () => {
-    JS_CODE_ARRAY.forEach((sample) => {
-      it(`should install and work with code that ${sample.description}`,
-         async function() {
-           this.timeout(TIMEOUT_MS);
-           assert(installDir);
-           await spawnP(
-               'npm', ['install', '--save', 'winston'],
-               {cwd: installDir, stdio}, log);
-           await spawnP(
-               'npm', ['install', '--save-dev', '@types/winston'],
-               {cwd: installDir, stdio}, log);
-           await writeFileP(
-               path.join(installDir!, INDEX_JS), sample.code, 'utf-8');
-           await spawnP('node', [INDEX_JS], {cwd: installDir, stdio}, log);
-         });
-    });
-  });
-});
+check.testInstallation(
+    TS_CODE_SAMPLES, JS_CODE_SAMPLES, {timeout: 2 * 60 * 1000});
