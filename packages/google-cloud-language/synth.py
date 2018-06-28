8069036870f5adeeb0a275e30d4de45ba08dc14d
Christopher Wilcox <crwilcox@google.com>
2018-06-27 19:26:55 -0700

    update google-gax and add synth.py (#64)
    * update google-gax and add synth.py
    
    * don't exclude version specific indexes

---
+++ packages/google-cloud-language/synth.py
@@ -0,0 +1,25 @@
+import synthtool as s
+import synthtool.gcp as gcp
+import logging
+import subprocess
+
+logging.basicConfig(level=logging.DEBUG)
+
+gapic = gcp.GAPICGenerator()
+
+# tasks has two product names, and a poorly named artman yaml
+for version in ['v1', 'v1beta2']:
+    library = gapic.node_library(
+        'language', version)
+
+    # skip index, protos, package.json, and README.md
+    s.copy(
+        library,
+        excludes=['package.json', 'README.md', 'src/index.js'])
+
+#
+# Node.js specific cleanup
+#
+subprocess.run(['npm', 'install'])
+subprocess.run(['npm', 'run', 'prettier'])
+subprocess.run(['npm', 'run', 'lint'])
