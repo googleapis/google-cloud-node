8c42310bd88f0ec01bb0de0c1a3ecd0915d6210e
Justin Beckwith <justin.beckwith@gmail.com>
2018-07-10 13:47:31 -0700

    chore(build): use `npm ci` instead of `npm install` (#42)

--- packages/google-cloud-dataproc/synth.py
+++ packages/google-cloud-dataproc/synth.py
@@ -24,12 +24,7 @@ version = 'v1'
 library = gapic.node_library('dataproc', version)
 s.copy(library, excludes=['README.md', 'package.json'])
 
-'''
-Node.js specific cleanup
-'''
-# Repo Cleanup/Setup
-subprocess.run(['npm', 'install'])
-
-# prettify and lint
+# Node.js specific cleanup
+subprocess.run(['npm', 'ci'])
 subprocess.run(['npm', 'run', 'prettier'])
 subprocess.run(['npm', 'run', 'lint'])
