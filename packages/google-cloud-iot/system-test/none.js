25fde1f31327d747f08bd1475985c0ba7f2534b9
Chris Wilcox <crwilcox@google.com>
2018-07-11 11:21:51 -0700

    add placeholder system test so lint does not complain

---
+++ packages/google-cloud-iot/system-test/none.js
@@ -0,0 +1,19 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+// This file is here so lint doesn't fail due to no system tests. This
+// file can be deleted once we have system tests.
+
