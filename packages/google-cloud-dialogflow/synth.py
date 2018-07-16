ffc1cf6249e84e1c6cfe56822d27cadde57617f9
Jonathan Lui <jonathanlui@google.com>
2018-07-16 15:10:32 -0700

    chore(build): synth.py: npm ci; node templates
    * chore(build): use `npm ci` instead of `npm install`
    
    * synth: add src/index.js to excludes
    
    * chore: add nodejs templates to synth.py
    
    * chore: use synth.py generated .circleci/config.yml

--- packages/google-cloud-dialogflow/synth.py
+++ packages/google-cloud-dialogflow/synth.py
@@ -23,23 +23,22 @@ import subprocess
 logging.basicConfig(level=logging.DEBUG)
 
 gapic = gcp.GAPICGenerator()
+# create common templates by gapic
+common_templates = gcp.CommonTemplates()
 
 versions = ['v2', 'v2beta1']
 for version in versions:
     library = gapic.node_library('dialogflow', version)
-    s.copy(library / 'protos')
-    s.copy(library / 'src' / version)
-    s.copy(library / 'test')
+
+s.copy(library, excludes=['package.json', 'README.md', 'src/index.js'])
+
+templates = common_templates.node_library(package_name="dialogflow")
+s.copy(templates)
 
 '''
 Node.js specific cleanup
 '''
-# Repo Cleanup/Setup
-subprocess.run(['npm', 'install'])
+subprocess.run(['npm', 'ci'])
 
-# Generates scaffolding, enters contributors names
-subprocess.run(['npm', 'run', 'generate-scaffolding'])
-
-# prettify and lint
 subprocess.run(['npm', 'run', 'prettier'])
 subprocess.run(['npm', 'run', 'lint'])
