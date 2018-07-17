720e3d16521b12e32cfa59f59fb9ba957efe6f1f
Ali Ijaz Sheikh <ofrobots@google.com>
2018-07-16 18:01:36 -0700

    feat: request-correlating middleware (#63)
    This change introduces a mechanism to export middleware – only express
    added at this point – others are possible in the future.
    
    The middleware makes it simple to start logging from express apps
    without having to deal with boilerplate to initialize bunyan and the
    stackdriver stream separately.
    
    Furthermore, the middleware exposes a `log` API on the `request` object
    that can do context-correlation. All log entries corresponding to a
    specific request will be bundled together.

---
+++ handwritten/nodejs-logging-bunyan/src/middleware/express.ts
@@ -0,0 +1,124 @@
+/*!
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as context from '@opencensus/propagation-stackdriver';
+import * as bunyan from 'bunyan';
+import {NextFunction, Request, Response} from 'express';
+
+import {LOGGING_TRACE_KEY, LoggingBunyan} from '../index';
+import * as types from '../types/core';
+
+export type LogObject = {
+  // tslint:disable-next-line:no-any bunyan interface.
+  [level in bunyan.LogLevelString]: (...args: any[]) => void;
+};
+
+export interface AnnotatedRequest extends Request {
+  log: LogObject;
+}
+
+function makeLogFunction(
+    level: bunyan.LogLevelString,
+    logger: ReturnType<typeof bunyan.createLogger>, trace: string|null) {
+  // tslint:disable-next-line:no-any bunyan interface.
+  return (...args: any[]) => {
+    let fields;
+    if (args[0] instanceof Error) {
+      // logger.info(err, ...);
+      fields = {err: args[0]};
+      args[0] = fields;
+    } else if (typeof args[0] !== 'object' || Array.isArray(args[0])) {
+      // logger.info(msg, ...);
+      fields = {};
+      args.unshift(fields);
+    } else {
+      // logger.info({...}, msg, ....);
+      fields = args[0];
+    }
+    if (trace) {
+      fields[LOGGING_TRACE_KEY] = trace;
+    }
+    return logger[level].apply(logger, args);
+  };
+}
+
+function makeLogObject(
+    logger: ReturnType<typeof bunyan.createLogger>,
+    trace: string|null): LogObject {
+  const log = {} as LogObject;
+  const levels: bunyan.LogLevelString[] =
+      ['trace', 'debug', 'info', 'warn', 'error', 'fatal'];
+  levels.forEach((level: bunyan.LogLevelString) => {
+    log[level] = makeLogFunction(level, logger, trace);
+  });
+  return log;
+}
+
+export interface MiddlewareOptions extends types.Options {
+  level?: types.LogLevel;
+}
+
+export interface MiddlewareReturnType {
+  logger: ReturnType<typeof bunyan.createLogger>;
+  // tslint:disable-next-line:no-any express middleware.
+  mw: (req: Request, res: Response, next: NextFunction) => any;
+}
+
+/**
+ * Express middleware
+ */
+export async function middleware(options?: MiddlewareOptions):
+    Promise<MiddlewareReturnType> {
+  const defaultOptions = {logName: 'bunyan_log', level: 'info'};
+  options = Object.assign({}, defaultOptions, options);
+
+  const bunyan = require('bunyan');
+  const loggingBunyan = new LoggingBunyan(options);
+  const projectId =
+      await loggingBunyan.stackdriverLog.logging.auth.getProjectId();
+  // Failure to acquire projectId from auth would throw to the user.
+
+  const logger = bunyan.createLogger({
+    name: options.logName,
+    streams: [loggingBunyan.stream(options.level as types.LogLevel)]
+  });
+
+  return {
+    logger,
+    mw: (req: Request, res: Response, next: NextFunction) => {
+      const wrapper = {
+        setHeader(name: string, value: string) {
+          req.headers[name] = value;
+        },
+        getHeader(name: string) {
+          return req.headers[name];
+        }
+      };
+
+      let spanContext = context.extract(wrapper);
+      if (!spanContext) {
+        // We were the first actor to detect lack of context. Establish context.
+        spanContext = context.generate();
+        context.inject(wrapper, spanContext);
+      }
+
+      const trace = `projects/${projectId}/traces/${spanContext.traceId}`;
+
+      (req as AnnotatedRequest).log = makeLogObject(logger, trace);
+      next();
+    }
+  };
+}
---
+++ handwritten/nodejs-logging-bunyan/system-test/test-middleware-express.ts
@@ -0,0 +1,78 @@
+/*!
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* AN EXAMPLE RATHER THAN A TEST AT THIS POINT */
+
+import * as assert from 'assert';
+import * as delay from 'delay';
+
+import {express as elb} from '../src/index';
+
+const logging = require('@google-cloud/logging')();
+
+const WRITE_CONSISTENCY_DELAY_MS = 20 * 1000;
+const TEST_TIMEOUT = WRITE_CONSISTENCY_DELAY_MS + (10 * 1000);
+
+describe('express middleware', () => {
+  let logger: elb.MiddlewareReturnType['logger'];
+  let mw: elb.MiddlewareReturnType['mw'];
+
+  before(async () => {
+    ({logger, mw} = await elb.middleware({level: 'info'}));
+  });
+
+  describe('global logger', () => {
+    it('should properly write log entries', async () => {
+      const LOG_MESSAGE = 'test log message';
+      logger.info(LOG_MESSAGE);
+
+      await delay(WRITE_CONSISTENCY_DELAY_MS);
+
+      const log = logging.log('bunyan_log');
+      const entries = (await log.getEntries({pageSize: 1}))[0];
+      assert.strictEqual(entries.length, 1);
+      assert.strictEqual(LOG_MESSAGE, entries[0].data.message);
+    }).timeout(TEST_TIMEOUT);
+  });
+
+  describe('request logging middleware', () => {
+    it('should write request correlated log entries', (done) => {
+      const LOG_MESSAGE = 'test request log message';
+      const fakeRequest = {headers: {fake: 'header'}};
+      const fakeResponse = {};
+      const next = async () => {
+        // At this point fakeRequest.log should have been installed.
+        // tslint:disable-next-line:no-any
+        (fakeRequest as any as elb.AnnotatedRequest).log.info(LOG_MESSAGE);
+
+        await delay(WRITE_CONSISTENCY_DELAY_MS);
+
+        const log = logging.log('bunyan_log');
+        const entries = (await log.getEntries({pageSize: 1}))[0];
+        assert.strictEqual(entries.length, 1);
+        const entry = entries[0];
+        assert.strictEqual(LOG_MESSAGE, entry.data.message);
+        assert(entry.metadata.trace, 'should have a trace property');
+        assert(entry.metadata.trace.match(/projects\/.*\/traces\/.*/));
+        done();
+      };
+
+      // Call middleware with mocks.
+      // tslint:disable-next-line:no-any
+      mw(fakeRequest as any, fakeResponse as any, next);
+    }).timeout(TEST_TIMEOUT);
+  });
+});
