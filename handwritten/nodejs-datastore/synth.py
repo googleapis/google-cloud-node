c7cae68ff40c8b14860e3d8fd1e7e7e978ccad2c
Jonathan Lui <jonathanlui@google.com>
2018-07-27 05:32:39 -0700

    chore: add node templates to synth.py (#143)

--- handwritten/nodejs-datastore/synth.py
+++ handwritten/nodejs-datastore/synth.py
@@ -7,6 +7,7 @@ import subprocess
 logging.basicConfig(level=logging.DEBUG)
 
 gapic = gcp.GAPICGenerator()
+common_templates = gcp.CommonTemplates()
 
 # tasks has two product names, and a poorly named artman yaml
 version = 'v1'
@@ -18,6 +19,10 @@ library = gapic.node_library(
 s.copy(
     library,
     excludes=['package.json', 'README.md', 'src/index.js'])
+
+templates = common_templates.node_library(package_name="@google-cloud/datastore")
+s.copy(templates)
+
 
 # Node.js specific cleanup
 subprocess.run(['npm', 'ci'])
