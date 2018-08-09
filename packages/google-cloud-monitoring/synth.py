4dccd013d10615cb169fabd2c606dd444aee78de
Justin Beckwith <justin.beckwith@gmail.com>
2018-08-09 14:42:52 -0700

    chore: do not use npm ci (#94)

--- packages/google-cloud-monitoring/synth.py
+++ packages/google-cloud-monitoring/synth.py
@@ -22,6 +22,6 @@ s.copy(
 )
 
 # Node.js specific cleanup
-subprocess.run(['npm', 'ci'])
+subprocess.run(['npm', 'install'])
 subprocess.run(['npm', 'run', 'prettier'])
 subprocess.run(['npm', 'run', 'lint'])
