1fc45a68f2a5f39d5dc1dd118dc930d936689dae
Justin Beckwith <justin.beckwith@gmail.com>
2018-08-09 14:43:20 -0700

    chore: do not use npm ci (#71)

--- packages/google-cloud-bigquery-datatransfer/synth.py
+++ packages/google-cloud-bigquery-datatransfer/synth.py
@@ -22,6 +22,6 @@ s.copy(
     excludes=['package.json', 'README.md', 'src/index.js',
               f'src/{version}/index.js'])
 # Node.js specific cleanup
-subprocess.run(['npm', 'ci'])
+subprocess.run(['npm', 'install'])
 subprocess.run(['npm', 'run', 'prettier'])
 subprocess.run(['npm', 'run', 'lint'])
