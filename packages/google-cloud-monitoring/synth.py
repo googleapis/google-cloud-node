f6f73d0cb0a863ef3820db4578dc48d373c5c3b6
Jonathan Lui <jonathanlui@google.com>
2018-08-15 15:54:48 -0700

    build: node templates (#101)
    * build: add node_templates to synth.py
    
    * npm ci => npm install

--- packages/google-cloud-monitoring/synth.py
+++ packages/google-cloud-monitoring/synth.py
@@ -1,27 +1,45 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This script is used to synthesize generated parts of this library."""
+
 import synthtool as s
 import synthtool.gcp as gcp
-import logging
-from pathlib import Path
 import subprocess
+import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
 gapic = gcp.GAPICGenerator()
+common_templates = gcp.CommonTemplates()
 
-version = 'v3'
+version = "v3"
 
-library = gapic.node_library(
-    'monitoring', version,
-    config_path="/google/monitoring/"
-                "artman_monitoring.yaml")
+library = gapic.node_library("monitoring", version,
+        config_path="/google/monitoring/artman_monitoring.yaml")
 
-# skip index, protos, package.json, and README.md
-s.copy(
-    library,
-    excludes=['package.json', 'README.md', 'src/index.js'],
-)
+s.copy(library, excludes=["src/index.js", "README.md", "package.json"])
 
-# Node.js specific cleanup
-subprocess.run(['npm', 'install'])
-subprocess.run(['npm', 'run', 'prettier'])
-subprocess.run(['npm', 'run', 'lint'])
+templates = common_templates.node_library(
+    package_name="@google-cloud/monitoring",
+    repo_name="googleapis/nodejs-monitoring")
+
+s.copy(templates)
+
+"""
+Node.js specific cleanup
+"""
+subprocess.run(["npm", "install"])
+subprocess.run(["npm", "run", "prettier"])
+subprocess.run(["npm", "run", "lint"])
