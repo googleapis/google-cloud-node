120bded86281fc88170b9b57c31b7950f8d71227
Justin Beckwith <justin.beckwith@gmail.com>
2018-09-05 12:52:04 -0700

    Add synth script and update CI (#149)

---
+++ packages/google-cloud-compute/synth.py
@@ -0,0 +1,8 @@
+import synthtool as s
+import synthtool.gcp as gcp
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+common_templates = gcp.CommonTemplates()
+templates = common_templates.node_library()
+s.copy(templates)
