3f5fc6e9564d9e5cb78e201a494df0953c2ec2d2
Justin Beckwith <justin.beckwith@gmail.com>
2018-09-05 10:17:20 -0700

    Remove unused dependencies (#44)

--- packages/google-cloud-automl/synth.py
+++ packages/google-cloud-automl/synth.py
@@ -20,18 +20,15 @@ import subprocess
 logging.basicConfig(level=logging.DEBUG)
 
 gapic = gcp.GAPICGenerator()
-common_templates = gcp.CommonTemplates()
-
 versions = ['v1beta1']
-
 for version in versions:
     library = gapic.node_library('automl', version)
     s.copy(library, excludes=['src/index.js', 'README.md', 'package.json'])
 
-templates = common_templates.node_library(package_name="@google-cloud/automl")
+common_templates = gcp.CommonTemplates()
+templates = common_templates.node_library()
 s.copy(templates)
 
 # Node.js specific cleanup
 subprocess.run(['npm', 'install'])
 subprocess.run(['npm', 'run', 'prettier'])
-subprocess.run(['npm', 'run', 'lint'])
