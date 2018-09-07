320eb7209172cdff7c7fea465f8090bb41471118
Justin Beckwith <justin.beckwith@gmail.com>
2018-09-07 13:56:05 -0700

    Update all dependencies (#45)

--- packages/google-cloud-redis/samples/system-test/redis.js
+++ packages/google-cloud-redis/samples/system-test/redis.js
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-console.warn('no samples available 😐');
+console.warn('no system tests available 😐');
