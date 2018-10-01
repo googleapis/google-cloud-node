3445582cb8916264a6c000e0d0955fa4a5a1c1d7
Jonathan Lui <jonathanlui@google.com>
2018-10-01 13:55:04 -0700

    build: samples test by adding approprate test variables (#423)
    * share environment variabes between system and samples test
    
    * remove legacy kokoro build configs
    
    * fix script

--- handwritten/storage/.kokoro/setup-vars.sh
+++ handwritten/storage/.kokoro/setup-vars.sh
@@ -14,4 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-. .kokoro/setup-vars.sh
+# nodejs-storage's system tests require additional project and
+# system test key
+export GCN_STORAGE_2ND_PROJECT_ID=gcloud-node-whitelist-ci-tests
+export GCN_STORAGE_2ND_PROJECT_KEY=${KOKORO_GFILE_DIR}/no-whitelist-key.json
+
+export GOOGLE_CLOUD_KMS_KEY_ASIA="projects/long-door-651/locations/asia/keyRings/test-key-asia/cryptoKeys/test-key-asia"
+export GOOGLE_CLOUD_KMS_KEY_US="projects/long-door-651/locations/us/keyRings/test-key-us/cryptoKeys/test-key-us"
