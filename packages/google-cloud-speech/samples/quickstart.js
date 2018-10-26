996b9617579e83170d58a4ca983406a3fe773d9d
vijay-qlogic <36055624+vijay-qlogic@users.noreply.github.com>
2018-10-26 18:31:02 +0530

    docs(samples): updated samples code to use async await (#214)

--- packages/google-cloud-speech/samples/quickstart.js
+++ packages/google-cloud-speech/samples/quickstart.js
@@ -16,45 +16,41 @@
 'use strict';
 
 // [START speech_quickstart]
-// Imports the Google Cloud client library
-const speech = require('@google-cloud/speech');
-const fs = require('fs');
+async function main() {
+  // Imports the Google Cloud client library
+  const speech = require('@google-cloud/speech');
+  const fs = require('fs');
 
-// Creates a client
-const client = new speech.SpeechClient();
+  // Creates a client
+  const client = new speech.SpeechClient();
 
-// The name of the audio file to transcribe
-const fileName = './resources/audio.raw';
+  // The name of the audio file to transcribe
+  const fileName = './resources/audio.raw';
 
-// Reads a local audio file and converts it to base64
-const file = fs.readFileSync(fileName);
-const audioBytes = file.toString('base64');
+  // Reads a local audio file and converts it to base64
+  const file = fs.readFileSync(fileName);
+  const audioBytes = file.toString('base64');
 
-// The audio file's encoding, sample rate in hertz, and BCP-47 language code
-const audio = {
-  content: audioBytes,
-};
-const config = {
-  encoding: 'LINEAR16',
-  sampleRateHertz: 16000,
-  languageCode: 'en-US',
-};
-const request = {
-  audio: audio,
-  config: config,
-};
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US',
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
 
-// Detects speech in the audio file
-client
-  .recognize(request)
-  .then(data => {
-    const response = data[0];
-    const transcription = response.results
-      .map(result => result.alternatives[0].transcript)
-      .join('\n');
-    console.log(`Transcription: ${transcription}`);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
+  // Detects speech in the audio file
+  const [response] = await client.recognize(request);
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+  console.log(`Transcription: ${transcription}`);
+}
+main().catch(console.error);
 // [END speech_quickstart]
