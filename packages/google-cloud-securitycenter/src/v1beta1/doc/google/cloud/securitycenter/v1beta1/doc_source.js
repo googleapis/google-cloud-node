dd7dafe82db68391746a2ad1cd79c5f9f2ae0c4c
Jonathan Lui <jonathanlui@google.com>
2018-11-05 19:23:35 -0800

    initial commit

---
+++ packages/google-cloud-securitycenter/src/v1beta1/doc/google/cloud/securitycenter/v1beta1/doc_source.js
@@ -0,0 +1,53 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Note: this file is purely for documentation. Any contents are not expected
+// to be loaded as the JS file.
+
+/**
+ * Security Center's finding source. A finding source is an entity or a
+ * mechanism that can produce a finding. A source can also be thought of as a
+ * container of findings that come from the same scanner, logger, monitor, etc.
+ *
+ * @property {string} name
+ *   The relative resource name of this source. See:
+ *   https://cloud.google.com/apis/design/resource_names#relative_resource_name
+ *   Example:
+ *   "organizations/123/sources/456"
+ *
+ * @property {string} displayName
+ *   The source’s display name.
+ *   A source’s display name must be unique amongst its siblings, e.g.
+ *   no two sources with the same parent can share the same display name.
+ *   The display name must start and end with a letter or digit, may contain
+ *   letters, digits, spaces, hyphens and underscores and can be no longer
+ *   than 30 characters. This is captured by the regular expression:
+ *   [\p{L}\p{N}](https://cloud.google.com{\p{L}\p{N}_- ]{0,28}[\p{L}\p{N}])?.
+ *
+ * @property {string} description
+ *   The description of the source (max of 1024 characters).
+ *   Example:
+ *   "Cloud Security Scanner is a web security scanner for common
+ *   vulnerabilities in Google App Engine applications. It can automatically
+ *   scan and detect four common vulnerabilities, including cross-site-scripting
+ *   (XSS), Flash injection, mixed content (HTTP in HTTPS), and
+ *   outdated/insecure libraries."
+ *
+ * @typedef Source
+ * @memberof google.cloud.securitycenter.v1beta1
+ * @see [google.cloud.securitycenter.v1beta1.Source definition in proto format]{@link https://github.com/googleapis/googleapis/blob/master/google/cloud/securitycenter/v1beta1/source.proto}
+ */
+const Source = {
+  // This is for documentation. Actual contents will be loaded by gRPC.
+};
