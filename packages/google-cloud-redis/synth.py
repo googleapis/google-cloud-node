60a22c072614de4fe104ff83be1a2d5787231ec7
Justin Beckwith <justin.beckwith@gmail.com>
2018-11-08 13:15:25 -0800

    refactor: trim a few deps (#82)

--- packages/google-cloud-redis/synth.py
+++ packages/google-cloud-redis/synth.py
@@ -21,4 +21,4 @@ templates = common_templates.node_library()
 s.copy(templates)
 
 subprocess.run(['npm', 'install'])
-subprocess.run(['npm', 'run', 'prettier'])
+subprocess.run(['npm', 'run', 'fix'])
