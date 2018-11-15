5a650d34ec0c59d3e180e47c84504d03b74f4d5a
praveenqlogic <44371467+praveenqlogic@users.noreply.github.com>
2018-11-16 00:20:45 +0530

    docs(samples): updated samples code to use async await (#118)

--- packages/google-cloud-texttospeech/samples/quickstart.js
+++ packages/google-cloud-texttospeech/samples/quickstart.js
@@ -15,41 +15,36 @@
 
 'use strict';
 
-// [START tts_quickstart]
-const fs = require('fs');
+async function main() {
+  // [START tts_quickstart]
+  const fs = require('fs');
+  const util = require('util');
 
-// Imports the Google Cloud client library
-const textToSpeech = require('@google-cloud/text-to-speech');
+  // Imports the Google Cloud client library
+  const textToSpeech = require('@google-cloud/text-to-speech');
 
-// Creates a client
-const client = new textToSpeech.TextToSpeechClient();
+  // Creates a client
+  const client = new textToSpeech.TextToSpeechClient();
 
-// The text to synthesize
-const text = 'Hello, world!';
+  // The text to synthesize
+  const text = 'Hello, world!';
 
-// Construct the request
-const request = {
-  input: {text: text},
-  // Select the language and SSML Voice Gender (optional)
-  voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
-  // Select the type of audio encoding
-  audioConfig: {audioEncoding: 'MP3'},
-};
+  // Construct the request
+  const request = {
+    input: {text: text},
+    // Select the language and SSML Voice Gender (optional)
+    voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+    // Select the type of audio encoding
+    audioConfig: {audioEncoding: 'MP3'},
+  };
 
-// Performs the Text-to-Speech request
-client.synthesizeSpeech(request, (err, response) => {
-  if (err) {
-    console.error('ERROR:', err);
-    return;
-  }
-
+  // Performs the Text-to-Speech request
+  const [response] = await client.synthesizeSpeech(request);
   // Write the binary audio content to a local file
-  fs.writeFile('output.mp3', response.audioContent, 'binary', err => {
-    if (err) {
-      console.error('ERROR:', err);
-      return;
-    }
-    console.log('Audio content written to file: output.mp3');
-  });
-});
-// [END tts_quickstart]
+  const writeFile = util.promisify(fs.writeFile);
+  await writeFile('output.mp3', response.audioContent, 'binary');
+  console.log('Audio content written to file: output.mp3');
+  // [END tts_quickstart]
+}
+
+main().catch(console.error);
