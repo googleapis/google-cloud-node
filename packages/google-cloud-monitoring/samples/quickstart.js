ba4ad10449e9173d6e958b516a088b7c3010999d
muraliQlogic <38901089+muraliQlogic@users.noreply.github.com>
2018-11-17 04:07:55 +0530

    docs(samples): convert samples to async/await, and use mocha for tests (#146)

--- packages/google-cloud-monitoring/samples/quickstart.js
+++ packages/google-cloud-monitoring/samples/quickstart.js
@@ -18,56 +18,53 @@
 // [START monitoring_quickstart]
 // Imports the Google Cloud client library
 const monitoring = require('@google-cloud/monitoring');
+async function quickStart() {
+  // Your Google Cloud Platform project ID
+  const projectId = 'YOUR_PROJECT_ID';
 
-// Your Google Cloud Platform project ID
-const projectId = 'YOUR_PROJECT_ID';
+  // Creates a client
+  const client = new monitoring.MetricServiceClient();
 
-// Creates a client
-const client = new monitoring.MetricServiceClient();
-
-// Prepares an individual data point
-const dataPoint = {
-  interval: {
-    endTime: {
-      seconds: Date.now() / 1000,
-    },
-  },
-  value: {
-    // The amount of sales
-    doubleValue: 123.45,
-  },
-};
-
-// Prepares the time series request
-const request = {
-  name: client.projectPath(projectId),
-  timeSeries: [
-    {
-      // Ties the data point to a custom metric
-      metric: {
-        type: 'custom.googleapis.com/stores/daily_sales',
-        labels: {
-          store_id: 'Pittsburgh',
-        },
+  // Prepares an individual data point
+  const dataPoint = {
+    interval: {
+      endTime: {
+        seconds: Date.now() / 1000,
       },
-      resource: {
-        type: 'global',
-        labels: {
-          project_id: projectId,
-        },
-      },
-      points: [dataPoint],
     },
-  ],
-};
+    value: {
+      // The amount of sales
+      doubleValue: 123.45,
+    },
+  };
 
-// Writes time series data
-client
-  .createTimeSeries(request)
-  .then(results => {
-    console.log(`Done writing time series data.`, results[0]);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
+  // Prepares the time series request
+  const request = {
+    name: client.projectPath(projectId),
+    timeSeries: [
+      {
+        // Ties the data point to a custom metric
+        metric: {
+          type: 'custom.googleapis.com/stores/daily_sales',
+          labels: {
+            store_id: 'Pittsburgh',
+          },
+        },
+        resource: {
+          type: 'global',
+          labels: {
+            project_id: projectId,
+          },
+        },
+        points: [dataPoint],
+      },
+    ],
+  };
+
+  // Writes time series data
+  const [result] = await client.createTimeSeries(request);
+  console.log(`Done writing time series data.`, result);
+}
+
+quickStart().catch(console.error);
 // [END monitoring_quickstart]
