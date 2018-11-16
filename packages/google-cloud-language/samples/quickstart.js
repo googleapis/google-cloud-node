d1fd1ad6e5b48fb307f0937d744037bcc5d5a77e
praveenqlogic <44371467+praveenqlogic@users.noreply.github.com>
2018-11-16 14:20:08 +0530

    docs(samples): updated samples code to use async await (#154)

--- packages/google-cloud-language/samples/quickstart.js
+++ packages/google-cloud-language/samples/quickstart.js
@@ -16,31 +16,29 @@
 'use strict';
 
 // [START language_quickstart]
-// Imports the Google Cloud client library
-const language = require('@google-cloud/language');
+async function main() {
+  // Imports the Google Cloud client library
+  const language = require('@google-cloud/language');
 
-// Instantiates a client
-const client = new language.LanguageServiceClient();
+  // Instantiates a client
+  const client = new language.LanguageServiceClient();
 
-// The text to analyze
-const text = 'Hello, world!';
+  // The text to analyze
+  const text = 'Hello, world!';
 
-const document = {
-  content: text,
-  type: 'PLAIN_TEXT',
-};
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };
 
-// Detects the sentiment of the text
-client
-  .analyzeSentiment({document: document})
-  .then(results => {
-    const sentiment = results[0].documentSentiment;
+  // Detects the sentiment of the text
+  const [result] = await client.analyzeSentiment({document: document});
+  const sentiment = result.documentSentiment;
 
-    console.log(`Text: ${text}`);
-    console.log(`Sentiment score: ${sentiment.score}`);
-    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
+  console.log(`Text: ${text}`);
+  console.log(`Sentiment score: ${sentiment.score}`);
+  console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+}
+
+main().catch(console.error);
 // [END language_quickstart]
