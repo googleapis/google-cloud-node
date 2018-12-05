6ba46853941e48c9ce1a8019b4356b52517031d0
Justin Beckwith <justin.beckwith@gmail.com>
2018-12-05 14:31:58 -0800

    refactor: reduce the number of dependencies (#206)

--- handwritten/nodejs-logging-bunyan/system-test/errors-transport.ts
+++ handwritten/nodejs-logging-bunyan/system-test/errors-transport.ts
@@ -16,7 +16,8 @@
 
 import * as common from '@google-cloud/common';
 import delay from 'delay';
-import * as request from 'request';
+import * as r from 'request';  // types only
+import {teenyRequest} from 'teeny-request';
 
 const packageJson = require('../../package.json');
 
@@ -48,7 +49,7 @@ const ONE_HOUR_API = 'timeRange.period=PERIOD_1_HOUR';
 export class ErrorsApiTransport extends common.Service {
   constructor() {
     super({
-      requestModule: request,
+      requestModule: teenyRequest as typeof r,
       baseUrl: 'https://clouderrorreporting.googleapis.com/v1beta1',
       scopes: ['https://www.googleapis.com/auth/cloud-platform'],
       packageJson
