13219f63e0fed6ecd7ccaedd6360696c25ef2636
Jonathan Lui <jonathanlui@google.com>
2018-12-14 14:22:13 -0800

    remove synth.py (#375)

--- handwritten/devtools-cloudprofiler/synth.py
+++
@@ -1,8 +0,0 @@
-import synthtool as s
-import synthtool.gcp as gcp
-import logging
-
-common_templates = gcp.CommonTemplates()
-logging.basicConfig(level=logging.DEBUG)
-templates = common_templates.node_library()
-s.copy(templates / ".kokoro")
