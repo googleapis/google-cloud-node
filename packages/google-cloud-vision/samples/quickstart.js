0ddf1d804586f7b772f151ae4b8d736482323e0f
Justin Beckwith <justin.beckwith@gmail.com>
2018-12-18 13:42:02 -0800

    chore: modernize the samples and sample tests (#297)

--- packages/google-cloud-vision/samples/quickstart.js
+++ packages/google-cloud-vision/samples/quickstart.js
@@ -16,20 +16,19 @@
 'use strict';
 
 // [START vision_quickstart]
-// Imports the Google Cloud client library
-const vision = require('@google-cloud/vision');
+async function quickstart() {
+  // Imports the Google Cloud client library
+  const vision = require('@google-cloud/vision');
 
-// Creates a client
-const client = new vision.ImageAnnotatorClient();
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
 
-// Performs label detection on the image file
-async function main() {
+  // Performs label detection on the image file
   const [result] = await client.labelDetection('./resources/wakeupcat.jpg');
   const labels = result.labelAnnotations;
   console.log('Labels:');
   labels.forEach(label => console.log(label.description));
 }
-main().catch(err => {
-  console.error('ERROR:', err);
-});
 // [END vision_quickstart]
+
+quickstart().catch(console.error);
