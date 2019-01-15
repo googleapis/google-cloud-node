4e7db975d5b0725b9ddeb38c37c481325866912d
Jonathan Lui <jonathanlui@google.com>
2019-01-14 19:20:05 -0800

    build: check broken links in generated docs (#239)

--- handwritten/nodejs-logging-bunyan/synth.py
+++ handwritten/nodejs-logging-bunyan/synth.py
@@ -18,5 +18,5 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library()
+templates = common_templates.node_library(source_location='build/src')
 s.copy(templates)
