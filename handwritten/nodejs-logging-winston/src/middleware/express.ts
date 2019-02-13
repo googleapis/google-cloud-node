4f3032233ddc8b35177efa8c061febd85529fdd7
Ali Ijaz Sheikh <ofrobots@google.com>
2019-02-13 11:49:48 -0800

    feature: request logging middleware for express (#182)

---
+++ handwritten/nodejs-logging-winston/src/middleware/express.ts
@@ -0,0 +1,80 @@
+/*!
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {HttpRequest, middleware as commonMiddleware} from '@google-cloud/logging';
+import {GCPEnv} from 'google-auth-library';
+import * as winston from 'winston';
+
+import {LOGGING_TRACE_KEY} from '../common';
+import {LoggingWinston} from '../index';
+import * as types from '../types/core';
+
+import {makeChildLogger} from './make-child-logger';
+
+export const APP_LOG_SUFFIX = 'applog';
+
+export interface MiddlewareOptions extends types.Options {
+  level?: string;
+  levels?: winston.config.AbstractConfigSetLevels;
+}
+
+export async function middleware(options?: MiddlewareOptions) {
+  const defaultOptions = {
+    logName: 'winston_log',
+    level: 'info',
+    levels: winston.config.npm.levels
+  };
+  options = Object.assign({}, defaultOptions, options);
+
+  const loggingWinstonApp = new LoggingWinston(
+      {...options, logName: `${options.logName}_${APP_LOG_SUFFIX}`});
+  const logger = winston.createLogger({
+    level: options.level,
+    levels: options.levels,
+    transports: [loggingWinstonApp]
+  });
+
+  const auth = loggingWinstonApp.common.stackdriverLog.logging.auth;
+  const [env, projectId] =
+      await Promise.all([auth.getEnv(), auth.getProjectId()]);
+
+  // Unless we are running on Google App Engine or Cloud Functions, generate a
+  // parent request log entry that all the request specific logs ("app logs")
+  // will nest under. GAE and GCF generate the parent request logs
+  // automatically.
+  let emitRequestLog;
+  if (env !== GCPEnv.APP_ENGINE && env !== GCPEnv.CLOUD_FUNCTIONS) {
+    const loggingWinstonReq = new LoggingWinston(options);
+    const requestLogger = winston.createLogger({
+      level: options.level,
+      levels: options.levels,
+      transports: [loggingWinstonReq]
+    });
+    emitRequestLog = (httpRequest: HttpRequest, trace: string) => {
+      requestLogger.info({[LOGGING_TRACE_KEY]: trace, httpRequest});
+    };
+  }
+
+  return {
+    logger,
+    mw: commonMiddleware.express.makeMiddleware(
+        projectId,
+        (trace: string) => {
+          return makeChildLogger(logger, trace);
+        },
+        emitRequestLog)
+  };
+}
---
+++ handwritten/nodejs-logging-winston/test/middleware/make-child-logger.ts
@@ -0,0 +1,71 @@
+import * as assert from 'assert';
+import * as winston from 'winston';
+import {LOGGING_TRACE_KEY} from '../../src/common';
+
+import {makeChildLogger} from '../../src/middleware/make-child-logger';
+
+describe('makeChildLogger', () => {
+  const FAKE_TRACE = '🤥';
+  const LOGGER = winston.createLogger(
+      {transports: [new winston.transports.Console({silent: true})]});
+  const origWrite = LOGGER.write;
+
+  afterEach(() => {
+    LOGGER.write = origWrite;
+  });
+
+  it('should return a winston-like logger', () => {
+    const child = makeChildLogger(LOGGER, FAKE_TRACE);
+    let logEntry: winston.LogEntry;
+    // tslint:disable-next-line:no-any
+    (LOGGER.write as any) = (logEntry_: winston.LogEntry) => {
+      logEntry = logEntry_;
+    };
+
+    child.info('hello');
+    assert.strictEqual(logEntry!.message, 'hello');
+    assert.strictEqual(logEntry!.level, 'info');
+
+    child.error('👾', {key: '🎃'});
+    assert.strictEqual(logEntry!.message, '👾');
+    assert.strictEqual(logEntry!.level, 'error');
+    assert.strictEqual(logEntry!.key, '🎃');
+
+    child.warn('hello %d', 56, {key: 'value'});
+    assert.strictEqual(logEntry!.message, 'hello %d');
+    assert.strictEqual(logEntry!.level, 'warn');
+    assert.strictEqual(logEntry!.key, undefined);
+
+    child.log('silly', '🎈');
+    assert.strictEqual(logEntry!.message, '🎈');
+    assert.strictEqual(logEntry!.level, 'silly');
+  });
+
+  it('should override only the write function', () => {
+    const child = makeChildLogger(LOGGER, FAKE_TRACE);
+    assert.strictEqual(child.warn, LOGGER.warn);
+    assert.notStrictEqual(child.write, LOGGER.write);
+  });
+
+  it('should inject the LOGGING_TRACE_KEY into the metadata', () => {
+    const child = makeChildLogger(LOGGER, FAKE_TRACE);
+    let trace;
+    // tslint:disable-next-line:no-any
+    (LOGGER.write as any) = (info: winston.LogEntry) => {
+      trace = info[LOGGING_TRACE_KEY];
+    };
+    child.debug('hello world');
+    assert.strictEqual(trace, FAKE_TRACE);
+  });
+
+  it('should overwrite existing LOGGING_TRACE_KEY value', () => {
+    const child = makeChildLogger(LOGGER, FAKE_TRACE);
+    let trace;
+    // tslint:disable-next-line:no-any
+    (LOGGER.write as any) = (info: winston.LogEntry) => {
+      trace = info[LOGGING_TRACE_KEY];
+    };
+    child.debug('hello world', {[LOGGING_TRACE_KEY]: 'to-be-clobbered'});
+    assert.strictEqual(trace, FAKE_TRACE);
+  });
+});
