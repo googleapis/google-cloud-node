43abb510847632fb71edf9a9daff17fcf4c797db
Jonathan Lui <jonathanlui@google.com>
2019-04-04 18:40:05 -0700

    refactor: wrap execSync with encoding: utf-8 (#137)

--- packages/google-cloud-iot/samples/test/quickstart.test.js
+++ packages/google-cloud-iot/samples/test/quickstart.test.js
@@ -15,7 +15,9 @@
 'use strict';
 
 const {assert} = require('chai');
-const {execSync} = require('child_process');
+const cp = require('child_process');
+
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 describe('iot samples', () => {
   it('should run the quickstart', async () => {
