015be8bdd99c9d9ef17e62c523cb72103e3d32c8
Jonathan Lui <jonathanlui@google.com>
2019-04-04 18:39:53 -0700

    refactor: wrap execSync with encoding: utf-8 (#226)

--- packages/google-cloud-language/samples/test/quickstart.test.js
+++ packages/google-cloud-language/samples/test/quickstart.test.js
@@ -16,7 +16,9 @@
 'use strict';
 
 const {assert} = require('chai');
-const {execSync} = require('child_process');
+const cp = require('child_process');
+
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 describe('quickstart', () => {
   it('should analyze sentiment in text', async () => {
