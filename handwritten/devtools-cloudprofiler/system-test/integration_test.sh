5e8e7fabd03adc16ed3747780b9f4275b9462c9b
Margaret Nolan <nolanmar@google.com>
2019-04-05 18:05:19 -0700

    chore: remove old kokoro configs (#458)

--- handwritten/devtools-cloudprofiler/system-test/integration_test.sh
+++ handwritten/devtools-cloudprofiler/system-test/integration_test.sh
@@ -19,8 +19,6 @@ SERVICE_KEY="${KOKORO_KEYSTORE_DIR}/72935_cloud-profiler-e2e-service-account-key
 COMMIT=$(git rev-parse HEAD)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 REPO=$(git config --get remote.origin.url)
-RUN_ONLY_V8_CANARY_TEST="${RUN_ONLY_V8_CANARY_TEST:-false}"
-echo "$RUN_ONLY_V8_CANARY_TEST"
 export GCLOUD_TESTS_NODEJS_PROJECT_ID="cloud-profiler-e2e"
 export GCLOUD_TESTS_NODEJS_ZONE="us-east1-b"
 export GOOGLE_APPLICATION_CREDENTIALS="${SERVICE_KEY}"
@@ -34,8 +32,8 @@ cp -R "system-test" "$GOPATH/src/proftest"
 cd "$GOPATH/src/proftest"
 retry go get -t -d -tags=integration .
 if [ "$KOKORO_GITHUB_PULL_REQUEST_NUMBER" = "" ]; then
-  go test -timeout=30m -tags=integration -run TestAgentIntegration -commit="$COMMIT" -branch="$BRANCH" -repo="$REPO" -run_only_v8_canary_test="$RUN_ONLY_V8_CANARY_TEST"
+  go test -timeout=30m -tags=integration -run TestAgentIntegration -commit="$COMMIT" -branch="$BRANCH" -repo="$REPO"
 else
-  go test -timeout=30m -tags=integration -run TestAgentIntegration -commit="$COMMIT" -pr="$KOKORO_GITHUB_PULL_REQUEST_NUMBER" -run_only_v8_canary_test="$RUN_ONLY_V8_CANARY_TEST"
+  go test -timeout=30m -tags=integration -run TestAgentIntegration -commit="$COMMIT" -pr="$KOKORO_GITHUB_PULL_REQUEST_NUMBER"
 fi
 
