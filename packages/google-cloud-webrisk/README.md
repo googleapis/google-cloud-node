73e41edce6418e4b10da3d9a9078748682208218
Benjamin Coe <bencoe@google.com>
2019-04-29 13:09:56 -0700

    feat: initial commit of library for review

---
+++ packages/google-cloud-webrisk/README.md
@@ -0,0 +1,31 @@
+# Node.js Client for Web Risk API ([Alpha](https://github.com/GoogleCloudPlatform/google-cloud-node#versioning))
+
+[Web Risk API][Product Documentation]:
+
+- [Client Library Documentation][]
+- [Product Documentation][]
+
+## Quick Start
+In order to use this library, you first need to go through the following
+steps:
+
+1. [Select or create a Cloud Platform project.](https://console.cloud.google.com/project)
+2. [Enable billing for your project.](https://cloud.google.com/billing/docs/how-to/modify-project#enable_billing_for_a_project)
+3. [Enable the Web Risk API.](https://console.cloud.google.com/apis/library/webrisk.googleapis.com)
+4. [Setup Authentication.](https://googlecloudplatform.github.io/google-cloud-node/#/docs/google-cloud/master/guides/authentication)
+
+### Installation
+```
+$ npm install --save webrisk
+```
+
+### Next Steps
+- Read the [Client Library Documentation][] for Web Risk API
+  to see other available methods on the client.
+- Read the [Web Risk API Product documentation][Product Documentation]
+  to learn more about the product and see How-to Guides.
+- View this [repository's main README](https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/README.md)
+  to see the full list of Cloud APIs that we cover.
+
+[Client Library Documentation]: https://googlecloudplatform.github.io/google-cloud-node/#/docs/webrisk
+[Product Documentation]: https://cloud.google.com/webrisk
---
+++ packages/google-cloud-webrisk/test/gapic-v1beta1.js
@@ -0,0 +1,215 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const assert = require('assert');
+
+const webriskModule = require('../src');
+
+const FAKE_STATUS_CODE = 1;
+const error = new Error();
+error.code = FAKE_STATUS_CODE;
+
+describe('WebRiskServiceV1Beta1Client', () => {
+  describe('computeThreatListDiff', () => {
+    it('invokes computeThreatListDiff without error', done => {
+      const client = new webriskModule.v1beta1.WebRiskServiceV1Beta1Client({
+        credentials: {client_email: 'bogus', private_key: 'bogus'},
+        projectId: 'bogus',
+      });
+
+      // Mock request
+      const threatType = 'THREAT_TYPE_UNSPECIFIED';
+      const constraints = {};
+      const request = {
+        threatType: threatType,
+        constraints: constraints,
+      };
+
+      // Mock response
+      const newVersionToken = '115';
+      const expectedResponse = {
+        newVersionToken: newVersionToken,
+      };
+
+      // Mock Grpc layer
+      client._innerApiCalls.computeThreatListDiff = mockSimpleGrpcMethod(
+        request,
+        expectedResponse
+      );
+
+      client.computeThreatListDiff(request, (err, response) => {
+        assert.ifError(err);
+        assert.deepStrictEqual(response, expectedResponse);
+        done();
+      });
+    });
+
+    it('invokes computeThreatListDiff with error', done => {
+      const client = new webriskModule.v1beta1.WebRiskServiceV1Beta1Client({
+        credentials: {client_email: 'bogus', private_key: 'bogus'},
+        projectId: 'bogus',
+      });
+
+      // Mock request
+      const threatType = 'THREAT_TYPE_UNSPECIFIED';
+      const constraints = {};
+      const request = {
+        threatType: threatType,
+        constraints: constraints,
+      };
+
+      // Mock Grpc layer
+      client._innerApiCalls.computeThreatListDiff = mockSimpleGrpcMethod(
+        request,
+        null,
+        error
+      );
+
+      client.computeThreatListDiff(request, (err, response) => {
+        assert(err instanceof Error);
+        assert.strictEqual(err.code, FAKE_STATUS_CODE);
+        assert(typeof response === 'undefined');
+        done();
+      });
+    });
+  });
+
+  describe('searchUris', () => {
+    it('invokes searchUris without error', done => {
+      const client = new webriskModule.v1beta1.WebRiskServiceV1Beta1Client({
+        credentials: {client_email: 'bogus', private_key: 'bogus'},
+        projectId: 'bogus',
+      });
+
+      // Mock request
+      const uri = 'uri116076';
+      const threatTypes = [];
+      const request = {
+        uri: uri,
+        threatTypes: threatTypes,
+      };
+
+      // Mock response
+      const expectedResponse = {};
+
+      // Mock Grpc layer
+      client._innerApiCalls.searchUris = mockSimpleGrpcMethod(
+        request,
+        expectedResponse
+      );
+
+      client.searchUris(request, (err, response) => {
+        assert.ifError(err);
+        assert.deepStrictEqual(response, expectedResponse);
+        done();
+      });
+    });
+
+    it('invokes searchUris with error', done => {
+      const client = new webriskModule.v1beta1.WebRiskServiceV1Beta1Client({
+        credentials: {client_email: 'bogus', private_key: 'bogus'},
+        projectId: 'bogus',
+      });
+
+      // Mock request
+      const uri = 'uri116076';
+      const threatTypes = [];
+      const request = {
+        uri: uri,
+        threatTypes: threatTypes,
+      };
+
+      // Mock Grpc layer
+      client._innerApiCalls.searchUris = mockSimpleGrpcMethod(
+        request,
+        null,
+        error
+      );
+
+      client.searchUris(request, (err, response) => {
+        assert(err instanceof Error);
+        assert.strictEqual(err.code, FAKE_STATUS_CODE);
+        assert(typeof response === 'undefined');
+        done();
+      });
+    });
+  });
+
+  describe('searchHashes', () => {
+    it('invokes searchHashes without error', done => {
+      const client = new webriskModule.v1beta1.WebRiskServiceV1Beta1Client({
+        credentials: {client_email: 'bogus', private_key: 'bogus'},
+        projectId: 'bogus',
+      });
+
+      // Mock request
+      const request = {};
+
+      // Mock response
+      const expectedResponse = {};
+
+      // Mock Grpc layer
+      client._innerApiCalls.searchHashes = mockSimpleGrpcMethod(
+        request,
+        expectedResponse
+      );
+
+      client.searchHashes(request, (err, response) => {
+        assert.ifError(err);
+        assert.deepStrictEqual(response, expectedResponse);
+        done();
+      });
+    });
+
+    it('invokes searchHashes with error', done => {
+      const client = new webriskModule.v1beta1.WebRiskServiceV1Beta1Client({
+        credentials: {client_email: 'bogus', private_key: 'bogus'},
+        projectId: 'bogus',
+      });
+
+      // Mock request
+      const request = {};
+
+      // Mock Grpc layer
+      client._innerApiCalls.searchHashes = mockSimpleGrpcMethod(
+        request,
+        null,
+        error
+      );
+
+      client.searchHashes(request, (err, response) => {
+        assert(err instanceof Error);
+        assert.strictEqual(err.code, FAKE_STATUS_CODE);
+        assert(typeof response === 'undefined');
+        done();
+      });
+    });
+  });
+
+});
+
+function mockSimpleGrpcMethod(expectedRequest, response, error) {
+  return function(actualRequest, options, callback) {
+    assert.deepStrictEqual(actualRequest, expectedRequest);
+    if (error) {
+      callback(error);
+    } else if (response) {
+      callback(null, response);
+    } else {
+      callback(null);
+    }
+  };
+}
