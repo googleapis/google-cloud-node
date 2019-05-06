a7fe2ba3075e9201bd1478b836acd1961c00793c
Benjamin E. Coe <bencoe@google.com>
2019-05-06 12:20:58 -0700

    build: patch Windows container, fixing Node 10 (#479)

--- handwritten/devtools-cloudprofiler/.kokoro/test.bat
+++ handwritten/devtools-cloudprofiler/.kokoro/test.bat
@@ -12,7 +12,24 @@
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
 
-cd /d %~dp0	
+@echo "Starting Windows build"
+
+cd /d %~dp0
 cd ..
 
-"C:\Program Files\Git\bin\bash.exe" .kokoro/test.sh --windows
+@rem The image we're currently running has a broken version of Node.js enabled
+@rem by nvm (v10.15.3), which has no npm bin. This hack uses the functional
+@rem Node v8.9.1 to install npm@latest, it then uses this version of npm to
+@rem install npm for v10.15.3.
+call nvm use v8.9.1 || goto :error
+call node C:\Users\kbuilder\AppData\Roaming\nvm-ps\versions\v8.9.1\node_modules\npm-bootstrap\bin\npm-cli.js i npm -g || goto :error
+call nvm use v10.15.3 || goto :error
+call node C:\Users\kbuilder\AppData\Roaming\nvm-ps\versions\v8.9.1\node_modules\npm\bin\npm-cli.js i npm -g || goto :error
+
+call npm install || goto :error
+call npm run test || goto :error
+
+goto :EOF
+
+:error
+exit /b 1
