941a8365640878df2e4fe2adcfcbf6e4d7c4e764
Justin Beckwith <justin.beckwith@gmail.com>
2019-06-06 08:19:08 -0700

    build: set correct src path for template (#508)

--- handwritten/devtools-cloudprofiler/synth.py
+++ handwritten/devtools-cloudprofiler/synth.py
@@ -3,5 +3,5 @@ import synthtool.gcp as gcp
 import logging
 logging.basicConfig(level=logging.DEBUG)
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library()
+templates = common_templates.node_library(source_location='out/src')
 s.copy(templates)
