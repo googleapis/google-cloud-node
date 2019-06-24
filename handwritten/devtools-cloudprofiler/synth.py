22c5f3d1ad14ebf42a151d5e834aaf86bb48ca5e
Justin Beckwith <beckwith@google.com>
2019-06-24 07:32:39 -0700

    docs: add repo-metadata for docs (#511)

--- handwritten/devtools-cloudprofiler/synth.py
+++ handwritten/devtools-cloudprofiler/synth.py
@@ -4,4 +4,4 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library(source_location='out/src')
-s.copy(templates)
+s.copy(templates, excludes=['README.md'])
