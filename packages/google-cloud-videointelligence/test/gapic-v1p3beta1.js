48f135ddfa39706d92c6bbdea6d90b5ebe3b4dbc
Justin Beckwith <justin.beckwith@gmail.com>
2019-07-22 22:31:43 -0700

    chore(deps): drop dependency on through2 (#272)

--- packages/google-cloud-videointelligence/test/gapic-v1p3beta1.js
+++ packages/google-cloud-videointelligence/test/gapic-v1p3beta1.js
@@ -15,7 +15,7 @@
 'use strict';
 
 const assert = require('assert');
-const through2 = require('through2');
+const {PassThrough} = require('stream');
 
 const videoIntelligenceModule = require('../src');
 
@@ -251,13 +251,16 @@ describe('StreamingVideoIntelligenceServiceClient', () => {
 
 function mockBidiStreamingGrpcMethod(expectedRequest, response, error) {
   return () => {
-    const mockStream = through2.obj((chunk, enc, callback) => {
-      assert.deepStrictEqual(chunk, expectedRequest);
-      if (error) {
-        callback(error);
-      } else {
-        callback(null, response);
-      }
+    const mockStream = new PassThrough({
+      objectMode: true,
+      transform: (chunk, enc, callback) => {
+        assert.deepStrictEqual(chunk, expectedRequest);
+        if (error) {
+          callback(error);
+        } else {
+          callback(null, response);
+        }
+      },
     });
     return mockStream;
   };
