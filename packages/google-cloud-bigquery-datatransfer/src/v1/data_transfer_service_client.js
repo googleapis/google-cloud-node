a9f0b96fca5747e16a886bfea4a73e88c6f36510
Justin Beckwith <justin.beckwith@gmail.com>
2019-07-31 16:04:56 -0700

    docs: document apiEndpoint over servicePath (#236)

--- packages/google-cloud-bigquery-datatransfer/src/v1/data_transfer_service_client.js
+++ packages/google-cloud-bigquery-datatransfer/src/v1/data_transfer_service_client.js
@@ -54,7 +54,7 @@ class DataTransferServiceClient {
    *     your project ID will be detected automatically.
    * @param {function} [options.promise] - Custom promise module to use instead
    *     of native Promises.
-   * @param {string} [options.servicePath] - The domain name of the
+   * @param {string} [options.apiEndpoint] - The domain name of the
    *     API remote host.
    */
   constructor(opts) {
