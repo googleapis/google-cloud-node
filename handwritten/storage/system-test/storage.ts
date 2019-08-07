7e3d41594514b495373a16c0b3797b4476e0dcc0
Stephen <stephenplusplus@users.noreply.github.com>
2019-08-07 11:08:06 -0400

    tests: fix system tests (#802)

--- handwritten/storage/system-test/storage.ts
+++ handwritten/storage/system-test/storage.ts
@@ -2178,7 +2178,9 @@ describe('storage', () => {
                     // stop sending data half way through.
                     this.push(chunk);
                     this.destroy();
-                    ws.destroy(new Error('Interrupted.'));
+                    process.nextTick(() => {
+                      ws.destroy(new Error('Interrupted.'));
+                    });
                   } else {
                     this.push(chunk);
                     next();
