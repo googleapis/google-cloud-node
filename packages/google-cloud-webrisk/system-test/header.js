3c96b82141bd31a52894f5a35643fb8bacaee063
Benjamin E. Coe <bencoe@google.com>
2019-08-26 10:43:12 -0700

    test: add a smoke test for x-googl-api-client header being set (#46)

---
+++ packages/google-cloud-webrisk/system-test/header.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const {assert} = require('chai');
+const http2spy = require('http2spy');
+const {WebRiskServiceV1Beta1Client} = http2spy.require(require.resolve('../'));
+
+describe('header', () => {
+  it('populates x-goog-api-client header', async () => {
+    const client = new WebRiskServiceV1Beta1Client();
+    const request = {
+      uri: 'http://testsafebrowsing.appspot.com/s/malware.html',
+      threatTypes: ['MALWARE'],
+    };
+    await client.searchUris(request);
+    assert.ok(
+      /^gl-node\/[0-9]+\.[\w.-]+ grpc\/[\w.-]+ gax\/[\w.-]+ gapic\/[\w.-]+$/.test(
+        http2spy.requests[0]['x-goog-api-client'][0]
+      )
+    );
+  });
+});
