7fa9bc85ff63c501b4e5cd69578c4c22c24b68d0
praveenqlogic01 <50827134+praveenqlogic01@users.noreply.github.com>
2019-09-12 01:27:41 +0530

    feat: add support for auto-unindex  (#453)

--- handwritten/datastore/test/request.ts
+++ handwritten/datastore/test/request.ts
@@ -1542,6 +1542,142 @@ describe('Request', () => {
       );
     });
 
+    it('should allow setting the indexed value on arrays', done => {
+      request.request_ = (config: RequestConfig) => {
+        const property = config.reqOpts!.mutations![0].upsert!.properties!.name;
+
+        property.arrayValue!.values!.forEach((value: Any) => {
+          assert.strictEqual(value.excludeFromIndexes, true);
+        });
+
+        done();
+      };
+
+      request.save(
+        {
+          key,
+          data: [
+            {
+              name: 'name',
+              value: ['one', 'two', 'three'],
+              excludeFromIndexes: true,
+            },
+          ],
+        },
+        assert.ifError
+      );
+    });
+
+    it('should prepare excludeFromIndexes array for large values', done => {
+      const longString = Buffer.alloc(1501, '.').toString();
+      const data = {
+        longString,
+        notMetadata: true,
+        longStringArray: [longString],
+        metadata: {
+          longString,
+          otherProperty: 'value',
+          obj: {
+            longStringArray: [
+              {
+                longString,
+                nestedLongStringArray: [
+                  {
+                    longString,
+                    nestedProperty: true,
+                  },
+                  {
+                    longString,
+                  },
+                ],
+              },
+            ],
+          },
+          longStringArray: [
+            {
+              longString,
+              nestedLongStringArray: [
+                {
+                  longString,
+                  nestedProperty: true,
+                },
+                {
+                  longString,
+                },
+              ],
+            },
+          ],
+        },
+      };
+
+      const excludeFromIndexes = [
+        'longString',
+        'longStringArray[]',
+        'metadata.longString',
+        'metadata.obj.longStringArray[].longString',
+        'metadata.obj.longStringArray[].nestedLongStringArray[].longString',
+        'metadata.longStringArray[].longString',
+        'metadata.longStringArray[].nestedLongStringArray[].longString',
+      ];
+
+      entity.entityToEntityProto = entity => {
+        return (entity as unknown) as EntityProto;
+      };
+      request.request_ = (config: RequestConfig) => {
+        assert.strictEqual(
+          (config.reqOpts!.mutations![0].upsert! as Entity)
+            .excludeLargeProperties,
+          true
+        );
+        assert.deepStrictEqual(
+          (config.reqOpts!.mutations![0].upsert! as Entity).excludeFromIndexes,
+          excludeFromIndexes
+        );
+        done();
+      };
+
+      request.save(
+        {
+          key,
+          data,
+          excludeLargeProperties: true,
+        },
+        assert.ifError
+      );
+    });
+
+    it('should allow auto setting the indexed value of a property with excludeLargeProperties', done => {
+      const longString = Buffer.alloc(1501, '.').toString();
+      const data = [
+        {
+          name: 'name',
+          value: longString,
+        },
+        {
+          name: 'description',
+          value: 'value',
+        },
+      ];
+
+      request.request_ = (config: RequestConfig) => {
+        assert.deepStrictEqual(
+          config.reqOpts!.mutations![0].upsert!.properties!.name
+            .excludeFromIndexes,
+          true
+        );
+        done();
+      };
+
+      request.save(
+        {
+          key,
+          data,
+          excludeLargeProperties: true,
+        },
+        assert.ifError
+      );
+    });
+
     it('should assign ID on keys without them', done => {
       const incompleteKey = new entity.Key({path: ['Incomplete']});
       const incompleteKey2 = new entity.Key({path: ['Incomplete']});
