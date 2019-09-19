4a8f31adf46d45d506486a46995284f619f45a29
Kelvin Jin <kelvinjin@google.com>
2019-09-19 15:36:38 -0700

    feat: add getDefaultMetadataForTracing (#388)

---
+++ handwritten/nodejs-logging-winston/test/stackdriver-trace-integration.ts
@@ -0,0 +1,178 @@
+/**
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as assert from 'assert';
+import * as proxyquire from 'proxyquire';
+import * as winston from 'winston';
+import * as loggingWinstonLibTypes from '../src/index';
+
+/**
+ * Tests that ensure that getDefaultMetadataForTracing can be used for
+ * trace-log correlation when Stackdriver Trace Agent is present. See
+ * src/default-metadata.ts for an explanation on why this to exist.
+ */
+describe('Stackdriver Trace Log Correlation', () => {
+  // Trace context IDs seen in logs so far in a test.
+  const seenContextIds: string[] = [];
+  // Set a trace context ID for all succeeding Winston logs.
+  let setCurrentContextId: (id: string) => void;
+  // The Stackdriver Logging Winston transport library.
+  let loggingWinstonLib: typeof loggingWinstonLibTypes;
+
+  class FakeLogging {
+    constructor() {}
+    log(data: never, callback: () => void) {
+      if (typeof callback === 'function') setImmediate(callback);
+      return this;
+    }
+    // Stub entry to record the incoming trace context ID.
+    entry(metadata: {trace: string}) {
+      if (metadata.trace) {
+        const traceId = metadata.trace.split('/')[3];
+        assert.ok(traceId);
+        seenContextIds.push(traceId);
+      } else {
+        seenContextIds.push('');
+      }
+      return {};
+    }
+    info(data: never, callback: () => void) {
+      return this.log(data, callback);
+    }
+  }
+
+  before(() => {
+    loggingWinstonLib = proxyquire('../src/index', {
+      '@google-cloud/logging': {'@global': true, Logging: FakeLogging},
+    });
+  });
+
+  beforeEach(() => {
+    seenContextIds.length = 0;
+    setCurrentContextId = (() => {
+      let currentContextId: string;
+      global._google_trace_agent = {
+        getCurrentContextId: () => {
+          return currentContextId;
+        },
+        getWriterProjectId: () => {
+          return 'project1';
+        },
+      };
+      return (id: string) => {
+        currentContextId = id;
+      };
+    })();
+  });
+
+  after(() => {
+    delete global._google_trace_agent;
+  });
+
+  it('Works when using supporting default metadata', done => {
+    const transport = new loggingWinstonLib.LoggingWinston();
+    const logger = winston.createLogger({
+      transports: [transport],
+      defaultMeta: loggingWinstonLib.getDefaultMetadataForTracing(),
+    });
+    setCurrentContextId('1');
+    logger.log({level: 'info', message: 'hello'});
+    setCurrentContextId('2');
+    logger.log({level: 'info', message: 'hello'});
+    setCurrentContextId('3');
+    setImmediate(() => {
+      assert.strictEqual(seenContextIds.length, 2);
+      assert.deepStrictEqual(seenContextIds, ['1', '2']);
+      done();
+    });
+  });
+
+  /**
+   * This test is just like the previous one, but without using
+   * getDefaultMetadataForTracing. The expected seen context IDs are ['1', '3'],
+   * which are wrong.
+   * If this test ever fails, that signals that getDefaultMetadataForTracing
+   * may no longer be a necessary API, as Winston 3 has fixed its context
+   * propagation issue.
+   */
+  it('Does not work without using supporting default metadata', done => {
+    const transport = new loggingWinstonLib.LoggingWinston();
+    const logger = winston.createLogger({
+      transports: [transport],
+    });
+    setCurrentContextId('1');
+    logger.log({level: 'info', message: 'hello'});
+    setCurrentContextId('2');
+    logger.log({level: 'info', message: 'hello'});
+    setCurrentContextId('3');
+    setImmediate(() => {
+      assert.strictEqual(seenContextIds.length, 2);
+      assert.throws(() => {
+        assert.deepStrictEqual(seenContextIds, ['1', '2']);
+      });
+      done();
+    });
+  });
+
+  [null, {}, {getWriterProjectId: () => 'project1'}].forEach(testCase => {
+    it(`Doesn't crash when a non-compatible Trace Agent is present: ${testCase}`, done => {
+      global._google_trace_agent = testCase;
+      const transport = new loggingWinstonLib.LoggingWinston();
+      const logger = winston.createLogger({
+        transports: [transport],
+        defaultMeta: loggingWinstonLib.getDefaultMetadataForTracing(),
+      });
+      setCurrentContextId('1');
+      logger.log({level: 'info', message: 'hello'});
+      setCurrentContextId('2');
+      logger.log({level: 'info', message: 'hello'});
+      setCurrentContextId('3');
+      setImmediate(() => {
+        assert.strictEqual(seenContextIds.length, 2);
+        done();
+      });
+    });
+  });
+
+  [
+    {
+      getCurrentContextId: () => 'trace1',
+      getWriterProjectId: () => null,
+    },
+    {
+      getCurrentContextId: () => null,
+      getWriterProjectId: () => 'project1',
+    },
+  ].forEach(testCase => {
+    it(`Doesn't crash when a Trace Agent field is not present: ${testCase}`, done => {
+      global._google_trace_agent = testCase;
+      const transport = new loggingWinstonLib.LoggingWinston();
+      const logger = winston.createLogger({
+        transports: [transport],
+        defaultMeta: loggingWinstonLib.getDefaultMetadataForTracing(),
+      });
+      setCurrentContextId('1');
+      logger.log({level: 'info', message: 'hello'});
+      setCurrentContextId('2');
+      logger.log({level: 'info', message: 'hello'});
+      setCurrentContextId('3');
+      setImmediate(() => {
+        assert.strictEqual(seenContextIds.length, 2);
+        done();
+      });
+    });
+  });
+});
