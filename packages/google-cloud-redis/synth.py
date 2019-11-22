367ac459a6e23f7bfcfacc0ff0a5171158d1389d
Xiaozhen Liu <xiaozhenliu@google.com>
2019-11-22 11:46:35 -0800

    feat: convert to TypeScript (#236)
    * setup
    
    * add comment
    
    * fix: make stuff work
    
    * fix: pagination
    
    * chore: formatting
    
    * fix: pr feedback
    
    * fix: run synthtool
    
    * fix: run synthtool one last time

--- packages/google-cloud-redis/synth.py
+++ packages/google-cloud-redis/synth.py
@@ -5,26 +5,31 @@ import subprocess
 
 logging.basicConfig(level=logging.DEBUG)
 
-gapic = gcp.GAPICGenerator()
+gapic = gcp.GAPICMicrogenerator()
 
 # tasks has two product names, and a poorly named artman yaml
 versions = ['v1beta1', 'v1']
-
+name = 'redis'
 for version in versions:
-    library = gapic.node_library('redis', version)
+    library = gapic.typescript_library(
+            name, 
+            generator_args={
+                "package-name": f"@google-cloud/{name}"
+            },
+            proto_path=f'google/cloud/{name}/{version}',
+            extra_proto_files=["google/cloud/common_resources.proto"],
+            version=version,
+    )
     s.copy(
         library,
-        excludes=['package.json', 'README.md', 'src/index.js'])
+        excludes=['package.json', 'README.md', 'src/index.ts'])
 
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library()
+templates = common_templates.node_library(source_location='build/src')
 s.copy(templates)
 
-# [START fix-dead-link]
-s.replace('**/doc/google/protobuf/doc_timestamp.js',
-        'https:\/\/cloud\.google\.com[\s\*]*http:\/\/(.*)[\s\*]*\)',
-        r"https://\1)")
-# [END fix-dead-link]
+# fix for broken link in docs
+s.replace('src/v*/*_client.ts', '/compute/docs/', 'https://cloud.google.com/compute/docs/')
 
 subprocess.run(['npm', 'install'])
 subprocess.run(['npm', 'run', 'fix'])
