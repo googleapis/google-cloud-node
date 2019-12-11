41116c17ddb85e6015a5fd2f8c9cfd237f21889a
Benjamin Coe <bencoe@google.com>
2019-12-11 12:15:54 -0800

    chore: add initial files for launch

---
+++ packages/google-monitoring-dashboard/synth.py
@@ -0,0 +1,64 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This script is used to synthesize generated parts of this library."""
+
+import synthtool as s
+import synthtool.gcp as gcp
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+# run the gapic generator
+gapic = gcp.GAPICMicrogenerator()
+versions = ['v1']
+for version in versions:
+  library = gapic.typescript_library(
+    '@google-cloud/monitoring-dashboard',
+    generator_args={
+    },
+    proto_path='/google/monitoring/dashboard/v1',
+    version=version)
+s.copy(library, excludes=['README.md'])
+
+# Copy common templates
+common_templates = gcp.CommonTemplates()
+templates = common_templates.node_library(source_location='build/src')
+s.copy(templates, excludes=[])
+
+# Node.js specific cleanup
+subprocess.run(['rm', '-rf', './system-test/fixtures'])
+subprocess.run(['npm', 'install'])
+subprocess.run(['npm', 'run', 'fix'])
+
+# Add missing jsdoc annotations:
+s.replace("src/index.js",
+r"""\/\*\*
+ \* @namespace google
+ \*/""",
+r"""/**
+ * @namespace google
+ */
+/**
+ * @namespace google.rpc
+ */
+/**
+ * @namespace google.protobuf
+ */
+/**
+ * @namespace google.longrunning
+ */""")
+s.replace("src/index.js",
+r"namespace google.cloud.cloudbuild",
+r"namespace google.devtools.cloudbuild")
