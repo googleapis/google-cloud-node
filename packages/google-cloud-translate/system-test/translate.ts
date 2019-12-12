b74914e8258c1f112deead9c9170075350689674
Benjamin E. Coe <bencoe@google.com>
2019-12-12 17:31:06 -0500

    test: adds integration test for x-goog-user-project header (#404)

--- packages/google-cloud-translate/system-test/translate.ts
+++ packages/google-cloud-translate/system-test/translate.ts
@@ -17,6 +17,7 @@
 import * as assert from 'assert';
 import {TranslationServiceClient} from '../src';
 
+const http2spy = require('http2spy');
 const API_KEY = process.env.TRANSLATE_API_KEY;
 
 describe('translate', () => {
@@ -134,6 +135,85 @@ describe('translate', () => {
         supportSource: true,
         supportTarget: true,
       });
+    });
+
+    it('should populate x-goog-user-project header, and succeed if valid project', async () => {
+      const {GoogleAuth} = require('google-auth-library');
+      const auth = new GoogleAuth({
+        credentials: Object.assign(
+          require(process.env.GOOGLE_APPLICATION_CREDENTIALS || ''),
+          {
+            quota_project_id: process.env.GCLOUD_PROJECT,
+          }
+        ),
+      });
+      const {TranslationServiceClient} = http2spy.require(
+        require.resolve('../src')
+      );
+      const translate = new TranslationServiceClient({
+        auth,
+      });
+
+      // We run the same test as "list of supported languages", but with an
+      // alternate "quota_project_id" set; Given that GCLOUD_PROJECT
+      // references a valid project, we expect success:
+      const projectId = await translate.getProjectId();
+      // This should not hrow an exception:
+      await translate.getSupportedLanguages({
+        parent: `projects/${projectId}`,
+      });
+      // Ensure we actually populated the header:
+      assert.strictEqual(
+        process.env.GCLOUD_PROJECT,
+        http2spy.requests[http2spy.requests.length - 1][
+          'x-goog-user-project'
+        ][0]
+      );
+    });
+
+    it('should populate x-goog-user-project header, and fail if invalid project', async () => {
+      const {GoogleAuth} = require('google-auth-library');
+      const auth = new GoogleAuth({
+        credentials: Object.assign(
+          require(process.env.GOOGLE_APPLICATION_CREDENTIALS || ''),
+          {
+            quota_project_id: 'my-fake-billing-project',
+          }
+        ),
+      });
+      const {TranslationServiceClient} = http2spy.require(
+        require.resolve('../src')
+      );
+      const translate = new TranslationServiceClient({
+        auth,
+      });
+
+      // We set a quota project "my-fake-billing-project" that does not exist,
+      // this should result in an error.
+      let err: Error | null = null;
+      try {
+        const projectId = await translate.getProjectId();
+        const [result] = await translate.getSupportedLanguages({
+          parent: `projects/${projectId}`,
+        });
+      } catch (_err) {
+        err = _err;
+      }
+      assert(err);
+      assert(
+        err!.message.includes(
+          // make sure the error included our fake project name, we shouldn't
+          // be too specific about the error incase it changes upstream.
+          'my-fake-billing-project'
+        ),
+        err!.message
+      );
+      assert.strictEqual(
+        'my-fake-billing-project',
+        http2spy.requests[http2spy.requests.length - 1][
+          'x-goog-user-project'
+        ][0]
+      );
     });
   });
 });
