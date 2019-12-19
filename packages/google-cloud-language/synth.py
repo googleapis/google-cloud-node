1bb99bde07e368dece899c0b4243857ad9e43c46
Xiaozhen Liu <xiaozhenliu@google.com>
2019-12-18 16:32:01 -0800

    feat: move library to typescript code generation (#338)
    * move to typescript
    
    * run synth tool
    
    * rerun synthtool to pick up new timeout number
    
    * clean package json
    
    * rerun synth tool
    
    * try eslintrc rules

--- packages/google-cloud-language/synth.py
+++ packages/google-cloud-language/synth.py
@@ -5,20 +5,26 @@ import subprocess
 
 logging.basicConfig(level=logging.DEBUG)
 
-gapic = gcp.GAPICGenerator()
+gapic = gcp.GAPICMicrogenerator()
 # tasks has two product names, and a poorly named artman yaml
 for version in ['v1', 'v1beta2']:
-    library = gapic.node_library(
-        'language', version)
+    library = gapic.typescript_library(
+        'language', 
+        generator_args={
+            "grpc-service-config": f"google/cloud/language/{version}/language_grpc_service_config.json",
+            "package-name":f"@google-cloud/language"
+            },
+        proto_path=f'/google/cloud/language/{version}',
+        version=version)
 
     # skip index, protos, package.json, and README.md
     s.copy(
         library,
-        excludes=['package.json', 'README.md', 'src/index.js'])
+        excludes=['package.json', 'README.md', 'src/index.ts'])
 
 # Update common templates
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library()
+templates = common_templates.node_library(source_location='build/src')
 s.copy(templates)
 
 # Node.js specific cleanup
