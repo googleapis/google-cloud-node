7d82e47e88a788e99ad62bed5136290287dcae0d
Justin Beckwith <justin.beckwith@gmail.com>
2019-12-30 10:17:01 -0800

    refactor: use explicit mocha imports (#119)

--- packages/google-devtools-containeranalysis/system-test/system.js
+++ packages/google-devtools-containeranalysis/system-test/system.js
@@ -15,5 +15,10 @@
 'use strict';
 
 const assert = require('assert');
+const {describe, it} = require('mocha');
 
-assert.ok('we should really write a system test');
+describe('container analysis', () => {
+  it('should have tests', () => {
+    assert.ok('we should really write a system test');
+  });
+});
