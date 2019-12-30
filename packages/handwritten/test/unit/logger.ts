784722f1c61005fe92c7a2c2c3e63a193c694812
Justin Beckwith <justin.beckwith@gmail.com>
2019-12-30 11:15:31 -0800

    refactor: use explicit mocha imports (#425)

--- packages/handwritten/test/unit/logger.ts
+++ packages/handwritten/test/unit/logger.ts
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import * as assert from 'assert';
+import {describe, it} from 'mocha';
 import {createLogger} from '../../src/logger';
 
 describe('logger', () => {
