5cf44e86b847a89f82c32b46f4469d2652ad86f9
Justin Beckwith <justin.beckwith@gmail.com>
2019-12-30 11:36:01 -0800

    refactor: use explicit mocha imports

--- handwritten/nodejs-promisify/samples/test/test.js
+++ handwritten/nodejs-promisify/samples/test/test.js
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 const {assert} = require('chai');
+const {describe, it} = require('mocha');
 const cp = require('child_process');
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
