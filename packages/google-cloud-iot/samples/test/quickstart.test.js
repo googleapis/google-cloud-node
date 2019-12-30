e65f7fd63d8f979bc22f45529befca2d515c8b3d
Justin Beckwith <justin.beckwith@gmail.com>
2019-12-30 11:35:58 -0800

    refactor: use explicit mocha imports

--- packages/google-cloud-iot/samples/test/quickstart.test.js
+++ packages/google-cloud-iot/samples/test/quickstart.test.js
@@ -15,6 +15,7 @@
 'use strict';
 
 const {assert} = require('chai');
+const {describe, it} = require('mocha');
 const cp = require('child_process');
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
