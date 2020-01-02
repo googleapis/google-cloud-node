d6959635dda40fcfe52bd5baa5009f7a08b9f911
Xiaozhen Liu <xiaozhenliu@google.com>
2020-01-02 15:55:35 -0800

    feat!: move to typescript code generation (#260)
    * move to ts
    
    * run synthtool
    
    * run synthtool
    
    Co-authored-by: Benjamin E. Coe <bencoe@google.com>

--- packages/google-cloud-oslogin/synth.py
+++ packages/google-cloud-oslogin/synth.py
@@ -21,25 +21,31 @@ import subprocess
 
 logging.basicConfig(level=logging.DEBUG)
 
-gapic = gcp.GAPICGenerator()
+gapic = gcp.GAPICMicrogenerator()
 versions = ['v1beta']
 for version in versions:
-    library = gapic.node_library(
-        'os-login', version,
-        config_path="/google/cloud/oslogin/"
-                    f"artman_oslogin_{version}.yaml")
+    library = gapic.typescript_library(
+        'os-login', 
+        generator_args={
+            "grpc-service-config": f"google/cloud/oslogin/{version}/oslogin_grpc_service_config.json",
+            "package-name": f"@google-cloud/os-login"
+            },
+        proto_path=f'/google/cloud/oslogin/{version}',
+        extra_proto_files=['google/cloud/oslogin/common/common.proto'],
+        version=version)
 
     # skip index, protos, package.json, and README.md
     s.copy(
         library,
-        excludes=['package.json', 'README.md', 'src/index.js'],
+        excludes=['package.json', 'README.md', 'src/index.ts'],
     )
 
 # Copy common templates
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library()
+templates = common_templates.node_library(source_location='build/src')
 s.copy(templates)
 
 # Node.js specific cleanup
 subprocess.run(['npm', 'install'])
 subprocess.run(['npm', 'run', 'fix'])
+subprocess.run(['npm', 'compileProtos', 'src'])
