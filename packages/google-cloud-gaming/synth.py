4bb05f46b83e4c3184a9feee5267b86d000af528
Eric Schmidt <erschmid@google.com>
2020-02-10 13:56:48 -0800

    Initial client library creation

---
+++ packages/google-cloud-gaming/synth.py
@@ -0,0 +1,40 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This script is used to synthesize generated parts of this library."""
+import synthtool as s
+import synthtool.gcp as gcp
+import subprocess
+import logging
+logging.basicConfig(level=logging.DEBUG)
+# run the gapic generator
+gapic = gcp.GAPICMicrogenerator()
+versions = ['v1beta1']
+for version in versions:
+   library = gapic.typescript_library(
+     'gameservices',
+     generator_args={
+       'package-name': '@google-cloud/game-servers'
+     },
+     proto_path='/google/cloud/gaming/v1beta',
+     private=True,
+     version=version)
+s.copy(library, excludes=['package.json', 'README.md'])
+# Copy common templates
+common_templates = gcp.CommonTemplates()
+templates = common_templates.node_library(source_location='build/src')
+s.copy(templates, excludes=[])
+# Node.js specific cleanup
+subprocess.run(['npm', 'install'])
+subprocess.run(['npm', 'run', 'fix'])
+subprocess.run(['npx', 'compileProtos', 'src'])
