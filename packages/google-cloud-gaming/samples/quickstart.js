6bc706f166dff9eb22df6d6464418b57275a981f
Eric Schmidt <erschmid@google.com>
2020-02-21 14:50:55 -0800

    Adds quickstart

--- packages/google-cloud-gaming/samples/quickstart.js
+++ packages/google-cloud-gaming/samples/quickstart.js
@@ -14,13 +14,37 @@
 'use strict';
 
 /**
-* DESCRIBE WHAT THIS SAMPLE DOES.
-* @param {string} LIST EXPECTED ARGUMENTS.
-*/
-async function main() {
-// [START LIBRARY_NAME_quickstart]
+ * Create a Game Servers realm.
+ * @param {string} projectId string project identifier.
+ * @param {string} location Compute Engine region.
+ */
+async function main(projectId, location) {
+  // [START game_servers_quickstart]
+  const {RealmsServiceClient} = require('@google-cloud/game-servers');
 
-// [END LIBRARY_NAME_quickstart]
+  const client = new RealmsServiceClient();
+
+  const request = {
+    parent: `projects/${projectId}/locations/${location}`,
+    realmId: 'my-realm',
+    realm: {
+      // Must use a valid support time zone.
+      // See https://cloud.google.com/dataprep/docs/html/Supported-Time-Zone-Values_66194188
+      timeZone: 'US/Pacific',
+      description: 'My Game Server realm',
+    },
+  };
+
+  const [operation] = await client.createRealm(request);
+  const results = await operation.promise();
+  const [realm] = results;
+
+  console.log('Realm created:');
+
+  console.log(`\tRealm name: ${realm.name}`);
+  console.log(`\tRealm description: ${realm.description}`);
+  console.log(`\tRealm time zone: ${realm.timeZone}`);
+  // [END game_servers_quickstart]
 }
 
 main(...process.argv.slice(2)).catch(err => {
