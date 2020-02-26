57b6ad0d34dcabf968f8050e739679e52f4edbbb
Xiaozhen Liu <xiaozhenliu@google.com>
2020-02-26 15:47:24 -0800

    feat: export protos in src/index.ts

--- packages/google-cloud-recommender/src/index.ts
+++ packages/google-cloud-recommender/src/index.ts
@@ -22,3 +22,5 @@ export {v1, RecommenderClient};
 // For compatibility with JavaScript libraries we need to provide this default export:
 // tslint:disable-next-line no-default-export
 export default {v1, RecommenderClient};
+import * as protos from '../protos/protos';
+export {protos};
