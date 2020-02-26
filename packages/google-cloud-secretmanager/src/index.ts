a716c75335ccc13b7a2406452a794fce86dcfab8
Xiaozhen Liu <xiaozhenliu@google.com>
2020-02-26 15:51:23 -0800

    feat: export protos in src/index.ts

--- packages/google-cloud-secretmanager/src/index.ts
+++ packages/google-cloud-secretmanager/src/index.ts
@@ -22,3 +22,5 @@ export {v1beta1, SecretManagerServiceClient};
 // For compatibility with JavaScript libraries we need to provide this default export:
 // tslint:disable-next-line no-default-export
 export default {v1beta1, SecretManagerServiceClient};
+import * as protos from '../protos/protos';
+export {protos};
