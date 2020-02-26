8a8d286fa1270bea801d6ebb7599428ecdad45e3
Xiaozhen Liu <xiaozhenliu@google.com>
2020-02-26 15:49:18 -0800

    feat: export protos in src/index.ts

--- packages/google-cloud-redis/src/index.ts
+++ packages/google-cloud-redis/src/index.ts
@@ -24,3 +24,5 @@ export {v1, v1beta1, CloudRedisClient};
 // For compatibility with JavaScript libraries we need to provide this default export:
 // tslint:disable-next-line no-default-export
 export default {v1, v1beta1, CloudRedisClient};
+import * as protos from '../protos/protos';
+export {protos};
