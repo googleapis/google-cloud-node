bf38b341a92d3e130687f0bfe866d620de04a311
Xiaozhen Liu <xiaozhenliu@google.com>
2020-02-26 15:07:29 -0800

    feat: export protos in src/index.ts

--- packages/google-devtools-cloudbuild/src/index.ts
+++ packages/google-devtools-cloudbuild/src/index.ts
@@ -22,3 +22,5 @@ export {v1, CloudBuildClient};
 // For compatibility with JavaScript libraries we need to provide this default export:
 // tslint:disable-next-line no-default-export
 export default {v1, CloudBuildClient};
+import * as protos from '../protos/protos';
+export {protos};
