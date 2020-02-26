458538d27e5733e9fef645ea83dba759bf789296
Xiaozhen Liu <xiaozhenliu@google.com>
2020-02-26 15:19:35 -0800

    feat: export protos in src/index.ts

--- packages/google-cloud-iot/src/index.ts
+++ packages/google-cloud-iot/src/index.ts
@@ -22,3 +22,5 @@ export {v1, DeviceManagerClient};
 // For compatibility with JavaScript libraries we need to provide this default export:
 // tslint:disable-next-line no-default-export
 export default {v1, DeviceManagerClient};
+import * as protos from '../protos/protos';
+export {protos};
