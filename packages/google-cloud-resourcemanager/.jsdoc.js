6787bd6a610a7e66f3470fef249bf8f1c693435d
Summer Ji <summerji@google.com>
2020-02-27 21:42:49 -0800

    chore: correct .jsdoc.js protos and double quotes (#285)

--- packages/google-cloud-resourcemanager/.jsdoc.js
+++ packages/google-cloud-resourcemanager/.jsdoc.js
@@ -31,7 +31,7 @@ module.exports = {
   source: {
     excludePattern: '(^|\\/|\\\\)[._]',
     include: [
-      'build/src'
+      'build/src',
     ],
     includePattern: '\\.js$'
   },
@@ -42,7 +42,7 @@ module.exports = {
     systemName: '@google-cloud/resource',
     theme: 'lumen',
     default: {
-      "outputSourceFiles": false
+      outputSourceFiles: false
     }
   },
   markdown: {
