08f8c20a4dc9485eaff52e1ff579a4c4a5a88b8b
Xiaozhen Liu <xiaozhenliu@google.com>
2020-03-10 10:31:03 -0700

    feat: move library to Typescript code generation (#608)
    * remove docs/
    
    * update client,test,system-test
    
    * update config
    
    * update synth.py
    
    * run synthtool & disable TODO comment check
    
    * system-test pads
    
    * update system-test in synth.py
    
    * update dependency to latest

--- handwritten/nodejs-datastore/synth.py
+++ handwritten/nodejs-datastore/synth.py
@@ -5,20 +5,27 @@ import subprocess
 
 logging.basicConfig(level=logging.DEBUG)
 
-gapic = gcp.GAPICGenerator()
+gapic = gcp.GAPICMicrogenerator()
 version = 'v1'
-library = gapic.node_library(
+library = gapic.typescript_library(
     'datastore', version,
-    config_path='/google/datastore/artman_datastore.yaml')
+    generator_args={
+        "grpc-service-config": f"google/datastore/{version}/datastore_grpc_service_config.json",
+        "package-name": f"@google-cloud/datastore",
+        "main-service": f"datastore"
+        },
+        proto_path=f'/google/datastore/{version}',
+        extra_proto_files=['google/cloud/common_resources.proto'],
+    )
 
 # Copy everything except for top level index, package.json, and README.md
 s.copy(
     library,
-    excludes=['package.json', 'README.md', 'src/index.js'])
+    excludes=['package.json', 'README.md', 'src/index.ts'])
 
-# Update path discovery due to build/ dir and TypeScript conversion.
-s.replace("src/v1/datastore_client.js", "../../package.json", "../../../package.json")
-
+system_test_files=['system-test/fixtures/sample/src/index.ts','system-test/fixtures/sample/src/index.js']
+for file in system_test_files:
+    s.replace(file, 'DatastoreClient', 'Datastore')
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library(source_location="build/src")
 s.copy(templates)
@@ -26,3 +33,4 @@ s.copy(templates)
 # Node.js specific cleanup
 subprocess.run(['npm', 'install'])
 subprocess.run(['npm', 'run', 'fix'])
+subprocess.run(['npx', 'compileProtos', 'src'])
