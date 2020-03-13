7945109d4879ed68d02789624897970dfb099cdc
Jonathan Lui <jonathanlui@google.com>
2020-03-13 13:39:06 -0700

    refactor(test): rewrite flaky test in promises to see error message (#1103)
    * refactor(test): rewrite flaky test in promises to see error
    
    * awaits
    
    * must await assert.rejects
    
    * npm run fix

--- handwritten/storage/system-test/storage.ts
+++ handwritten/storage/system-test/storage.ts
@@ -233,20 +233,18 @@ describe('storage', () => {
       it('should not download a file', async () => {
         const [isPublic] = await file.isPublic();
         assert.strictEqual(isPublic, false);
-        assert.rejects(
+        await assert.rejects(
           file.download(),
           (err: Error) =>
             err.message.indexOf('does not have storage.objects.get') > -1
         );
       });
 
-      it('should not upload a file', done => {
-        file.save('new data', err => {
-          assert(
-            err!.message.indexOf('Could not load the default credentials') > -1
-          );
-          done();
-        });
+      it('should not upload a file', async () => {
+        await assert.rejects(
+          () => file.save('new data'),
+          /Could not load the default credentials/
+        );
       });
     });
   });
