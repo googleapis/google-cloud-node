4c1cad64c8a68701e69c2edfc85112f260e52c28
Eric Schmidt <erschmid@google.com>
2020-03-24 13:41:43 -0700

    feat: adds media translation file, mic samples
    * feat: adds streaming samples
    
    * fix: linter errors
    
    * fix: per reviewer
    
    * fix: per reviewer
    
    * fix: updates package.json

--- packages/google-cloud-mediatranslation/samples/quickstart.js
+++ packages/google-cloud-mediatranslation/samples/quickstart.js
@@ -1,38 +1,101 @@
-// Copyright 2020 Google LLC
-//
+// Copyright 2020, Google LLC.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     https://www.apache.org/licenses/LICENSE-2.0
+//    https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
 
 'use strict';
 
-async function quickstart() {
-  // parent = 'projects/my-project' // Project to list dashboards for.
+/**
+ * Translate text from an audio file.
+ * @param {string} filename local path to
+ * @param {string} encoding the encoding of the audio rate, e.g. Linear16
+ * @param {string} sourceLanguage language translating from, as BCP-47 code
+ * @param {string} targetLanguage languate translating to, as BCP-47 code
+ */
+function main(filename, encoding, sourceLanguage, targetLanguage) {
   // [START media_translation_quickstart]
-  // Imports the Google Cloud client library
+  const fs = require('fs');
+
+  // Imports the CLoud Media Translation client library
   const {
     SpeechTranslationServiceClient,
   } = require('@google-cloud/media-translation');
 
   // Creates a client
-  const translate = new SpeechTranslationServiceClient();
+  const client = new SpeechTranslationServiceClient();
 
-  // parent = 'projects/my-project', // Project to list dashboards for.
+  async function quickstart() {
+    /**
+     * TODO(developer): Uncomment the following lines before running the sample.
+     */
+    // const filename = 'Local path to audio file, e.g. /path/to/audio.raw';
+    // const encoding = 'Encoding of the audio file, e.g. LINEAR16';
+    // const sourceLanguage = 'BCP-47 source language code, e.g. en-US';
+    // const targetLangauge = 'BCP-47 target language code, e.g. es-ES';
 
-  // TODO: add an actual sample.
-  console.info(translate);
+    const config = {
+      audioConfig: {
+        audioEncoding: encoding,
+        sourceLanguageCode: sourceLanguage,
+        targetLanguageCode: targetLanguage,
+      },
+    };
 
-  // [END media_translation_quickstart]
+    // First request needs to have only a streaming config, no data.
+    const initialRequest = {
+      streamingConfig: config,
+      audioContent: null,
+    };
+
+    const readStream = fs.createReadStream(filename, {
+      highWaterMark: 4096,
+      encoding: 'base64',
+    });
+
+    const chunks = [];
+    readStream
+      .on('data', chunk => {
+        const request = {
+          streamingConfig: config,
+          audioContent: chunk.toString(),
+        };
+        chunks.push(request);
+      })
+      .on('close', () => {
+        // Config-only request should be first in stream of requests
+        stream.write(initialRequest);
+        for (let i = 0; i < chunks.length; i++) {
+          stream.write(chunks[i]);
+        }
+        stream.end();
+      });
+
+    const stream = client.streamingTranslateSpeech().on('data', response => {
+      const {result} = response;
+      if (result.textTranslationResult.isFinal) {
+        console.log(
+          `\nFinal translation: ${result.textTranslationResult.translation}`
+        );
+        console.log(`Final recognition result: ${result.recognitionResult}`);
+      } else {
+        console.log(
+          `\nPartial translation: ${result.textTranslationResult.translation}`
+        );
+        console.log(`Partial recognition result: ${result.recognitionResult}`);
+      }
+    });
+
+    // [END media_translation_quickstart]
+  }
+  quickstart();
 }
 
-const args = process.argv.slice(2);
-quickstart(...args).catch(console.error);
+main(...process.argv.slice(2));
