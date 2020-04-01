e61753febb08b080c807669f0acb28697c8c9a7e
Benjamin E. Coe <bencoe@google.com>
2020-03-31 18:39:28 -0700

    build: set AUTOSYNTH_MULTIPLE_COMMITS=true for context aware commits (#304)

--- packages/google-cloud-resourcemanager/synth.py
+++ packages/google-cloud-resourcemanager/synth.py
@@ -4,6 +4,9 @@ import logging
 import subprocess
 
 logging.basicConfig(level=logging.DEBUG)
+
+AUTOSYNTH_MULTIPLE_COMMITS = True
+
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library(source_location="build/src")
 s.copy(templates)
