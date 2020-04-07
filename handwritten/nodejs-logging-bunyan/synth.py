56e9917e95ce2c940e43ca96d743f3a2383c2b3b
Benjamin E. Coe <bencoe@google.com>
2020-04-06 21:08:28 -0700

    build: set AUTOSYNTH_MULTIPLE_COMMITS=true for context aware commits (#429)
    Co-authored-by: Justin Beckwith <justin.beckwith@gmail.com>

--- handwritten/nodejs-logging-bunyan/synth.py
+++ handwritten/nodejs-logging-bunyan/synth.py
@@ -17,6 +17,9 @@ import synthtool.gcp as gcp
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
+
+AUTOSYNTH_MULTIPLE_COMMITS = True
+
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library(source_location='build/src')
 s.copy(templates)
