ca7918493e4b5905d1b204e36492137d99eab2be
Alexander Fenster <fenster@google.com>
2020-04-10 18:50:29 -0700

    fix: remove eslint, update gax, fix generated protos, run the generator (#311)
    Run the latest version of the generator, update google-gax, update gts, and remove direct dependencies on eslint.

--- packages/google-cloud-asset/protos/protos.js
+++ packages/google-cloud-asset/protos/protos.js
@@ -28,7 +28,7 @@
     var $Reader = $protobuf.Reader, $Writer = $protobuf.Writer, $util = $protobuf.util;
     
     // Exported root namespace
-    var $root = $protobuf.roots._google_cloud_asset_2_2_0_protos || ($protobuf.roots._google_cloud_asset_2_2_0_protos = {});
+    var $root = $protobuf.roots._google_cloud_asset_3_0_0_protos || ($protobuf.roots._google_cloud_asset_3_0_0_protos = {});
     
     $root.google = (function() {
     
