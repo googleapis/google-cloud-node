12552d355fd27668a69d50411a77dadbc9944e18
Alexander Fenster <fenster@google.com>
2020-04-10 18:48:43 -0700

    fix: remove eslint, update gax, fix generated protos, run the generator (#133)
    Run the latest version of the generator, update google-gax, update gts, and remove direct dependencies on eslint.

--- packages/google-cloud-phishingprotection/test/gapic_phishing_protection_service_v1_beta1_v1beta1.ts
+++ packages/google-cloud-phishingprotection/test/gapic_phishing_protection_service_v1_beta1_v1beta1.ts
@@ -256,7 +256,7 @@ describe('v1beta1.PhishingProtectionServiceV1Beta1Client', () => {
         undefined,
         expectedError
       );
-      assert.rejects(async () => {
+      await assert.rejects(async () => {
         await client.reportPhishing(request);
       }, expectedError);
       assert(
