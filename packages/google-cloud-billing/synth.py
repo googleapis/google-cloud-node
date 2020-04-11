0d6621b0e66d0b6c7686198e1e66233a15156693
Alexander Fenster <fenster@google.com>
2020-04-10 18:48:09 -0700

    fix: remove eslint, update gax, fix generated protos, run the generator (#37)
    Run the latest version of the generator, update google-gax, update gts, and remove direct dependencies on eslint.

--- packages/google-cloud-billing/synth.py
+++ packages/google-cloud-billing/synth.py
@@ -46,4 +46,5 @@ s.copy(templates, excludes=[])
 
 # Node.js specific cleanup
 subprocess.run(['npm', 'install'])
-subprocess.run(['npm', 'run', 'fix'])
+subprocess.run(['npm', 'run', 'lint'])
+subprocess.run(['npx', 'compileProtos', 'src'])
