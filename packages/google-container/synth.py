a8acf8ca21622613181d85eb70d15c7095894fdb
Justin Beckwith <justin.beckwith@gmail.com>
2020-04-13 16:48:42 -0700

    fix: remove unused files from package (#333)

--- packages/google-container/synth.py
+++ packages/google-container/synth.py
@@ -30,5 +30,5 @@ s.copy(templates)
 
 # Node.js specific cleanup
 subprocess.run(['npm', 'install'])
-subprocess.run(['npm', 'run', 'lint'])
+subprocess.run(['npm', 'run', 'fix'])
 subprocess.run(['npm', 'run', 'compile-protos'])
