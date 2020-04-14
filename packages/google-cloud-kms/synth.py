3221e6aa641a5afc2de3d3b1776f934efe7848ff
Justin Beckwith <justin.beckwith@gmail.com>
2020-04-14 12:04:17 -0700

    chore: update lint rules and synth (#316)

--- packages/google-cloud-kms/synth.py
+++ packages/google-cloud-kms/synth.py
@@ -52,5 +52,5 @@ s.copy(templates)
 
 # Node.js specific cleanup
 subprocess.run(['npm', 'install'])
-subprocess.run(['npm', 'run', 'lint'])
+subprocess.run(['npm', 'run', 'fix'])
 subprocess.run(['npx', 'compileProtos', 'src'])
