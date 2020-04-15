ae25473c89e1a0e2b5a787ed9cb729987d3a3f24
Justin Beckwith <justin.beckwith@gmail.com>
2020-04-15 10:31:12 -0700

    chore: run fix instead of lint in synthfile (#77)

--- packages/google-monitoring-dashboard/synth.py
+++ packages/google-monitoring-dashboard/synth.py
@@ -43,6 +43,6 @@ s.copy(templates, excludes=['.github/release-please.yml'])
 
 # Node.js specific cleanup
 subprocess.run(['npm', 'install'])
-subprocess.run(['npm', 'run', 'lint'])
+subprocess.run(['npm', 'run', 'fix'])
 subprocess.run(['npx', 'compileProtos', 'src'])
 
