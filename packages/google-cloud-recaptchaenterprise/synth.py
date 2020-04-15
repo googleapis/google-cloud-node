1cb0dc2cd31b7efce469cc42a34e3c1db1ecc48e
Justin Beckwith <justin.beckwith@gmail.com>
2020-04-15 10:30:45 -0700

    chore: run fix instead of lint in synthfile (#164)

--- packages/google-cloud-recaptchaenterprise/synth.py
+++ packages/google-cloud-recaptchaenterprise/synth.py
@@ -45,5 +45,5 @@ s.copy(templates)
 
 # Node.js specific cleanup
 subprocess.run(['npm', 'install'])
-subprocess.run(['npm', 'run', 'lint'])
+subprocess.run(['npm', 'run', 'fix'])
 subprocess.run(['npx', 'compileProtos', 'src'])
