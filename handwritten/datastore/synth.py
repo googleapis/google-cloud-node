32b46c46fa0f7742eda833b6ce3b3c566e98e759
Xiaozhen Liu <xiaozhenliu@google.com>
2020-04-16 10:57:22 -0700

    chore: clean up synth.py (#656)
    * clean up synth.py
    
    * rerun synthtool
    
    * remove unneeded hacks in synth.py
    
    * rerun synthtool

--- handwritten/datastore/synth.py
+++ handwritten/datastore/synth.py
@@ -14,8 +14,8 @@ library = gapic.typescript_library(
     'datastore', version,
     generator_args={
         "grpc-service-config": f"google/datastore/{version}/datastore_grpc_service_config.json",
-        "package-name": f"@google-cloud/datastore",
-        "main-service": f"datastore"
+        "package-name": "@google-cloud/datastore",
+        "main-service": "datastore"
         },
         proto_path=f'/google/datastore/{version}',
         extra_proto_files=['google/cloud/common_resources.proto'],
@@ -24,14 +24,14 @@ library = gapic.typescript_library(
 # Copy everything except for top level index, package.json, and README.md
 s.copy(
     library,
-    excludes=['package.json', 'README.md', 'src/index.ts', '.eslintignore'])
+    excludes=['package.json', 'README.md', 'src/index.ts'])
 
 system_test_files=['system-test/fixtures/sample/src/index.ts','system-test/fixtures/sample/src/index.js']
 for file in system_test_files:
     s.replace(file, 'DatastoreClient', 'Datastore')
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library(source_location="build/src")
-s.copy(templates, excludes=['.eslintignore'])
+s.copy(templates)
 
 # Node.js specific cleanup
 subprocess.run(['npm', 'install'])
