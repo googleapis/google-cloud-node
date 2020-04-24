325dacd161256aedaa96bd3e4d27913a6794fa81
Alexander Fenster <fenster@google.com>
2020-04-23 19:33:51 -0700

    chore: update npm scripts and synth.py (#356)
    Update npm scripts: add clean, prelint, prefix; make sure that lint and fix are set properly. Use post-process feature of synthtool.

--- packages/google-cloud-bigquery-datatransfer/synth.py
+++ packages/google-cloud-bigquery-datatransfer/synth.py
@@ -1,7 +1,7 @@
 import synthtool as s
 import synthtool.gcp as gcp
+import synthtool.languages.node as node
 import logging
-import subprocess
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -34,6 +34,4 @@ common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library(source_location='build/src')
 s.copy(templates)
 
-subprocess.run(['npm', 'install'])
-subprocess.run(['npm', 'run', 'fix'])
-subprocess.run(['npx', 'compileProtos', 'src'])
+node.postprocess_gapic_library()
