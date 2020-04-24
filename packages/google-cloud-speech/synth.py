bd966638fa9f4c82a72520b05e0c3754530aff83
Alexander Fenster <fenster@google.com>
2020-04-23 19:32:02 -0700

    chore: update npm scripts and synth.py (#587)
    Update npm scripts: add clean, prelint, prefix; make sure that lint and fix are set properly. Use post-process feature of synthtool.

--- packages/google-cloud-speech/synth.py
+++ packages/google-cloud-speech/synth.py
@@ -16,8 +16,8 @@
 
 import synthtool as s
 import synthtool.gcp as gcp
+import synthtool.languages.node as node
 import logging
-import subprocess
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -62,8 +62,4 @@ templates = common_templates.node_library(source_location='build/src')
 s.copy(templates)
 
 #
-# Node.js specific cleanup
-#
-subprocess.run(['npm', 'install'])
-subprocess.run(['npm', 'run', 'fix'])
-subprocess.run(['npx', 'compileProtos', 'src'])
+node.postprocess_gapic_library()
