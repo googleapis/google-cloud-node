ed7c52f40cb9d3b9379f9257136f209feec8da28
Summer Ji <summerji@google.com>
2020-05-04 17:03:06 -0700

    fix: synth.py clean up for multiple version (#326)

--- packages/google-cloud-asset/synth.py
+++ packages/google-cloud-asset/synth.py
@@ -37,31 +37,36 @@ for version in versions:
         },
         # This API has dependencies outside of its own folder so we list them here.
         # Switching to bazel build should help get rid of this.
-        extra_proto_files=['google/cloud/common_resources.proto', 'google/cloud/orgpolicy/v1', 'google/identity/accesscontextmanager'],
+        extra_proto_files=['google/cloud/common_resources.proto',
+                           'google/cloud/orgpolicy/v1', 'google/identity/accesscontextmanager'],
         version=version),
     # skip index, protos, package.json, and README.md
     s.copy(
         library,
-        excludes=['package.json', 'src/index.ts']
+        excludes=['package.json']
     )
 
 # Copy common templates
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library(source_location='build/src')
+templates = common_templates.node_library(
+    source_location='build/src', versions=versions, default_version='v1')
 s.copy(templates)
 
 # Extra proto dependencies make the *_proto_list.json have some common files
 # that conflict with the same common files imported from gax. This is likely
 # a generator problem that needs to be fixed when we start handling synth.py
 # custom fixes.
-proto_lists=[f'src/{version}/asset_service_proto_list.json' for version in versions]
-remove_proto_keywords=['/google/api', '/google/protobuf', '/google/rpc', '/google/type']
+proto_lists = [
+    f'src/{version}/asset_service_proto_list.json' for version in versions]
+remove_proto_keywords = ['/google/api',
+                         '/google/protobuf', '/google/rpc', '/google/type']
 for file in proto_lists:
-  with open(file, 'r') as f:
-    items=json.load(f)
-    content =[item for item in items if all([(x not in item) for x in remove_proto_keywords])]
-    new_file=json.dumps(content, indent=2) + '\n'
-  with open(file, 'w') as f:
-    f.write(new_file)
+    with open(file, 'r') as f:
+        items = json.load(f)
+        content = [item for item in items if all(
+            [(x not in item) for x in remove_proto_keywords])]
+        new_file = json.dumps(content, indent=2) + '\n'
+    with open(file, 'w') as f:
+        f.write(new_file)
 
 node.postprocess_gapic_library()
