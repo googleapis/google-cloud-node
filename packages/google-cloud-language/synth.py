28eb2e7a01b15c762ffbd70e152ada95a222c95c
Summer Ji <summerji@google.com>
2020-05-06 16:37:54 -0700

    fix: synth.py clean up for multiple version (#463)

--- packages/google-cloud-language/synth.py
+++ packages/google-cloud-language/synth.py
@@ -9,25 +9,27 @@ AUTOSYNTH_MULTIPLE_COMMITS = True
 
 
 gapic = gcp.GAPICMicrogenerator()
+versions = ['v1', 'v1beta2']
 # tasks has two product names, and a poorly named artman yaml
-for version in ['v1', 'v1beta2']:
+for version in versions:
     library = gapic.typescript_library(
         'language',
         generator_args={
             "grpc-service-config": f"google/cloud/language/{version}/language_grpc_service_config.json",
-            "package-name":f"@google-cloud/language"
-            },
+            "package-name": f"@google-cloud/language"
+        },
         proto_path=f'/google/cloud/language/{version}',
         version=version)
 
     # skip index, protos, package.json, and README.md
     s.copy(
         library,
-        excludes=['package.json', 'README.md', 'src/index.ts'])
+        excludes=['package.json', 'README.md'])
 
 # Update common templates
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library(source_location='build/src')
+templates = common_templates.node_library(
+    source_location='build/src', versions=versions, default_version='v1')
 s.copy(templates)
 
 node.postprocess_gapic_library()
