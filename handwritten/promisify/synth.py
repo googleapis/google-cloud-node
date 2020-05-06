7c8df20193c1c1da1c62dfbb1caa2223f82438f9
Alexander Fenster <fenster@google.com>
2020-05-05 17:31:44 -0700

    chore: update npm scripts and synth.py (#197)
    * chore: update npm scripts and synth.py
    
    Update npm scripts: add clean, prelint, prefix; make sure that lint and fix are set properly. Use post-process feature of synthtool.
    
    * fix: broken tsconfig.json

--- handwritten/promisify/synth.py
+++ handwritten/promisify/synth.py
@@ -1,5 +1,6 @@
 import synthtool as s
 import synthtool.gcp as gcp
+import synthtool.languages.node as node
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -8,3 +9,5 @@ AUTOSYNTH_MULTIPLE_COMMITS = True
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library()
 s.copy(templates)
+node.install()
+node.fix()
