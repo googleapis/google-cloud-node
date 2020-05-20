535ffe7124b73d23c756919b5e00ec06188071c2
Alexander Fenster <fenster@google.com>
2020-05-20 15:46:17 -0700

    chore: update npm scripts and synth.py (#640)
    Update npm scripts: add clean, prelint, prefix; make sure that lint and fix are set properly. Use post-process feature of synthtool.
    
    Co-authored-by: Justin Beckwith <justin.beckwith@gmail.com>

--- handwritten/devtools-cloudprofiler/synth.py
+++ handwritten/devtools-cloudprofiler/synth.py
@@ -1,5 +1,6 @@
 import synthtool as s
 import synthtool.gcp as gcp
+import synthtool.languages.node as node
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -8,3 +9,5 @@ AUTOSYNTH_MULTIPLE_COMMITS = True
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library(source_location='build/src')
 s.copy(templates, excludes=[".github/workflows/ci.yaml"])
+node.install()
+node.fix()
