93625aef35646039d167f6504a9e398817f8de57
Jonathan Lui <jonathanlui@google.com>
2020-05-21 10:00:58 -0700

    samples: enable bucket lifecycle management (#1195)
    * samples: enable bucket lifecycle management
    
    * fix
    
    * assert.match => include
    
    * update sample to 100 days
    
    * typo
    
    Co-authored-by: Justin Beckwith <justin.beckwith@gmail.com>

---
+++ handwritten/storage/samples/enableBucketLifecycleManagement.js
@@ -0,0 +1,52 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * This application demonstrates how to enable Object Lifecycle Management for
+ * a bucket.
+ *
+ * For more information, see the documentation at https://cloud.google.com/storage/docs/lifecycle.
+ */
+
+function main(bucketName = 'my-bucket') {
+  // [START storage_enable_bucket_lifecycle_management]
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+  // const bucketName = 'Name of a bucket, e.g. my-bucket';
+
+  // Imports the Google Cloud client library
+  const {Storage} = require('@google-cloud/storage');
+
+  // Creates a client
+  const storage = new Storage();
+
+  async function enableBucketLifecycleManagement() {
+    const [metadata] = await storage.bucket(bucketName).addLifecycleRule({
+      action: 'delete',
+      condition: {age: 100},
+    });
+
+    console.log(
+      `Lifecycle management is enabled for bucket ${bucketName} and the rules are:`
+    );
+
+    console.log(metadata.lifecycle.rule);
+  }
+
+  enableBucketLifecycleManagement().catch(console.error);
+  // [END storage_enable_bucket_lifecycle_management]
+}
+
+main(...process.argv.slice(2));
---
+++ handwritten/storage/samples/system-test/bucketLifecycle.test.js
@@ -0,0 +1,56 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const {Storage} = require('@google-cloud/storage');
+const {assert} = require('chai');
+const {before, beforeEach, after, describe, it} = require('mocha');
+const cp = require('child_process');
+const uuid = require('uuid');
+
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
+
+const storage = new Storage();
+const bucketName = `nodejs-storage-samples-${uuid.v4()}`;
+const bucket = storage.bucket(bucketName);
+
+describe('Bucket lifecycle management', () => {
+  before(async () => {
+    await bucket.create();
+  });
+
+  beforeEach(async () => {
+    await bucket.setMetadata({lifecycle: null});
+  });
+
+  after(async () => {
+    await bucket.delete().catch(console.error);
+  });
+
+  it('should add a lifecycle delete rule', async () => {
+    const output = execSync(
+      `node enableBucketLifecycleManagement.js ${bucketName}`
+    );
+    assert.include(
+      output,
+      `Lifecycle management is enabled for bucket ${bucketName} and the rules are:`
+    );
+    const [metadata] = await bucket.getMetadata();
+    assert.deepStrictEqual(metadata.lifecycle.rule[0], {
+      action: {type: 'Delete'},
+      condition: {age: 100},
+    });
+  });
+});
