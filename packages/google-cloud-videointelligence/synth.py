59efd7aa41ad62f70b4a2854801412038f6e0037
Summer Ji <summerji@google.com>
2020-06-02 15:42:31 -0700

    fix!: synth.py clean up for multiple version (#436)

--- packages/google-cloud-videointelligence/synth.py
+++ packages/google-cloud-videointelligence/synth.py
@@ -11,8 +11,14 @@ gapic = gcp.GAPICMicrogenerator()
 common_templates = gcp.CommonTemplates()
 
 versions = ["v1", "v1beta2", "v1p1beta1", "v1p2beta1", "v1p3beta1"]
+default_version = "v1"
 
-for version in versions:
+# Rearrange the default version to the last item in the array, to generate appropriate system-test
+order_versions = versions.copy()
+order_versions.append(order_versions.pop(
+    order_versions.index(default_version)))
+
+for version in order_versions:
     library = gapic.typescript_library(
         "video-intelligence",
         version,
@@ -25,9 +31,10 @@ for version in versions:
     )
 
     # skip index, protos, package.json, and README.md
-    s.copy(library, excludes=["package.json", "README.md", "src/index.ts"])
+    s.copy(library, excludes=["package.json", "README.md"])
 
-templates = common_templates.node_library(source_location='build/src')
+templates = common_templates.node_library(
+    source_location="build/src", versions=versions, default_version=default_version)
 s.copy(templates)
 
 node.postprocess_gapic_library()
