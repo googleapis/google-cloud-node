959047b88ef3b62c72d4a672387c9453d2655696
Stephen <stephenplusplus@users.noreply.github.com>
2020-06-16 13:22:47 -0400

    docs: add "support_documentation" to libraries.json

--- bin/generate-readme.js
+++ bin/generate-readme.js
@@ -66,21 +66,52 @@ async function collectRepoMetadata (repos) {
 // Fills in README.mustache with contents loaded from sloth/repos.json.
 // Given the simplicity of the template, we do not actually use a templating
 // engine, instead calling string.replace.
-function generateReadme (repoMetadata) {
+async function generateReadme (repoMetadata) {
   const template = readFileSync('./bin/README.mustache', 'utf8');
   const libraries = [];
 
   // filter libraries to only contain those with Google Cloud api_id,
   // standardizing naming along the way.
-  Object.keys(repoMetadata).forEach(key => {
-    const metadata = repoMetadata[key];
-    if (metadata.api_id) {
-      // making naming more consistent, sometimes we've appended Cloud,
-      // sometimes Google Cloud.
-      metadata.name_pretty = metadata.name_pretty.replace(/^(Google )?Cloud /, '');
-      libraries.push(metadata);
+  for (const repoMetadataKey in repoMetadata) {
+    const metadata = repoMetadata[repoMetadataKey];
+
+    if (!metadata.api_id) {
+      continue;
     }
-  });
+
+    // making naming more consistent, sometimes we've appended Cloud,
+    // sometimes Google Cloud.
+    metadata.name_pretty = metadata.name_pretty.replace(/^(Google )?Cloud /, '');
+
+    if (metadata.product_documentation) {
+      // add a link to the "Getting Support" page on the docs
+      // examples:
+      //     input: https://cloud.google.com/container-registry/docs/container-analysis
+      //     output: https://cloud.google.com/container-registry/docs/getting-support
+      //     input: https://cloud.google.com/natural-language/docs/
+      //     output: https://cloud.google.com/natural-language/docs/getting-support
+      let supportDocsUrl = metadata.product_documentation
+        // guarantee trailing /
+        .replace(/\/*$/, '/')
+        // append "docs/getting-support" path, if not already there
+        // this also strips anything else found after "docs/"
+        .replace(/(docs\/(.+)*)*$/, 'docs/getting-support');
+
+      // multiple product docs point to the same docs page
+      if (metadata.name_pretty.toLowerCase().trim().startsWith('stackdriver')) {
+        supportDocsUrl = 'https://cloud.google.com/stackdriver/docs/getting-support';
+      }
+
+      // if URL doesn't exist, fall back to the generic docs page
+      const remoteUrlExists = (await fetch(supportDocsUrl, {method: 'HEAD'})).status !== 404;
+      if (!remoteUrlExists) {
+        supportDocsUrl = metadata.product_documentation;
+      }
+      metadata.support_documentation = supportDocsUrl;
+    }
+
+    libraries.push(metadata);
+  }
 
   libraries.sort((a, b) => {
     return a.name_pretty.localeCompare(b.name_pretty);
@@ -101,7 +132,7 @@ require('yargs')
     const repos = (await getContent('googleapis', 'sloth', 'repos.json')).repos;
     checkpoint('loaded list of repos from sloth');
     const repoMetadata = await collectRepoMetadata(repos);
-    generateReadme(repoMetadata);
+    await generateReadme(repoMetadata);
   })
   .option('token', {
     describe: 'GitHub authorization token'
