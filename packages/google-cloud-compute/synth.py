10c3824a7244902db80c776c37e89df12cc2b55c
Benjamin E. Coe <bencoe@google.com>
2020-06-21 14:50:07 -0700

    build: typo in synth.py breaking autosynth (#455)
    fixes #454

--- packages/google-cloud-compute/synth.py
+++ packages/google-cloud-compute/synth.py
@@ -9,7 +9,7 @@ AUTOSYNTH_MULTIPLE_COMMITS = True
 
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library()
-s.copy(templates, exclude=[
+s.copy(templates, excludes=[
   '.mocharc.js'
 ])
 node.install()
