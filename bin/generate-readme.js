0f79c47b3e6b0cb1c8109ca6452a345e00b1aaf7
Justin Beckwith <justin.beckwith@gmail.com>
2020-06-24 13:30:29 -0700

    chore: drop usage of repos.json (#3013)

--- bin/generate-readme.js
+++ bin/generate-readme.js
@@ -14,50 +14,42 @@
 // limitations under the License.
 
 const chalk = require('chalk');
-const fetch = require('node-fetch');
+const { request, Gaxios } = require('gaxios');
 const figures = require('figures');
 const { readFileSync, writeFileSync } = require('fs');
+const parseLinkHeader = require('parse-link-header');
 
-let argv = null;
-const apiUrl = 'https://api.github.com';
+const token = process.env.GITHUB_TOKEN;
+if (!token) {
+  throw new Error('Please include a GITHUB_TOKEN env var.');
+}
+const baseUrl = 'https://api.github.com';
+const github = new Gaxios({
+  headers: {
+    authorization: `token ${token}`
+  }
+});
 
 function checkpoint (message, success = true) {
   const prefix = success ? chalk.green(figures.tick) : chalk.red(figures.cross);
   console.info(`${prefix} ${message}`);
 }
 
-// grab files using GitHub API.
-async function getContent (owner, repo, path) {
-  const headers = {};
-  const token = argv.token || process.env.GITHUB_TOKEN;
-  if (token) headers.authorization = `token ${token}`;
-  const res = await fetch(`${apiUrl}/repos/${owner}/${repo}/contents/${path}`, {
-    headers: headers
-  });
-  if (res.status !== 200) {
-    const err = Error(`unexpected status = ${res.status}`);
-    err.status = res.status;
-    throw err;
-  }
-  const content = (await res.json()).content;
-  return JSON.parse(
-    Buffer.from(content, 'base64').toString('utf8')
-  );
-}
-
-// for each repo listed in sloth/repos.json pull down .repo-metadata.json.
 async function collectRepoMetadata (repos) {
   const repoMetadata = {};
-  for (let i = 0, repo; (repo = repos[i]) !== undefined; i++) {
-    if (repo.language === 'nodejs') {
-      try {
-        const [o, r] = repo.repo.split('/');
-        repoMetadata[r] = await getContent(o, r, '.repo-metadata.json');
-        checkpoint(`${repo.repo} found .repo-metadata.json`);
-      } catch (err) {
-        if (err.status === 404) checkpoint(`${repo.repo} had no .repo-metadata.json`, false);
-        else throw err;
+  for (const repo of repos) {
+    try {
+      const url = `${baseUrl}/repos/${repo}/contents/.repo-metadata.json`;
+      const res = await github.request({ url });
+      repoMetadata[repo] = JSON.parse(
+        Buffer.from(res.data.content, 'base64').toString('utf8')
+      );
+      checkpoint(`${repo} found .repo-metadata.json`);
+    } catch (err) {
+      if (!err.response || err.response.status !== 404) {
+        throw err;
       }
+      checkpoint(`${repo} had no .repo-metadata.json`, false);
     }
   }
   return repoMetadata;
@@ -103,7 +95,12 @@ async function generateReadme (repoMetadata) {
       }
 
       // if URL doesn't exist, fall back to the generic docs page
-      const remoteUrlExists = (await fetch(supportDocsUrl, {method: 'HEAD'})).status !== 404;
+      const res = await request({
+        url: supportDocsUrl,
+        method: 'HEAD',
+        validateStatus: () => true
+      });
+      const remoteUrlExists = res.status !== 404;
       if (!remoteUrlExists) {
         supportDocsUrl = metadata.product_documentation;
       }
@@ -126,15 +123,30 @@ async function generateReadme (repoMetadata) {
   writeFileSync('./README.md', template.replace('{{libraries}}', partial), 'utf8');
 }
 
-require('yargs')
-  .command('$0', 'generate README from sloth list of repos', () => {}, async (_argv) => {
-    argv = _argv;
-    const repos = (await getContent('googleapis', 'sloth', 'repos.json')).repos;
-    checkpoint('loaded list of repos from sloth');
-    const repoMetadata = await collectRepoMetadata(repos);
-    await generateReadme(repoMetadata);
-  })
-  .option('token', {
-    describe: 'GitHub authorization token'
-  })
-  .parse();
+async function getRepos () {
+  const q = 'nodejs in:.repo-metadata.json org:googleapis is:public archived:false';
+  let url = new URL('/search/repositories', baseUrl);
+  url.searchParams.set('q', q);
+  url.searchParams.set('per_page', 100);
+  const repos = [];
+  while (url) {
+    const res = await github.request({ url: url.href });
+    repos.push(...res.data.items.map(r => r.full_name));
+    url = null;
+    if (res.headers['link']) {
+      const link = parseLinkHeader(res.headers['link']);
+      if (link.next) {
+        url = new URL(link.next.url);
+      }
+    }
+  }
+  return repos;
+}
+
+async function main () {
+  const repos = await getRepos();
+  checkpoint(`Discovered ${repos.length} node.js repos with metadata`);
+  const repoMetadata = await collectRepoMetadata(repos);
+  await generateReadme(repoMetadata);
+}
+main().catch(console.error);
