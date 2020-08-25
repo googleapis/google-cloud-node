f5e5d1c08a24c0ed29b2c8bc673deca9788e0e8d
Benjamin E. Coe <bencoe@google.com>
2020-08-25 14:26:14 -0700

    build: update kokoro config/unblock rotted test (#190)

--- packages/google-cloud-phishingprotection/samples/test/quickstart.js
+++ packages/google-cloud-phishingprotection/samples/test/quickstart.js
@@ -28,7 +28,13 @@ const URI = 'http://testsafebrowsing.appspot.com/s/phishing.html';
 
 describe('Quickstart', () => {
   it('should run quickstart', async () => {
-    const stdout = execSync(`node ./quickstart.js ${URI} ${PROJECT_ID}`, {cwd});
-    assert.include(stdout, 'reported');
+    try {
+      execSync(`node ./quickstart.js ${URI} ${PROJECT_ID}`, {
+        cwd,
+      });
+      assert('unreachable');
+    } catch (err) {
+      assert.match(err.message, /ALREADY_EXISTS/);
+    }
   });
 });
