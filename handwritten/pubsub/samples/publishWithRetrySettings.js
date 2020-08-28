83832c7942d44cfbb34469299d7104a259f7ee32
Lalji Kanjareeya <46327204+laljikanjareeya@users.noreply.github.com>
2020-08-28 20:58:19 +0530

    docs: retry setting docs on sample (#1100)

--- handwritten/pubsub/samples/publishWithRetrySettings.js
+++ handwritten/pubsub/samples/publishWithRetrySettings.js
@@ -81,12 +81,24 @@ function main(
         2, // 'UNKNOWN'
       ],
       backoffSettings: {
+        // The initial delay time, in milliseconds, between the completion
+        // of the first failed request and the initiation of the first retrying request.
         initialRetryDelayMillis: 100,
+        // The multiplier by which to increase the delay time between the completion
+        // of failed requests, and the initiation of the subsequent retrying request.
         retryDelayMultiplier: 1.3,
+        // The maximum delay time, in milliseconds, between requests.
+        // When this value is reached, retryDelayMultiplier will no longer be used to increase delay time.
         maxRetryDelayMillis: 60000,
+        // The initial timeout parameter to the request.
         initialRpcTimeoutMillis: 5000,
+        // The multiplier by which to increase the timeout parameter between failed requests.
         rpcTimeoutMultiplier: 1.0,
+        // The maximum timeout parameter, in milliseconds, for a request. When this value is reached,
+        // rpcTimeoutMultiplier will no longer be used to increase the timeout.
         maxRpcTimeoutMillis: 600000,
+        // The total time, in milliseconds, starting from when the initial request is sent,
+        // after which an error will be returned, regardless of the retrying attempts made meanwhile.
         totalTimeoutMillis: 600000,
       },
     };
