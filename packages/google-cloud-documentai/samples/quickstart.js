aaf8d20ba4b9cd92d093f408a238f94e1f3985ed
Eric Schmidt <erschmid@google.com>
2020-10-15 12:18:40 -0700

    docs(samples): new Doc AI samples for v1beta3 (#101)
    * docs(samples): new Doc AI samples for v1beta3
    * feat: add a new version to the library
    * fix: adds processor ID as literal in tests
    * fix: removed apiEndpoint from client instantiation
    
    Co-authored-by: Sofia Leon <sofialeon@google.com>
    Co-authored-by: sofisl <55454395+sofisl@users.noreply.github.com>
    Co-authored-by: Justin Beckwith <justin.beckwith@gmail.com>

--- packages/google-cloud-documentai/samples/quickstart.js
+++ packages/google-cloud-documentai/samples/quickstart.js
@@ -15,56 +15,72 @@
 
 'use strict';
 
-async function main(
-  projectId,
-  location,
-  gcsInputUri = 'gs://cloud-samples-data/documentai/invoice.pdf'
-) {
+async function main(projectId, location, processorId, filePath) {
   // [START documentai_quickstart]
   /**
    * TODO(developer): Uncomment these variables before running the sample.
    */
   // const projectId = 'YOUR_PROJECT_ID';
   // const location = 'YOUR_PROJECT_LOCATION'; // Format is 'us' or 'eu'
-  // const gcsInputUri = 'YOUR_SOURCE_PDF';
+  // const processor = 'YOUR_PROCESSOR_ID'; // Create processor in Cloud Console
+  // const filePath = '/path/to/local/pdf';
 
   const {
-    DocumentUnderstandingServiceClient,
-  } = require('@google-cloud/documentai').v1beta2;
-  const client = new DocumentUnderstandingServiceClient();
+    DocumentProcessorServiceClient,
+  } = require('@google-cloud/documentai').v1beta3;
+
+  // Instantiates a client
+  const client = new DocumentProcessorServiceClient();
 
   async function quickstart() {
-    // Configure the request for processing the PDF
-    const parent = `projects/${projectId}/locations/${location}`;
+    // The full resource name of the processor, e.g.:
+    // projects/project-id/locations/location/processor/processor-id
+    // You must create new processors in the Cloud Console first
+    const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
+
+    // Read the file into memory.
+    const fs = require('fs').promises;
+    const imageFile = await fs.readFile(filePath);
+
+    // Convert the image data to a Buffer and base64 encode it.
+    const encodedImage = Buffer.from(imageFile).toString('base64');
+
     const request = {
-      parent,
-      inputConfig: {
-        gcsSource: {
-          uri: gcsInputUri,
-        },
+      name,
+      document: {
+        content: encodedImage,
         mimeType: 'application/pdf',
       },
     };
 
     // Recognizes text entities in the PDF document
     const [result] = await client.processDocument(request);
+    const {document} = result;
 
     // Get all of the document text as one big string
-    const {text} = result;
+    const {text} = document;
 
     // Extract shards from the text field
-    function extractText(textAnchor) {
+    const getText = textAnchor => {
+      if (!textAnchor.textSegments || textAnchor.textSegments.length === 0) {
+        return '';
+      }
+
       // First shard in document doesn't have startIndex property
       const startIndex = textAnchor.textSegments[0].startIndex || 0;
       const endIndex = textAnchor.textSegments[0].endIndex;
 
       return text.substring(startIndex, endIndex);
-    }
+    };
 
-    for (const entity of result.entities) {
-      console.log(`\nEntity text: ${extractText(entity.textAnchor)}`);
-      console.log(`Entity type: ${entity.type}`);
-      console.log(`Entity mention text: ${entity.mentionText}`);
+    // Read the text recognition output from the processor
+    console.log('The document contains the following paragraphs:');
+    const [page1] = document.pages;
+    const {paragraphs} = page1;
+
+    for (const paragraph of paragraphs) {
+      const paragraphText = getText(paragraph.layout.textAnchor);
+      console.log(`Paragraph text:\n${paragraphText}`);
     }
   }
   // [END documentai_quickstart]
--- packages/google-cloud-documentai/samples/test/quickstart.test.js
+++ packages/google-cloud-documentai/samples/test/quickstart.test.js
@@ -16,20 +16,38 @@
 'use strict';
 
 const path = require('path');
-const {assert} = require('chai');
+const assert = require('assert');
 const cp = require('child_process');
+
+const {
+  DocumentProcessorServiceClient,
+} = require('@google-cloud/documentai').v1beta3;
+const client = new DocumentProcessorServiceClient({
+  apiEndpoint: 'us-documentai.googleapis.com',
+});
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 const cwd = path.join(__dirname, '..');
-const projectId = process.env.GCLOUD_PROJECT;
 const LOCATION = 'us';
+const PROCESSOR_ID = '8f1123c1b125e0b7';
+
+const fileName = 'invoice.pdf';
+const filePath = path.resolve(path.join(__dirname, `../resources/${fileName}`));
 
 describe('Quickstart', () => {
+  let projectId;
+  before(async () => {
+    projectId = await client.getProjectId();
+  });
+
   it('should run quickstart', async () => {
-    const stdout = execSync(`node ./quickstart.js ${projectId} ${LOCATION}`, {
-      cwd,
-    });
-    assert.match(stdout, /Entity/);
+    const stdout = execSync(
+      `node ./quickstart.js ${projectId} ${LOCATION} ${PROCESSOR_ID} ${filePath}`,
+      {
+        cwd,
+      }
+    );
+    assert.notStrictEqual(stdout.indexOf('Paragraph'), -1);
   });
 });
