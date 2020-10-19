a30317bc3376a93ae19c6ee1a73a66f3cf410787
Sameena Shaffeeullah <shaffeeullah@google.com>
2020-10-19 15:37:44 -0700

    fix: self-upload files for Unicode system tests (#1318)
    * fix: self-upload files for Unicode system tests
    
    * refactored file variable in response to PR comments
    
    Co-authored-by: Stephen <stephenplusplus@users.noreply.github.com>
    
    Co-authored-by: Stephen <stephenplusplus@users.noreply.github.com>

--- handwritten/storage/system-test/storage.ts
+++ handwritten/storage/system-test/storage.ts
@@ -880,17 +880,18 @@ describe('storage', () => {
 
     let bucket: Bucket;
 
-    before(() => {
-      bucket = storage.bucket('storage-library-test-bucket');
+    before(async () => {
+      [bucket] = await storage.createBucket(generateName());
     });
 
     // Normalization form C: a single character for e-acute;
     // URL should end with Cafe%CC%81
-    it('should not perform normalization form C', () => {
+    it('should not perform normalization form C', async () => {
       const name = 'Caf\u00e9';
-      const file = bucket.file(name);
-
       const expectedContents = 'Normalization Form C';
+
+      const file = bucket.file(name);
+      await file.save(expectedContents);
 
       return file
         .get()
@@ -906,11 +907,12 @@ describe('storage', () => {
 
     // Normalization form D: an ASCII character followed by U+0301 combining
     // character; URL should end with Caf%C3%A9
-    it('should not perform normalization form D', () => {
+    it('should not perform normalization form D', async () => {
       const name = 'Cafe\u0301';
-      const file = bucket.file(name);
-
       const expectedContents = 'Normalization Form D';
+
+      const file = bucket.file(name);
+      await file.save(expectedContents);
 
       return file
         .get()
