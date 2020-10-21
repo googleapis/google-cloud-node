f9b2e614b59f1820a76fb412aa83f7b0441aac01
Justin Beckwith <justin.beckwith@gmail.com>
2020-10-21 13:28:09 -0700

    docs(samples): update samples to latest standards (#475)

--- packages/google-cloud-tasks/samples/quickstart.js
+++ packages/google-cloud-tasks/samples/quickstart.js
@@ -13,12 +13,11 @@
 // limitations under the License.
 
 'use strict';
-/*eslint no-warning-comments: [0, { "terms": ["todo", "fixme"], "location": "anywhere" }]*/
 
 /**
  * Create a task for a given queue with an arbitrary payload.
  */
-async function createTask(project, location, queue, payload, inSeconds) {
+function main(project, location, queue, payload, inSeconds) {
   // [START tasks_quickstart]
   // Imports the Google Cloud Tasks library.
   const {CloudTasksClient} = require('@google-cloud/tasks');
@@ -26,45 +25,52 @@ async function createTask(project, location, queue, payload, inSeconds) {
   // Instantiates a client.
   const client = new CloudTasksClient();
 
-  // TODO(developer): Uncomment these lines and replace with your values.
-  // const project = 'my-project-id';
-  // const queue = 'my-appengine-queue';
-  // const location = 'us-central1';
-  // const payload = 'hello';
+  async function quickstart() {
+    // TODO(developer): Uncomment these lines and replace with your values.
+    // const project = 'my-project-id';
+    // const queue = 'my-appengine-queue';
+    // const location = 'us-central1';
+    // const payload = 'hello';
 
-  // Construct the fully qualified queue name.
-  const parent = client.queuePath(project, location, queue);
+    // Construct the fully qualified queue name.
+    const parent = client.queuePath(project, location, queue);
 
-  const task = {
-    appEngineHttpRequest: {
-      httpMethod: 'POST',
-      relativeUri: '/log_payload',
-    },
-  };
-
-  if (payload) {
-    task.appEngineHttpRequest.body = Buffer.from(payload).toString('base64');
-  }
-
-  if (inSeconds) {
-    task.scheduleTime = {
-      seconds: inSeconds + Date.now() / 1000,
+    const task = {
+      appEngineHttpRequest: {
+        httpMethod: 'POST',
+        relativeUri: '/log_payload',
+      },
     };
+
+    if (payload) {
+      task.appEngineHttpRequest.body = Buffer.from(payload).toString('base64');
+    }
+
+    if (inSeconds) {
+      task.scheduleTime = {
+        seconds: inSeconds + Date.now() / 1000,
+      };
+    }
+
+    const request = {
+      parent: parent,
+      task: task,
+    };
+
+    console.log('Sending task:');
+    console.log(task);
+    // Send create task request.
+    const [response] = await client.createTask(request);
+    const name = response.name;
+    console.log(`Created task ${name}`);
   }
-
-  const request = {
-    parent: parent,
-    task: task,
-  };
-
-  console.log('Sending task:');
-  console.log(task);
-  // Send create task request.
-  const [response] = await client.createTask(request);
-  const name = response.name;
-  console.log(`Created task ${name}`);
-
+  quickstart();
   // [END tasks_quickstart]
 }
 
-createTask(...process.argv.slice(2)).catch(console.error);
+process.on('unhandledRejection', err => {
+  console.error(err.message);
+  process.exitCode = 1;
+});
+
+main(...process.argv.slice(2));
