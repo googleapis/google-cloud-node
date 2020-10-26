90f672a2910c63dce37a5c319120f5cc0a6ba994
Justin Beckwith <justin.beckwith@gmail.com>
2020-10-26 09:06:39 -0700

    docs(samples): modernize the samples (#838)

--- packages/google-cloud-vision/samples/quickstart.js
+++ packages/google-cloud-vision/samples/quickstart.js
@@ -14,20 +14,33 @@
 
 'use strict';
 
-// [START vision_quickstart]
-async function quickstart() {
-  // Imports the Google Cloud client library
-  const vision = require('@google-cloud/vision');
+// sample-metadata:
+//   title: Cloud Vision Quickstart
+//   description: Performs label detection of an image with a cat.
+//   usage: node quickstart.js
 
-  // Creates a client
-  const client = new vision.ImageAnnotatorClient();
+function main() {
+  // [START vision_quickstart]
+  async function quickstart() {
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
 
-  // Performs label detection on the image file
-  const [result] = await client.labelDetection('./resources/wakeupcat.jpg');
-  const labels = result.labelAnnotations;
-  console.log('Labels:');
-  labels.forEach(label => console.log(label.description));
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+
+    // Performs label detection on the image file
+    const [result] = await client.labelDetection('./resources/wakeupcat.jpg');
+    const labels = result.labelAnnotations;
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description));
+  }
+  quickstart();
+  // [END vision_quickstart]
 }
-// [END vision_quickstart]
 
-quickstart().catch(console.error);
+process.on('unhandledRejection', err => {
+  console.error(err.message);
+  process.exitCode = 1;
+});
+
+main(...process.argv.slice(2));
