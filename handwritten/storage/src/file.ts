0d48b1bde842fed47816aab915d6f668378c3eb3
Sameena Shaffeeullah <shaffeeullah@google.com>
2020-10-26 16:50:13 -0700

    fix: moved publicUrl comment (#1327)
    * Moved publicUrl comment
    
    * Added space after // for publicUrl

--- handwritten/storage/src/file.ts
+++ handwritten/storage/src/file.ts
@@ -3073,8 +3073,8 @@ class File extends ServiceObject<File> {
    * const bucket = storage.bucket('albums');
    * const file = bucket.file('my-file');
    *
+   * // publicUrl will be "https://storage.googleapis.com/albums/my-file"
    * const publicUrl = file.publicUrl();
-   *  // publicUrl will be "https://storage.googleapis.com/albums/my-file"
    */
   publicUrl(): string {
     return `${this.storage.apiEndpoint}/${this.bucket.name}/${this.name}`;
