ad2392713b64b339c910493b5363434268bdc3e2
Lalji Kanjareeya <46327204+laljikanjareeya@users.noreply.github.com>
2020-12-10 23:10:03 +0530

    docs(samples): enable/disable bucket's versioning sample (#1279)
    PR contains below samples:
    
    - Enable bucket versioning (storage_enable_versioning)
    - Disable bucket versioning (storage_disable_versioning)

--- handwritten/storage/samples/system-test/buckets.test.js
+++ handwritten/storage/samples/system-test/buckets.test.js
@@ -135,6 +135,20 @@ it('should remove a bucket cors configuration', async () => {
   assert.ok(!bucket.metadata.cors);
 });
 
+it("should enable a bucket's versioning", async () => {
+  const output = execSync(`node enableBucketVersioning.js ${bucketName}`);
+  assert.include(output, `Versioning is enabled for bucket ${bucketName}.`);
+  await bucket.getMetadata();
+  assert.strictEqual(bucket.metadata.versioning.enabled, true);
+});
+
+it("should disable a bucket's versioning", async () => {
+  const output = execSync(`node disableBucketVersioning.js ${bucketName}`);
+  assert.include(output, `Versioning is disabled for bucket ${bucketName}.`);
+  await bucket.getMetadata();
+  assert.strictEqual(bucket.metadata.versioning.enabled, false);
+});
+
 it('should delete a bucket', async () => {
   const output = execSync(`node deleteBucket.js ${bucketName}`);
   assert.match(output, new RegExp(`Bucket ${bucketName} deleted.`));
