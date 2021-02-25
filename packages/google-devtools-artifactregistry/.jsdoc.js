6360e62e45ffa016813dd9b7bd48385bf744b8d1
sofisl <55454395+sofisl@users.noreply.github.com>
2021-02-25 13:55:18 -0800

    build: fix package name, rerun synthtool (#16)

--- packages/google-devtools-artifactregistry/.jsdoc.js
+++ packages/google-devtools-artifactregistry/.jsdoc.js
@@ -43,7 +43,7 @@ module.exports = {
     copyright: 'Copyright 2021 Google LLC',
     includeDate: false,
     sourceFiles: false,
-    systemName: 'artifactregistry',
+    systemName: '@google-cloud/artifact-registry',
     theme: 'lumen',
     default: {
       outputSourceFiles: false
