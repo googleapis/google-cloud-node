9ea7b5a85d7f1ab10b35f2d683b6c6ffafcf7b7d
sofisl <55454395+sofisl@users.noreply.github.com>
2021-03-10 20:23:25 -0800

    docs: improve sample (#21)
    * docs: improve sample

--- packages/google-iam-credentials/samples/quickstart.js
+++ packages/google-iam-credentials/samples/quickstart.js
@@ -17,19 +17,18 @@ async function main(serviceAccount, scopes) {
   // [START nodejs_iam_credentials_quickstart]
   // Imports the Google Cloud client library
 
-  // remove this line after package is released
-  // eslint-disable-next-line node/no-missing-require
   const {IAMCredentialsClient} = require('@google-cloud/iam-credentials');
 
-  // TODO(developer): replace with your prefered project ID.
-  // const projectId = 'my-project'
+  // TODO(developer): replace with your prefered project values.
+  // The service account must be granted the roles/iam.serviceAccountTokenCreator role
+  // const serviceAccount = 'ACCOUNT_EMAIL_OR_UNIQUEID'
+  // const scopes = 'my-scopes', e.g., 'https://www.googleapis.com/auth/iam'
 
   // Creates a client
   const client = new IAMCredentialsClient();
 
-  //TODO(library generator): write the actual function you will be testing
   async function generateAccessToken() {
-    const token = await client.generateAccessToken({
+    const [token] = await client.generateAccessToken({
       name: `projects/-/serviceAccounts/${serviceAccount}`,
       scope: [scopes],
     });
