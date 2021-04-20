d2ccb0aa50960fc4f1e897695500a144ae9f184c
Jeffrey Rennie <rennie@google.com>
2021-04-20 10:35:37 -0700

    chore: migrate to owl bot (#154)
    * chore: migrate to owl bot
    
    * chore: copy files from googleapis-gen e0ea8b51f30e2ff6104abd1e4c8d1eb67078c86a
    
    * chore: run the post processor
    
    * 🦉 Updates from OwlBot
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- packages/google-cloud-bigquery-storage/owlbot.py
+++ packages/google-cloud-bigquery-storage/owlbot.py
@@ -12,23 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """This script is used to synthesize generated parts of this library."""
-import synthtool as s
-import synthtool.gcp as gcp
 import synthtool.languages.node as node
-import logging
-logging.basicConfig(level=logging.DEBUG)
 
-AUTOSYNTH_MULTIPLE_COMMITS = True
-
-# Run the gapic generator
-gapic = gcp.GAPICBazel()
-name = 'bigquery-storage'
-versions = ['v1beta1', 'v1']
-for version in versions:
-    library = gapic.node_library(name, version, proto_path=f'google/cloud/bigquery/storage/{version}')
-    s.copy(library, excludes=['package.json', 'README.md', 'src/index.ts'])
-# Copy common templates
-common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library(source_location='build/src')
-s.copy(templates, excludes=[])
-node.postprocess_gapic_library()
+node.owlbot_main(
+    staging_excludes=['package.json', 'README.md', 'src/index.ts'],
+    templates_excludes=['src/index.ts']
+)
