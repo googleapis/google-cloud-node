0b92bcd8fee4819ebc877e14d3a4bffac5299e07
Jeffrey Rennie <rennie@google.com>
2021-04-20 10:33:32 -0700

    chore: migrate to owl bot (#647)
    * chore: migrate to owl bot
    
    * chore: copy files from googleapis-gen f43939eac6a0bb5998c1fa0f79063194e699230e
    
    * chore: run the post processor
    
    * 🦉 Updates from OwlBot
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- packages/google-cloud-translate/owlbot.py
+++ packages/google-cloud-translate/owlbot.py
@@ -14,25 +14,6 @@
 
 """This script is used to synthesize generated parts of this library."""
 
-import synthtool as s
-import synthtool.gcp as gcp
 import synthtool.languages.node as node
-import logging
 
-# Run the gapic generator
-gapic = gcp.GAPICBazel()
-versions = ['v3beta1', 'v3']
-name = 'translation'
-for version in versions:
-  library = gapic.node_library(name, version, proto_path=f'google/cloud/translate/{version}')
-  s.copy(library, excludes=['README.md', 'package.json', 'src/index.ts'])
-
-logging.basicConfig(level=logging.DEBUG)
-
-AUTOSYNTH_MULTIPLE_COMMITS = True
-
-common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library(source_location='build/src')
-s.copy(templates, excludes=[])
-
-node.postprocess_gapic_library()
+node.owlbot_main(templates_excludes=['src/index.ts'])
