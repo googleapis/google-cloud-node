525170819df98e258721be338d093d350841d702
sofisl <55454395+sofisl@users.noreply.github.com>
2021-06-21 11:16:26 -0400

    feat: generate new surface for v1 (#504)

--- packages/google-cloud-asset/src/v1p2beta1/asset_service_client.ts
+++ packages/google-cloud-asset/src/v1p2beta1/asset_service_client.ts
@@ -131,6 +131,8 @@ export class AssetServiceClient {
     }
     if (!opts.fallback) {
       clientHeader.push(`grpc/${this._gaxGrpc.grpcVersion}`);
+    } else if (opts.fallback === 'rest') {
+      clientHeader.push(`rest/${this._gaxGrpc.grpcVersion}`);
     }
     if (opts.libName && opts.libVersion) {
       clientHeader.push(`${opts.libName}/${opts.libVersion}`);
