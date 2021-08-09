2be70f2e4948cb6b63b487e7e5730905e2890223
Sameena Shaffeeullah <shaffeeullah@google.com>
2021-08-09 08:39:11 -0700

    chore: remove GOOGLE_CLOUD_PROJECT linter warning (#1526)
    * chore: remove GOOGLE_CLOUD_PROJECT linter warning
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/master/packages/owl-bot/README.md
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- handwritten/storage/system-test/storage.ts
+++ handwritten/storage/system-test/storage.ts
@@ -173,6 +173,7 @@ describe('storage', () => {
       // CI authentication is done with ADC. Cache it here, restore it `after`
       GOOGLE_APPLICATION_CREDENTIALS =
         process.env.GOOGLE_APPLICATION_CREDENTIALS;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       GOOGLE_CLOUD_PROJECT = process.env.GOOGLE_CLOUD_PROJECT;
 
       privateBucket = bucket; // `bucket` was created in the global `before`
