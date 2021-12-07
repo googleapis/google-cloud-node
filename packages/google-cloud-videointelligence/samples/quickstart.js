ce36b197f0cabce193c2f7d54434c7e73ddff322
nicain <nicain.seattle@gmail.com>
2021-12-07 08:38:23 -0800

    chore: Cleanup timestamp setting blocks in samples (#631)

--- packages/google-cloud-videointelligence/samples/quickstart.js
+++ packages/google-cloud-videointelligence/samples/quickstart.js
@@ -48,18 +48,6 @@ async function main() {
     console.log(`Label ${label.entity.description} occurs at:`);
     label.segments.forEach(segment => {
       segment = segment.segment;
-      if (segment.startTimeOffset.seconds === undefined) {
-        segment.startTimeOffset.seconds = 0;
-      }
-      if (segment.startTimeOffset.nanos === undefined) {
-        segment.startTimeOffset.nanos = 0;
-      }
-      if (segment.endTimeOffset.seconds === undefined) {
-        segment.endTimeOffset.seconds = 0;
-      }
-      if (segment.endTimeOffset.nanos === undefined) {
-        segment.endTimeOffset.nanos = 0;
-      }
       console.log(
         `\tStart: ${segment.startTimeOffset.seconds}` +
           `.${(segment.startTimeOffset.nanos / 1e6).toFixed(0)}s`
