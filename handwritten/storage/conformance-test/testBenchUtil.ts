5a0862b7d23d82cb180338061a57ce130876f7d2
Denis DelGrosso <85250797+ddelgrosso1@users.noreply.github.com>
2022-02-03 19:17:33 -0500

    chore: update testbench to v0.14.0 (#1778)

--- handwritten/storage/conformance-test/testBenchUtil.ts
+++ handwritten/storage/conformance-test/testBenchUtil.ts
@@ -21,7 +21,7 @@ const PORT = new URL(HOST).port;
 const CONTAINER_NAME = 'storage-testbench';
 const DEFAULT_IMAGE_NAME =
   'gcr.io/cloud-devrel-public-resources/storage-testbench';
-const DEFAULT_IMAGE_TAG = 'v0.12.0';
+const DEFAULT_IMAGE_TAG = 'v0.14.0';
 const DOCKER_IMAGE = `${DEFAULT_IMAGE_NAME}:${DEFAULT_IMAGE_TAG}`;
 const PULL_CMD = `docker pull ${DOCKER_IMAGE}`;
 const RUN_CMD = `docker run --rm -d -p ${PORT}:${PORT} --name ${CONTAINER_NAME} ${DOCKER_IMAGE} && sleep 1`;
