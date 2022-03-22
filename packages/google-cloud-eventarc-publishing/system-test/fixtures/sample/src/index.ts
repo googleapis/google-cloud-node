503963b99f99158c8dbffbe35a1f29dd972e5528
Sofia Leon <sofialeon@google.com>
2022-03-21 18:16:30 -0700

    fix import

--- packages/google-cloud-eventarc-publishing/system-test/fixtures/sample/src/index.ts
+++ packages/google-cloud-eventarc-publishing/system-test/fixtures/sample/src/index.ts
@@ -16,7 +16,7 @@
 // ** https://github.com/googleapis/gapic-generator-typescript **
 // ** All changes to this file may be overwritten. **
 
-import {PublisherClient} from '@google-cloud/publishing';
+import {PublisherClient} from '@google-cloud/eventarc-publishing';
 
 // check that the client class type name can be used
 function doStuffWithPublisherClient(client: PublisherClient) {
