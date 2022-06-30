dd6eccded558bdd533a2456d5c61adb26b741f65
Sofia Leon <sofialeon@google.com>
2022-06-29 17:09:12 -0700

    fix var

--- containers/node-bootstrap-container/entrypoint.sh
+++ containers/node-bootstrap-container/entrypoint.sh
@@ -25,5 +25,5 @@ cd $WORKSPACE_DIR/$MONO_REPO_NAME/containers/node-bootstrap-container
 FOLDER_NAME=$(node create-folder-name.js $API_ID) || node create-gh-issue.js
 mkdir -p "$WORKSPACE_DIR/$MONO_REPO_NAME/packages/$FOLDER_NAME" || node create-gh-issue.js
 cd  $WORKSPACE_DIR/$MONO_REPO_NAME
-OWLBOT_YAML_PATH="packages/$FOLDER_NAME"
+_OWLBOT_YAML_PATH="packages/$FOLDER_NAME"
 /usr/local/bin/gapic-node-templating bootstrap-library --api-id $API_ID --destination-folder "$WORKSPACE_DIR/$MONO_REPO_NAME/packages/$FOLDER_NAME" --mono-repo-name $MONO_REPO_NAME --github-token "$GITHUB_TOKEN" || node create-gh-issue.js
