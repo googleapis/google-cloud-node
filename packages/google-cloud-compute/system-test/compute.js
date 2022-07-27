55def8f5ce0dc69154a116dd9d5ccf39be4ecce9
sofisl <55454395+sofisl@users.noreply.github.com>
2022-07-27 13:12:11 -0700

    build: add more retries for flaky test (#766)
    * fix: add more retries for flaky test

--- packages/google-cloud-compute/system-test/compute.js
+++ packages/google-cloud-compute/system-test/compute.js
@@ -328,7 +328,7 @@ describe('Compute', () => {
 
     it('instances update desc to an empty string', async function () {
       this.timeout(10 * 60 * 1000);
-      this.retries(3);
+      this.retries(10);
       await delay(this.test);
       const [instance] = await client.get({
         project,
