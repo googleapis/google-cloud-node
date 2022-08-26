10ba079153cb101218299f1a3dc5321d2c988d09
Sofia Leon <sofialeon@google.com>
2022-08-26 14:08:49 -0700

    fix test

--- packages/google-cloud-gkemulticloud/samples/test/quickstart.js
+++ packages/google-cloud-gkemulticloud/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/gkemulticloud').v1;
-const gkemulticloudClient = new Client();
+const {AwsClustersClient} = require('@google-cloud/gkemulticloud').v1;
+const gkemulticloudClient = new AwsClustersClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
