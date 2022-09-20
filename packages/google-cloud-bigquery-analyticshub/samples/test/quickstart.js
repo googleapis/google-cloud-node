dab7aa534338bd766ce9939057249cf229a23235
Benjamin Coe <bencoe@google.com>
2022-09-20 11:57:34 -0400

    samples: add integration test

--- packages/google-cloud-bigquery-analyticshub/samples/test/quickstart.js
+++ packages/google-cloud-bigquery-analyticshub/samples/test/quickstart.js
@@ -34,7 +34,7 @@ describe('Quickstart', () => {
 
   it('should run quickstart', async () => {
     const output = execSync(
-      `node ./quickstart.js projects/${projectId}/locations/us-central1`,
+      `node ./quickstart.js projects/${projectId}/locations/asia-east1/dataExchanges/abc123`,
       {cwd}
     );
     assert(output !== null);
