4b45779b9d772a5bff4a8a1dd4cf92988cc628ce
sofisl <55454395+sofisl@users.noreply.github.com>
2022-09-30 15:13:06 -0700

    build: add update split repo script to update the README and delete the rest of the repo (#3377)
    * build: add update split repo script to update the README and delete the rest of the repo
    
    Co-authored-by: Jeff Ching <chingor@google.com>

--- bin/migrate-split-repo.sh
+++ bin/migrate-split-repo.sh
@@ -41,3 +41,6 @@ ${SCRIPT_DIR}/migrate-git-history.sh \
   "${PACKAGE_PATH}" \
   ".kokoro,.github,.trampolinerc,SECURITY.md,renovate.json,samples" \
   ".github/.OwlBot.yaml,samples/quickstart.js,samples/test/quickstart.js,samples/test/quickstart.test.js,system-test/test/quickstart.test.js,system-test/test/quickstart.js,samples/README.md,samples/package.json,samples/generated"
+
+# run the script to update the split repo
+${SCRIPT_DIR}/update-split-repo.sh ${SPLIT_REPO} ${ARTIFACT_NAME}
