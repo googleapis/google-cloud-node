571b4be6f8fed87e039a2463b2bb37123d9d0bd4
sofisl <55454395+sofisl@users.noreply.github.com>
2022-10-11 13:22:01 -0700

    build: leave .kokoro files so sample tests will still run, add a few more possible files to look for when migrating (#3392)
    * build: leave .kokoro files so sample tests will still run, add a few more possible test files

--- bin/migrate-split-repo.sh
+++ bin/migrate-split-repo.sh
@@ -40,8 +40,8 @@ ${SCRIPT_DIR}/migrate-git-history.sh \
   "" \
   "${PACKAGE_PATH}" \
   ".kokoro,.github,.trampolinerc,SECURITY.md,renovate.json,samples" \
-  ".github/.OwlBot.yaml,samples/quickstart.js,samples/test/quickstart.js,samples/test/quickstart.test.js,system-test/test/quickstart.test.js,system-test/test/quickstart.js,samples/README.md,samples/package.json,samples/generated"
+  ".github/.OwlBot.yaml,samples/quickstart.js,samples/test/quickstart.js,samples/.eslintrc.yml,samples/test/sample.test.js,samples/test/quickstart.test.js,system-test/test/quickstart.test.js,system-test/test/quickstart.js,samples/README.md,samples/package.json,samples/generated"
 
 # run the script to update the split repo and either delete all the samples or just update the README
-${SCRIPT_DIR}/delete-samples-split-repo.sh ${SPLIT_REPO} ${ARTIFACT_NAME}
+${SCRIPT_DIR}/delete-everything-split-repo.sh ${SPLIT_REPO} ${ARTIFACT_NAME}
 ${SCRIPT_DIR}/update-readme-only-split-repo.sh ${SPLIT_REPO} ${ARTIFACT_NAME}
