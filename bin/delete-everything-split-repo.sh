48519e0e630e0c47e32e0492f3778cc8cb887a41
Sofia Leon <sofialeon@google.com>
2022-10-12 14:55:32 -0700

    add EOD phrase

--- bin/delete-everything-split-repo.sh
+++ bin/delete-everything-split-repo.sh
@@ -73,30 +73,31 @@ OWLBOT_PY="/tmp/${SPLIT_REPO}/owlbot.py"
 
 echo "$IGNORE_README_TXT" >> "$OWLBOT_PY"
 
-SAMPLES_CFG_TXT="#!/bin/bash
+SAMPLES_CFG_TXT=<<'EOD'
+#!/bin/bash
 # Copyright 2018 Google LLC
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the License);
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an \"AS IS\" BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
 set -eo pipefail
 
-export NPM_CONFIG_PREFIX=\${HOME}/.npm-global
+export NPM_CONFIG_PREFIX=${HOME}/.npm-global
 
 # Setup service account credentials.
-export GOOGLE_APPLICATION_CREDENTIALS=\${KOKORO_GFILE_DIR}/service-account.json
+export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/service-account.json
 export GCLOUD_PROJECT=long-door-651
 
-cd \$(dirname \$0)/..
+cd $(dirname $0)/..
 
 # Run a pre-test hook, if a pre-samples-test.sh is in the project
 if [ -f .kokoro/pre-samples-test.sh ]; then
@@ -111,12 +112,12 @@ if [ -f samples/package.json ]; then
     npm install
     # If tests are running against main branch, configure flakybot
     # to open issues on failures:
-    if [[ \$KOKORO_BUILD_ARTIFACTS_SUBDIR = *\"continuous\"* ]] || [[ \$KOKORO_BUILD_ARTIFACTS_SUBDIR = *\"nightly\"* ]]; then
+    if [[ $KOKORO_BUILD_ARTIFACTS_SUBDIR = *"continuous"* ]] || [[ $KOKORO_BUILD_ARTIFACTS_SUBDIR = *"nightly"* ]]; then
       export MOCHA_REPORTER_OUTPUT=test_output_sponge_log.xml
       export MOCHA_REPORTER=xunit
       cleanup() {
-        chmod +x \$KOKORO_GFILE_DIR/linux_amd64/flakybot
-        \$KOKORO_GFILE_DIR/linux_amd64/flakybot
+        chmod +x $KOKORO_GFILE_DIR/linux_amd64/flakybot
+        $KOKORO_GFILE_DIR/linux_amd64/flakybot
       }
       trap cleanup EXIT HUP
     fi
@@ -127,15 +128,16 @@ fi
 # codecov combines coverage across integration and unit tests. Include
 # the logic below for any environment you wish to collect coverage for:
 COVERAGE_NODE=12
-if npx check-node-version@3.3.0 --silent --node \$COVERAGE_NODE; then
+if npx check-node-version@3.3.0 --silent --node $COVERAGE_NODE; then
   NYC_BIN=./node_modules/nyc/bin/nyc.js
-  if [ -f \"\$NYC_BIN\" ]; then
-    \$NYC_BIN report || true
+  if [ -f "$NYC_BIN" ]; then
+    $NYC_BIN report || true
   fi
-  bash \$KOKORO_GFILE_DIR/codecov.sh
+  bash $KOKORO_GFILE_DIR/codecov.sh
 else
-  echo \"coverage is only reported for Node \$COVERAGE_NODE\"
-fi"
+  echo "coverage is only reported for Node $COVERAGE_NODE"
+fi
+EOD
 
 echo "$SAMPLES_CFG_TXT" >> "/tmp/${SPLIT_REPO}/.kokoro/samples-test.sh"
 chmod u+x "/tmp/${SPLIT_REPO}/.kokoro/samples-test.sh"
