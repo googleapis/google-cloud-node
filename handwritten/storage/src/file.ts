1dac409acc947042411573515226733bbf0a79aa
Denis DelGrosso <85250797+ddelgrosso1@users.noreply.github.com>
2022-10-25 11:33:47 -0400

    fix: remove async from final function which causes double invocation … (#2094)
    * fix: remove async from final function which causes double invocation of callback
    
    * add catch clause

--- handwritten/storage/src/file.ts
+++ handwritten/storage/src/file.ts
@@ -1530,12 +1530,15 @@ class File extends ServiceObject<File> {
         }
 
         const handoffStream = new PassThrough({
-          final: async cb => {
+          final: cb => {
             // Preserving `onComplete`'s ability to
             // destroy `throughStream` before pipeline
             // attempts to.
-            await onComplete(null);
-            cb();
+            onComplete(null)
+              .then(() => {
+                cb();
+              })
+              .catch(cb);
           },
         });
 
