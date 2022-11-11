8611e4b27f92b7686580797fc421b0bb7d66bd10
Sofia Leon <sofialeon@google.com>
2022-11-11 01:40:10 -0800

    build: add release-please config, fix owlbot-config

--- packages/google-cloud-functions/owlbot.py
+++ packages/google-cloud-functions/owlbot.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 """This script is used to synthesize generated parts of this library."""
 import synthtool as s
-import synthtool.languages.node as node
+import synthtool.languages.node_mono_repo as node
 from pathlib import Path
 
-node.owlbot_main(staging_excludes=['README.md', 'package.json'])
+node.owlbot_main(relative_dir="packages/google-cloud-functions",staging_excludes=['README.md', 'package.json'])
