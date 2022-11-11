78a8253053e0a1118d645d2d8cdff72083a7faee
Sofia Leon <sofialeon@google.com>
2022-11-11 03:00:34 -0800

    build: add release-please config, fix owlbot-config

--- packages/google-analytics-admin/owlbot.py
+++ packages/google-analytics-admin/owlbot.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """This script is used to synthesize generated parts of this library."""
-import synthtool.languages.node as node
+import synthtool.languages.node_mono_repo as node
 
-node.owlbot_main(templates_excludes=['.github/CODEOWNERS'])
+node.owlbot_main(relative_dir="packages/google-analytics-admin",templates_excludes=['.github/CODEOWNERS'])
