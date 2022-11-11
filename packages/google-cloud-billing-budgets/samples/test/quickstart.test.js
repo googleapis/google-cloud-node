bcc7a0330934e09bd5be7ef01e0037e8aa1b3af5
Sofia Leon <sofialeon@google.com>
2022-11-11 15:45:04 -0800

    add dependencies

--- packages/google-cloud-billing-budgets/samples/test/quickstart.test.js
+++ packages/google-cloud-billing-budgets/samples/test/quickstart.test.js
@@ -19,6 +19,7 @@
 const path = require('path');
 const {assert} = require('chai');
 const cp = require('child_process');
+const {describe, it} = require('mocha');
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 const cwd = path.join(__dirname, '..');
