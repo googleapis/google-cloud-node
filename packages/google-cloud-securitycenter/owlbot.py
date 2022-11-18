12a3c822e7cb9a65d4ac40bc55de65edfd920599
Sofia Leon <sofialeon@google.com>
2022-11-17 17:34:12 -0800

    build: add release-please config, fix owlbot-config

--- packages/google-cloud-securitycenter/owlbot.py
+++ packages/google-cloud-securitycenter/owlbot.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import synthtool.languages.node as node
+import synthtool.languages.node_mono_repo as node
 
-node.owlbot_main(
+node.owlbot_main(relative_dir="packages/google-cloud-securitycenter",
     staging_excludes=['package.json', 'README.md'],
     templates_excludes=['.kokoro/samples-test.sh', '.kokoro/samples-test.sh']
 )
