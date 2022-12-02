db8c69b84ff8785f90d0c95a1c8b55a04a2770cd
losalex <90795544+losalex@users.noreply.github.com>
2022-12-01 21:49:23 -0800

    fix: Add a partner team as approvers for PRs (#686)
    * fix: Add a partner team as approvers for PRs 
    
    Adding @googleapis/api-logging-partners to contain more people who can approve PRs
    
    * Update sync-repo-settings.yaml
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * Update sync-repo-settings.yaml
    
    * Update CODEOWNERS
    
    * Update owlbot.py
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- handwritten/owlbot.py
+++ handwritten/owlbot.py
@@ -22,7 +22,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library()
-s.copy(templates, excludes=[".github/auto-label.yaml"])
+s.copy(templates, excludes=[".github/auto-label.yaml", ".github/CODEOWNERS", ".github/sync-repo-settings.yaml"])
 node.fix_hermetic()
 
 
