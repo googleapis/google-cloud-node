f1093de034c3a518a52294dd7191e0fd560b3244
Sofia Leon <sofialeon@google.com>
2022-12-15 09:01:02 -0800

    build: add quickstart import

--- packages/google-cloud-vmwareengine/samples/test/quickstart.js
+++ packages/google-cloud-vmwareengine/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/vmwareengine').v1;
-const vmwareengineClient = new Client();
+const {VmwareEngineClient} = require('@google-cloud/vmwareengine').v1;
+const vmwareengineClient = new VmwareEngineClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
