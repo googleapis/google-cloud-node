a316c687f7e8ad6236530da6932530991a398477
Alexander Fenster <github@fenster.name>
2023-01-25 17:09:06 -0800

    Revert "fix(network-connectivity)!: remove policy-based routing (#3885)"
    This reverts commit a71d871e056dff4eb5ea18ca8977636f6ff4a9d4.

--- packages/google-cloud-networkconnectivity/samples/generated/v1/hub_service.create_hub.js
+++ packages/google-cloud-networkconnectivity/samples/generated/v1/hub_service.create_hub.js
@@ -41,11 +41,11 @@ function main(parent, hubId, hub) {
    */
   // const hub = {}
   /**
-   *  Optional. A unique request ID (optional). If you specify this ID, you can use it
-   *  in cases when you need to retry your request. When you need to retry, this
-   *  ID lets the server know that it can ignore the request if it has already
-   *  been completed. The server guarantees that for at least 60 minutes after
-   *  the first request.
+   *  Optional. A unique request ID (optional). If you specify this ID, you can
+   *  use it in cases when you need to retry your request. When you need to
+   *  retry, this ID lets the server know that it can ignore the request if it
+   *  has already been completed. The server guarantees that for at least 60
+   *  minutes after the first request.
    *  For example, consider a situation where you make an initial request and
    *  the request times out. If you make the request again with the same request
    *  ID, the server can check to see whether the original operation
