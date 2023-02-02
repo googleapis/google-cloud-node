aaeb1271fad0417b2603fb56d623bd67f5a166ba
Sofia Leon <sofialeon@google.com>
2023-02-01 17:04:15 -0800

    docs: add quickstart test

--- packages/google-cloud-datacatalog-lineage/samples/test/quickstart.js
+++ packages/google-cloud-datacatalog-lineage/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/lineage').v1;
-const lineageClient = new Client();
+const {LineageClient} = require('@google-cloud/lineage').v1;
+const lineageClient = new LineageClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
