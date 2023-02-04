c76adaa30552ffd951766e492ca8a607997b03e2
sofisl <55454395+sofisl@users.noreply.github.com>
2023-02-03 16:54:19 -0800

    Update docs-single.sh

--- .kokoro/release/docs-single.sh
+++ .kokoro/release/docs-single.sh
@@ -25,7 +25,7 @@ npm run docs
 
 # create docs.metadata, based on package.json and .repo-metadata.json.
 --no-save
-npm install --no-save json@9.0.6
+npm install json@9.0.6
 python3 -m docuploader create-metadata \
   --name=$(cat .repo-metadata.json | json name) \
   --version=$(cat package.json | json version) \
