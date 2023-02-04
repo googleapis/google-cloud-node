b079bba56661895cfc580f4d43e04e37fa9b67c6
Sofia Leon <sofialeon@google.com>
2023-02-03 16:30:25 -0800

    add comments to test

--- packages/google-maps-mapsplatformdatasets/samples/test/quickstart.js
+++ packages/google-maps-mapsplatformdatasets/samples/test/quickstart.js
@@ -17,25 +17,29 @@
 const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
-const {describe, it, before} = require('mocha');
-// Imports the Mapsplatformdatasets library
-const {MapsPlatformDatasetsV1AlphaClient} =
-  require('@google-cloud/mapsplatformdatasets').v1alpha;
-// Instantiates a client
-const mapsplatformdatasetsClient = new MapsPlatformDatasetsV1AlphaClient();
+const {describe, it} = require('mocha');
+// const {describe, it, before} = require('mocha');
+// // Imports the Mapsplatformdatasets library
+// const {MapsPlatformDatasetsV1AlphaClient} =
+//   require('@google-cloud/mapsplatformdatasets').v1alpha;
+// // Instantiates a client
+// const mapsplatformdatasetsClient = new MapsPlatformDatasetsV1AlphaClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 const cwd = path.join(__dirname, '..');
 
 describe('Quickstart', () => {
-  let projectId;
+  // let projectId;
 
-  before(async () => {
-    projectId = await mapsplatformdatasetsClient.getProjectId();
-  });
+  // before(async () => {
+  //   projectId = await mapsplatformdatasetsClient.getProjectId();
+  // });
 
   it('should run quickstart', async () => {
+    // NOTE: This API does not yet support project ID (vs. just project number)
+    // We should use projectId once they do.
+    // DEVELOPER: change this to your project number when running locally.
     const output = execSync('node ./quickstart.js projects/1046198160504', {
       cwd,
     });
