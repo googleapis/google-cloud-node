13d5e9681bb0833e136859351c328f12dc489591
Sofia Leon <sofialeon@google.com>
2023-02-07 12:34:54 -0800

    update test

--- packages/google-cloud-gsuiteaddons/samples/test/quickstart.js
+++ packages/google-cloud-gsuiteaddons/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/gsuiteaddons').v1;
-const gsuiteaddonsClient = new Client();
+const {GSuiteAddOnsClient} = require('@google-cloud/gsuiteaddons').v1;
+const gsuiteaddonsClient = new GSuiteAddOnsClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
@@ -34,7 +34,7 @@ describe('Quickstart', () => {
 
   it('should run quickstart', async () => {
     const output = execSync(
-      `node ./quickstart.js projects/${projectId}/locations/us-central1`,
+      `node ./quickstart.js projects/${projectId}`,
       {cwd}
     );
     assert(output !== null);
