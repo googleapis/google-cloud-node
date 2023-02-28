8f1493c1f0b6ba544a07d38b68f4ad2e619033a3
sofisl <55454395+sofisl@users.noreply.github.com>
2023-02-28 15:23:51 -0800

    Update run_conditional_tests.sh

--- ci/run_conditional_tests.sh
+++ ci/run_conditional_tests.sh
@@ -24,10 +24,6 @@ export PROJECT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/..)
 # A script file for running the test in a sub project.
 test_script="${PROJECT_ROOT}/ci/run_single_test.sh"
 
-apt-get -y update; apt-get -y install curl
-curl https://github.com/googleapis/repo-automation-bots/releases/download/flakybot-1.1.0/flakybot -o flakybot -s -L
-chmod +x ./flakybot
-
 if [ ${BUILD_TYPE} == "presubmit" ]; then
     # For presubmit build, we want to know the difference from the
     # common commit in origin/main.
