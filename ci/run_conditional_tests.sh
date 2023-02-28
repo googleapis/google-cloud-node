dfcc3afd56c5ea2f16b26933b5d88b42e34c49ac
sofisl <55454395+sofisl@users.noreply.github.com>
2023-02-28 14:59:43 -0800

    Update run_conditional_tests.sh

--- ci/run_conditional_tests.sh
+++ ci/run_conditional_tests.sh
@@ -24,6 +24,8 @@ export PROJECT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/..)
 # A script file for running the test in a sub project.
 test_script="${PROJECT_ROOT}/ci/run_single_test.sh"
 
+curl https://github.com/googleapis/repo-automation-bots/releases/download/flakybot-1.1.0/flakybot -o flakybot -s -L
+chmod +x ./flakybot
 
 if [ ${BUILD_TYPE} == "presubmit" ]; then
     # For presubmit build, we want to know the difference from the
