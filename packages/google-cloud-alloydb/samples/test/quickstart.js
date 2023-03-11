f7476db7c6879e1f0440c4529bb7855b36a93651
sofisl <55454395+sofisl@users.noreply.github.com>
2023-03-10 19:41:42 -0800

    Update quickstart.js

--- packages/google-cloud-alloydb/samples/test/quickstart.js
+++ packages/google-cloud-alloydb/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/alloydb').v1;
-const alloydbClient = new Client();
+const {AlloyDBAdminClient} = require('@google-cloud/alloydb').v1;
+const alloydbClient = new AlloyDBAdminClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
