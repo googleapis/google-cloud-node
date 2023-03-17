891f9ea3629deafb7e794c45f6584da1c8dc906e
Sofia Leon <sofialeon@google.com>
2023-03-17 14:17:16 -0700

    build: update quickstart

--- packages/google-cloud-workstations/samples/test/quickstart.js
+++ packages/google-cloud-workstations/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/workstations').v1beta;
-const workstationsClient = new Client();
+const {WorkstationsClient} = require('@google-cloud/workstations').v1beta;
+const workstationsClient = new WorkstationsClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
