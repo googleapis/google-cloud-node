497e9c3fe199007c94f2d20401414f5ac499c6f7
sofisl <55454395+sofisl@users.noreply.github.com>
2023-03-20 14:00:01 -0700

    Update quickstart.js

--- packages/google-cloud-workstations/samples/test/quickstart.js
+++ packages/google-cloud-workstations/samples/test/quickstart.js
@@ -34,7 +34,7 @@ describe('Quickstart', () => {
 
   it('should run quickstart', async () => {
     const output = execSync(
-      `node ./quickstart.js projects/${projectId}/locations/us-central1/workstationClusters/cluster-lfdegjrv/workstationConfigs/config-lfdeggbx`,
+      `node ./quickstart.js projects/${projectId}/locations/us-central1/workstationClusters/cluster-lfha52bg/workstationConfigs/config-lfhb2xoh`,
       {cwd}
     );
     assert(output !== null);
