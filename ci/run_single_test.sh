dd39b934c50f293465db7aea54cfbdaee025f3de
sofisl <55454395+sofisl@users.noreply.github.com>
2023-03-27 16:04:09 -0700

    Update run_single_test.sh

--- ci/run_single_test.sh
+++ ci/run_single_test.sh
@@ -39,7 +39,7 @@ if [ ${BUILD_TYPE} != "presubmit" ]; then
 fi
 
 # Install dependencies
-npm install --engine-strict --only=prod; npm install --only=dev
+npm install --only=dev; npm install --engine-strict --only=prod
 
 retval=0
 
