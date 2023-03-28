c558006cce089ce304cb952b9d78d0826c3c19d1
sofisl <55454395+sofisl@users.noreply.github.com>
2023-03-27 17:04:46 -0700

    Update run_single_test.sh

--- ci/run_single_test.sh
+++ ci/run_single_test.sh
@@ -39,7 +39,7 @@ if [ ${BUILD_TYPE} != "presubmit" ]; then
 fi
 
 # Install dependencies
-npm install --unsafe-perm --ignore-scripts --omit=dev; npm install
+npm install --unsafe-perm --ignore-scripts --omit=dev; npm install --unsafe-perm
 
 retval=0
 
