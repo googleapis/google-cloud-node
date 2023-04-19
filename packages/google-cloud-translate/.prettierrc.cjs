a68fe20a43390841e943dd7917589de2cecf8d1d
Sofia Leon <sofialeon@google.com>
2023-04-19 12:31:08 -0700

    add fs import

--- packages/google-cloud-translate/.prettierrc.cjs
+++ packages/google-cloud-translate/.prettierrc.cjs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import fs from 'fs';
+
 module.exports = {
   ...JSON.parse(
     fs.readFileSync('gts/.prettierrc.json').toString()
