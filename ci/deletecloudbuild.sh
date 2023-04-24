ee931dfacaa897eae91d6c929af117deaa93b69b
Sofia Leon <sofialeon@google.com>
2023-04-24 16:48:32 -0700

    retry

--- ci/deletecloudbuild.sh
+++ ci/deletecloudbuild.sh
@@ -2,5 +2,6 @@ now=$(date -u -d "-2 min" +"%Y-%m-%dT%H:%M:%SZ")
 gcloud builds list --format="value(id)" --filter="substitutions.REF_NAME="$REF_NAME" AND status="WORKING" AND create_time<"$now"" | while IFS= read -r line ; do
     echo "$now"
     echo "$line"
-    gcloud builds cancel "$line"
+    echo "gcloud builds list --format="value(id)" --filter="substitutions.REF_NAME="$REF_NAME" AND status="WORKING" AND create_time<"$now"""
+    # gcloud builds cancel "$line"
 done
