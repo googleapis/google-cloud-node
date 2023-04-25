e334f9435865f82e45eb306b8a738726e9520b81
Sofia Leon <sofialeon@google.com>
2023-04-24 17:14:38 -0700

    delete old jobs

--- ci/deletecloudbuild.sh
+++ ci/deletecloudbuild.sh
@@ -1,9 +1,9 @@
 now_created=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-now=$(date -u -d "-5 min" +"%Y-%m-%dT%H:%M:%SZ")
+now=$(date -u -d "-6 min" +"%Y-%m-%dT%H:%M:%SZ")
 gcloud builds list --format="value(id)" --filter="substitutions.REF_NAME="$REF_NAME" AND status="WORKING" AND create_time<"$now"" | while IFS= read -r line ; do
     echo "$now_created"
     echo "$now"
     echo "$line"
     echo "gcloud builds list --format='value(id)' --filter=substitutions.REF_NAME="$REF_NAME" AND status="WORKING" AND create_time<"$now""
-    # gcloud builds cancel "$line"
+    gcloud builds cancel "$line"
 done
