ad992e2acb9abbb228e77f415c4848c6741e469a
sofisl <55454395+sofisl@users.noreply.github.com>
2023-04-24 17:47:15 -0700

    Update deletecloudbuild.sh

--- ci/deletecloudbuild.sh
+++ ci/deletecloudbuild.sh
@@ -1,9 +1,5 @@
-now_created=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 now=$(date -u -d "-15 min" +"%Y-%m-%dT%H:%M:%SZ")
 gcloud builds list --format="value(id)" --filter="substitutions.REF_NAME="$REF_NAME" AND status="WORKING" AND create_time<"$now"" | while IFS= read -r line ; do
-    echo "$now_created"
-    echo "$now"
-    echo "$line"
     echo "gcloud builds list --format='value(id)' --filter=substitutions.REF_NAME="$REF_NAME" AND status="WORKING" AND create_time<"$now""
     gcloud builds cancel "$line"
 done
