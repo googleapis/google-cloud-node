a05e7ef730daef12ff83046d94630b21e6f9ae91
Sofia Leon <sofialeon@google.com>
2023-09-07 16:16:25 -0700

    update sample

--- packages/google-cloud-netapp/samples/test/quickstart.js
+++ packages/google-cloud-netapp/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/netapp').v1;
-const netappClient = new Client();
+const {NetAppClient} = require('@google-cloud/netapp').v1;
+const netappClient = new NetAppClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
