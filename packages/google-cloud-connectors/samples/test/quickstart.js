2a0343a908b6f5dd1d3ae32183a7e952054965ca
Sofia Leon <sofialeon@google.com>
2023-09-12 11:37:46 -0700

    update sample test

--- packages/google-cloud-connectors/samples/test/quickstart.js
+++ packages/google-cloud-connectors/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/connectors').v1;
-const connectorsClient = new Client();
+const {ConnectorsClient} = require('@google-cloud/connectors').v1;
+const connectorsClient = new ConnectorsClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
