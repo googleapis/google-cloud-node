685ec94c6997dc1e3066faef1bfb55b213a52b0d
Sofia Leon <sofialeon@google.com>
2023-09-12 16:19:34 -0700

    retry

--- packages/google-cloud-edgecontainer/samples/test/quickstart.js
+++ packages/google-cloud-edgecontainer/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/edgecontainer').v1;
-const edgecontainerClient = new Client();
+const {EdgeContainerClient} = require('@google-cloud/edgecontainer').v1;
+const edgecontainerClient = new EdgeContainerClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
