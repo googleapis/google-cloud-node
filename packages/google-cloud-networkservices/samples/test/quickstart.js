5ba1970e880b0cf12c73555e542c7b97fd431cc3
Sofia Leon <sofialeon@google.com>
2023-09-12 11:35:07 -0700

    update sample test

--- packages/google-cloud-networkservices/samples/test/quickstart.js
+++ packages/google-cloud-networkservices/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/networkservices').v1;
-const networkservicesClient = new Client();
+const {NetworkServicesClient} = require('@google-cloud/networkservices').v1;
+const networkservicesClient = new NetworkServicesClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
