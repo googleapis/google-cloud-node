6affe68500102cf7dd291870528598680bb33dff
Sofia Leon <sofialeon@google.com>
2023-11-29 23:11:46 -0800

    chore: update the owlbot.py

--- packages/google-cloud-sql/owlbot.py
+++ packages/google-cloud-sql/owlbot.py
@@ -21,23 +21,26 @@ import synthtool.languages.node_mono_repo as node
 import json
 import logging
 from pathlib import Path
+from synthtool import shell
+
 
 def patch(library: Path):
   # remove empty v1 services
-  if re.search("/v1$", str(library)):
-    for service in ['sql_available_database_versions_service', 'sql_events_service', 'sql_iam_policies_service', 'sql_instance_names_service', 'sql_regions_service']:
-      for suffix in ['_client_config.json', '_client.ts', '_proto_list.json']:
-        os.unlink(library / f'src/v1/{service}{suffix}')
-      os.unlink(library / f'test/gapic_{service}_v1.ts')
-    # fix request.id which is Long but used in the path templates
-    s.replace(library / f'src/v1/sql_backup_runs_service_client.ts', r'id: request\.id', 'id: request.id?.toString()')  
+  # if re.search("/v1$", str(library)):
+  #   for service in ['sql_available_database_versions_service', 'sql_events_service', 'sql_iam_policies_service', 'sql_instance_names_service', 'sql_regions_service']:
+  #     for suffix in ['_client_config.json', '_client.ts', '_proto_list.json']:
+  #       os.unlink(library / f'src/v1/{service}{suffix}')
+  #     os.unlink(library / f'test/gapic_{service}_v1.ts')
+  # fix request.id which is Long but used in the path templates
+  s.replace(library / 'src/v1/sql_backup_runs_service_client.ts', 'id: request\.id', 'id: request.id?.toString()')  
   # remove empty v1beta4 services
-  if re.search("/v1beta4$", str(library)):
-    for service in ['sql_iam_policies_service']:
-      for suffix in ['_client_config.json', '_client.ts', '_proto_list.json']:
-        os.unlink(library / f'src/v1beta4/{service}{suffix}')
-      os.unlink(library / f'test/gapic_{service}_v1beta4.ts')
+  # if re.search("/v1beta4$", str(library)):
+  #   for service in ['sql_iam_policies_service']:
+  #     for suffix in ['_client_config.json', '_client.ts', '_proto_list.json']:
+  #       os.unlink(library / f'src/v1beta4/{service}{suffix}')
+  #     os.unlink(library / f'test/gapic_{service}_v1beta4.ts')
     # fix request.id which is Long but used in the path templates
-    s.replace(library / f'src/v1beta4/sql_backup_runs_service_client.ts', r'id: request\.id', 'id: request.id?.toString()')
-
+  s.replace(library / 'src/v1beta4/sql_backup_runs_service_client.ts', 'id: request\.id', 'id: request.id?.toString()')
 node.owlbot_main(relative_dir="packages/google-cloud-sql", templates_excludes=["src/index.ts", "src/v1/index.ts", "src/v1beta4/index.ts", "system-test/fixtures/sample/src/index.js", "system-test/fixtures/sample/src/index.ts"], staging_excludes=["src/index.ts", "src/v1/index.ts"], patch_staging=patch)
+
+shell.run(('rm', '-rf', 'packages/google-cloud-sql/src/v1/sql_available_database_versions_service_client.ts', 'packages/google-cloud-sql/src/v1/sql_available_database_versions_service_client_config.json', 'packages/google-cloud-sql/src/v1/sql_available_database_versions_service_proto_list.json', 'packages/google-cloud-sql/src/v1/sql_events_service_client.ts', 'packages/google-cloud-sql/src/v1/sql_events_service_client_config.json', 'packages/google-cloud-sql/src/v1/sql_events_service_proto_list.json', 'packages/google-cloud-sql/src/v1/sql_iam_policies_service_client.ts','packages/google-cloud-sql/src/v1/sql_iam_policies_service_client_config.json', 'packages/google-cloud-sql/src/v1/sql_iam_policies_service_proto_list.json', 'packages/google-cloud-sql/src/v1/sql_instance_names_service_client.ts', 'packages/google-cloud-sql/src/v1/sql_instance_names_service_client_config.json', 'packages/google-cloud-sql/src/v1/sql_instance_names_service_proto_list.json', 'packages/google-cloud-sql/src/v1/sql_regions_service_client.ts', 'packages/google-cloud-sql/src/v1/sql_regions_service_client_config.json', 'packages/google-cloud-sql/src/v1/sql_regions_service_proto_list.json', 'packages/google-cloud-sql/test/gapic_sql_available_database_versions_service_v1.ts', 'packages/google-cloud-sql/test/gapic_sql_events_service_v1.ts', 'packages/google-cloud-sql/test/gapic_sql_iam_policies_service_v1.ts', 'packages/google-cloud-sql/test/gapic_sql_instance_names_service_v1.ts', 'packages/google-cloud-sql/test/gapic_sql_regions_service_v1.ts'), hide_output = False)
