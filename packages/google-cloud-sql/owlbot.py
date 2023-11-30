521d1cd2a4a235e96e8a16b700f29f71b6c4d702
Sofia Leon <sofialeon@google.com>
2023-11-30 15:35:11 -0800

    retry

--- packages/google-cloud-sql/owlbot.py
+++ packages/google-cloud-sql/owlbot.py
@@ -9,7 +9,7 @@ from synthtool import shell
 def patch(library: Path):
   s.replace(library / 'src/v1/sql_backup_runs_service_client.ts', 'id: request\.id', 'id: request.id?.toString()') 
   s.replace(library / 'src/v1beta4/sql_backup_runs_service_client.ts', 'id: request\.id', 'id: request.id?.toString()')
-  s.replace(library / 'src/v1/index.ts', r'(?s).*', '// Copyright 2023 Google LLC\n//\n'+
+  s.replace(library / 'src/v1/index.ts', r'(?s).*', """// Copyright 2023 Google LLC\n//\n'+
   '// Licensed under the Apache License, Version 2.0 (the License");\n'+
   '// you may not use this file except in compliance with the License.\n'+
   '// You may obtain a copy of the License at\n'+
@@ -34,7 +34,7 @@ def patch(library: Path):
   'export {SqlOperationsServiceClient} from './sql_operations_service_client;\n'+
   'export {SqlSslCertsServiceClient} from './sql_ssl_certs_service_client;\n'+
   'export {SqlTiersServiceClient} from './sql_tiers_service_client;\n'+
-  'export {SqlUsersServiceClient} from './sql_users_service_client;')
+  'export {SqlUsersServiceClient} from './sql_users_service_client;""")
   
 node.owlbot_main(relative_dir="packages/google-cloud-sql", templates_excludes=["src/index.ts", "src/v1/index.ts", "src/v1beta4/index.ts", "system-test/fixtures/sample/src/index.js", "system-test/fixtures/sample/src/index.ts"], staging_excludes=["src/index.ts", "src/v1/index.ts"], patch_staging=patch)
 shell.run(('rm', '-rf', 'packages/google-cloud-sql/src/v1/sql_available_database_versions_service_client.ts', 'packages/google-cloud-sql/src/v1/sql_available_database_versions_service_client_config.json', 'packages/google-cloud-sql/src/v1/sql_available_database_versions_service_proto_list.json', 'packages/google-cloud-sql/src/v1/sql_events_service_client.ts', 'packages/google-cloud-sql/src/v1/sql_events_service_client_config.json', 'packages/google-cloud-sql/src/v1/sql_events_service_proto_list.json', 'packages/google-cloud-sql/src/v1/sql_iam_policies_service_client.ts','packages/google-cloud-sql/src/v1/sql_iam_policies_service_client_config.json', 'packages/google-cloud-sql/src/v1/sql_iam_policies_service_proto_list.json', 'packages/google-cloud-sql/src/v1/sql_instance_names_service_client.ts', 'packages/google-cloud-sql/src/v1/sql_instance_names_service_client_config.json', 'packages/google-cloud-sql/src/v1/sql_instance_names_service_proto_list.json', 'packages/google-cloud-sql/src/v1/sql_regions_service_client.ts', 'packages/google-cloud-sql/src/v1/sql_regions_service_client_config.json', 'packages/google-cloud-sql/src/v1/sql_regions_service_proto_list.json', 'packages/google-cloud-sql/test/gapic_sql_available_database_versions_service_v1.ts', 'packages/google-cloud-sql/test/gapic_sql_events_service_v1.ts', 'packages/google-cloud-sql/test/gapic_sql_iam_policies_service_v1.ts', 'packages/google-cloud-sql/test/gapic_sql_instance_names_service_v1.ts', 'packages/google-cloud-sql/test/gapic_sql_regions_service_v1.ts'), hide_output = False)
