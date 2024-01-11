5d02b1586c8bf4995a7b09d00e715de5e07050ed
Sofia Leon <sofialeon@google.com>
2024-01-10 20:23:26 -0800

    update package name

--- packages/google-shopping-merchant-reports/system-test/fixtures/sample/src/index.ts
+++ packages/google-shopping-merchant-reports/system-test/fixtures/sample/src/index.ts
@@ -16,7 +16,7 @@
 // ** https://github.com/googleapis/gapic-generator-typescript **
 // ** All changes to this file may be overwritten. **
 
-import {ReportServiceClient} from '@google-cloud/reports';
+import {ReportServiceClient} from '@google-shopping/reports';
 
 // check that the client class type name can be used
 function doStuffWithReportServiceClient(client: ReportServiceClient) {
