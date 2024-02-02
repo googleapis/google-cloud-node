8a811041463888036b5120b69fa1adb969033835
Megan Potter <57276408+feywind@users.noreply.github.com>
2024-02-02 14:00:29 -0500

    samples: missed import conversion in TS (#1877)
    I caught this while publishing all of the TypeScript samples.

--- handwritten/pubsub/samples/typescript/createSubscription.ts
+++ handwritten/pubsub/samples/typescript/createSubscription.ts
@@ -33,7 +33,7 @@
 // const subscriptionNameOrId = 'YOUR_SUBSCRIPTION_NAME_OR_ID';
 
 // Imports the Google Cloud client library
-const {PubSub} = require('@google-cloud/pubsub');
+import {PubSub} from '@google-cloud/pubsub';
 
 // Creates a client; cache this for further use
 const pubSubClient = new PubSub();
