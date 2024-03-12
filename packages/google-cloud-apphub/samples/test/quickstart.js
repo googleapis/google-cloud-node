6da5d6055d9d9abdc0fd62a642f8d51356262706
sofisl <55454395+sofisl@users.noreply.github.com>
2024-03-12 11:47:34 -0700

    Update quickstart.js

--- packages/google-cloud-apphub/samples/test/quickstart.js
+++ packages/google-cloud-apphub/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/apphub').v1;
-const apphubClient = new Client();
+const {AppHubClient} = require('@google-cloud/apphub').v1;
+const apphubClient = new AppHubClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
