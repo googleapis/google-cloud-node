fe6de8c71420694db48393834d3b36d98c9b6e08
Daniel Bankhead <danielbankhead@google.com>
2024-05-22 13:49:49 -0700

    chore: Update quickstart.js

--- packages/google-maps-routeoptimization/samples/test/quickstart.js
+++ packages/google-maps-routeoptimization/samples/test/quickstart.js
@@ -34,7 +34,7 @@ describe('Quickstart', () => {
 
   it('should run quickstart', async () => {
     const output = execSync(
-      `node ./quickstart.js projects/${projectId}/locations/us-central1`,
+      `node ./quickstart.js projects/${projectId}`,
       {cwd}
     );
     assert(output !== null);
