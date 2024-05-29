ad4e51fec37c0c0cbd7d3d0c44a96f4ed4959c7b
sofisl <55454395+sofisl@users.noreply.github.com>
2024-05-29 16:49:45 -0700

    Update quickstart.js

--- packages/google-streetview-publish/samples/test/quickstart.js
+++ packages/google-streetview-publish/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/publish').v1;
-const publishClient = new Client();
+const {StreetViewPublishServiceClient} = require('@google-cloud/publish').v1;
+const publishClient = new StreetViewPublishServiceClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
