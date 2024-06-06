efdc51eaa999b673956b8bab519d9178ea4189c3
sofisl <55454395+sofisl@users.noreply.github.com>
2024-06-06 13:57:41 -0700

    chore: remove requiring fetch (#5440)
    We should just magically get it... https://github.com/danmichaelo/github-script/blob/da8818015eb5c3d01f8ad6db6bd8e3a2b460c71c/README.md

--- .github/scripts/close-invalid-link.cjs
+++ .github/scripts/close-invalid-link.cjs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const fetch = require('node-fetch');
-
 async function closeIssue() {
     await github.rest.issues.update({
         owner: owner,
