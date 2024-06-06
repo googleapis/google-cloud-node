2505c344256b601962a1ab8f89afa650b6584b1c
sofisl <55454395+sofisl@users.noreply.github.com>
2024-06-06 12:06:10 -0700

    Update .jsdoc.js

--- packages/google-shopping-merchant-datasources/.jsdoc.js
+++ packages/google-shopping-merchant-datasources/.jsdoc.js
@@ -43,7 +43,7 @@ module.exports = {
     copyright: 'Copyright 2024 Google LLC',
     includeDate: false,
     sourceFiles: false,
-    systemName: '@google-cloud/datasources',
+    systemName: '@google-shopping/datasources',
     theme: 'lumen',
     default: {
       outputSourceFiles: false
