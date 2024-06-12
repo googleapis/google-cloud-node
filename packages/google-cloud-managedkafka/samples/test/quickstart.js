776322d10043b160fd320f7358603c7f3bb3a270
sofisl <55454395+sofisl@users.noreply.github.com>
2024-06-12 10:36:21 -0700

    Update quickstart.js

--- packages/google-cloud-managedkafka/samples/test/quickstart.js
+++ packages/google-cloud-managedkafka/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/managedkafka').v1;
-const managedkafkaClient = new Client();
+const {ManagedKafkaClient} = require('@google-cloud/managedkafka').v1;
+const managedkafkaClient = new ManagedKafkaClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
