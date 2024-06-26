500fbf1e94148d9df1f9bf7caf6e7b64200cf14b
sofisl <55454395+sofisl@users.noreply.github.com>
2024-06-26 13:52:54 -0700

    Update run_conditional_tests.sh

--- ci/run_conditional_tests.sh
+++ ci/run_conditional_tests.sh
@@ -45,7 +45,7 @@ elif [ ${BUILD_TYPE} == "continuous" ]; then
     GIT_DIFF_ARG="HEAD~.."
 
     # Then fetch one last commit for getting the diff.
-    git fetch origin main --deepen=0
+    git fetch origin main --deepen=1
 
 else
     # Run everything.
