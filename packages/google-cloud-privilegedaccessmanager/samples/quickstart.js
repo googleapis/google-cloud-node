0bc2e498dd4b5a7bce2bd64607230c79db2f639e
Daniel Bankhead <danielbankhead@google.com>
2024-07-10 17:20:21 -0700

    fix: parent

--- packages/google-cloud-privilegedaccessmanager/samples/quickstart.js
+++ packages/google-cloud-privilegedaccessmanager/samples/quickstart.js
@@ -29,7 +29,7 @@ function main(parent) {
   /**
    *  Required. The parent which owns the entitlement resources.
    */
-  // const parent = 'projects/${projectId}/locations/us-central1'
+  // const parent = 'projects/${projectId}'
   /**
    *  Optional. Requested page size. Server may return fewer items than
    *  requested. If unspecified, the server picks an appropriate default.
