4ed34dc51bc2a7d54d9c90b3a779945dd93b813f
sofisl <55454395+sofisl@users.noreply.github.com>
2024-07-17 16:14:13 -0500

    Update quickstart.js

--- packages/google-cloud-gdchardwaremanagement/samples/test/quickstart.js
+++ packages/google-cloud-gdchardwaremanagement/samples/test/quickstart.js
@@ -18,8 +18,9 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/gdchardwaremanagement').v1alpha;
-const gdchardwaremanagementClient = new Client();
+const {GDCHardwareManagementClient} =
+  require('@google-cloud/gdchardwaremanagement').v1alpha;
+const gdchardwaremanagementClient = new GDCHardwareManagementClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
