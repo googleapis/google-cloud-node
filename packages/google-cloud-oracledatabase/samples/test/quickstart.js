82499b0ad9fe26204ef530c17fb7eb63096465f5
sofisl <55454395+sofisl@users.noreply.github.com>
2024-10-01 12:21:28 -0700

    Update quickstart.js

--- packages/google-cloud-oracledatabase/samples/test/quickstart.js
+++ packages/google-cloud-oracledatabase/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/oracledatabase').v1;
-const oracledatabaseClient = new Client();
+const {OracleDatabaseClient} = require('@google-cloud/oracledatabase').v1;
+const oracledatabaseClient = new OracleDatabaseClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
