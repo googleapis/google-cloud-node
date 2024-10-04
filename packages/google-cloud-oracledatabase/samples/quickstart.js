901da14439460b2b5c6d97b528fd4910dc1cec19
sofisl <55454395+sofisl@users.noreply.github.com>
2024-10-04 15:42:09 -0700

    Update quickstart.js

--- packages/google-cloud-oracledatabase/samples/quickstart.js
+++ packages/google-cloud-oracledatabase/samples/quickstart.js
@@ -54,7 +54,7 @@ function main(parent) {
   const {OracleDatabaseClient} = require('@google-cloud/oracledatabase').v1;
 
   // Instantiates a client
-  const oracledatabaseClient = new OracleDatabaseClient({fallback: 'rest'});
+  const oracledatabaseClient = new OracleDatabaseClient({fallback: true});
 
   async function callListAutonomousDatabases() {
     // Construct request
