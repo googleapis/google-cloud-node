1b71f9a19be6b5ac62099b602697c08c636ba693
Kevin Kim <kevkim@google.com>
2024-10-24 16:18:25 -0400

    fix: query object description (#1340)
    FIxes strange query object description in:
    
    https://cloud.google.com/nodejs/docs/reference/datastore/latest/overview#query

--- handwritten/datastore/src/query.ts
+++ handwritten/datastore/src/query.ts
@@ -53,10 +53,10 @@ export interface Filter {
 }
 
 /**
- * Build a Query object.
+ * A Query object is used to build and execute queries for entities stored in Datastore.
  *
- * **Queries are built with {module:datastore#createQuery} and
- * {@link Transaction#createQuery}.**
+ * Create a Query object with {@link Datastore#createQuery} or
+ * {@link Transaction#createQuery}.
  *
  * @see {@link http://goo.gl/Cag0r6| Datastore Queries}
  *
