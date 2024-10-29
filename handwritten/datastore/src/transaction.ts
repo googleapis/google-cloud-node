ddb853424beec4985bb98b6c4e340e13eb64366b
danieljbruce <danieljbruce@users.noreply.github.com>
2024-10-29 10:19:54 -0400

    docs: Add parameters to second commit overload (#1343)
    * Add parameters to second commit overload
    
    * docs: Add parameters to second commit overload

--- handwritten/datastore/src/transaction.ts
+++ handwritten/datastore/src/transaction.ts
@@ -159,6 +159,15 @@ class Transaction extends DatastoreRequest {
    * ```
    */
   commit(gaxOptions?: CallOptions): Promise<CommitResponse>;
+  /**
+   * @param {object} [gaxOptions] Request configuration options, outlined here:
+   *     https://googleapis.github.io/gax-nodejs/global.html#CallOptions.
+   * @param {function} callback The callback function.
+   * @param {?error} callback.err An error returned while making this request.
+   *   If the commit fails, we automatically try to rollback the transaction
+   * (see {module:datastore/transaction#rollback}).
+   * @param {object} callback.apiResponse The full API response.
+   */
   commit(callback: CommitCallback): void;
   commit(gaxOptions: CallOptions, callback: CommitCallback): void;
   commit(
