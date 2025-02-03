3d3be1e05b5d1971c76b230c0171eb3f7a58b54a
Daniel Bankhead <danielbankhead@google.com>
2025-02-02 21:04:12 -0800

    chore: fix samples test

--- packages/google-cloud-parametermanager/samples/test/quickstart.js
+++ packages/google-cloud-parametermanager/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/parametermanager').v1;
-const parametermanagerClient = new Client();
+const {ParameterManagerClient} = require('@google-cloud/parametermanager').v1;
+const parametermanagerClient = new ParameterManagerClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
