315036d395eed631dcf1cc4c624f74c4e00cd077
sofisl <55454395+sofisl@users.noreply.github.com>
2025-02-26 12:19:54 -0800

    chore: remove dataform v1palpha2 surface as it has been deprecated (#6059)
    * chore: remove dataform v1palpha2 surface as it has been deprecated"
    
    * rerun protos
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    ---------
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- packages/google-cloud-dataform/src/index.ts
+++ packages/google-cloud-dataform/src/index.ts
@@ -16,13 +16,12 @@
 // ** https://github.com/googleapis/synthtool **
 // ** All changes to this file may be overwritten. **
 
-import * as v1alpha2 from './v1alpha2';
 import * as v1beta1 from './v1beta1';
 
 const DataformClient = v1beta1.DataformClient;
 type DataformClient = v1beta1.DataformClient;
 
-export {v1alpha2, v1beta1, DataformClient};
-export default {v1alpha2, v1beta1, DataformClient};
+export {v1beta1, DataformClient};
+export default {v1beta1, DataformClient};
 import * as protos from '../protos/protos';
 export {protos};
