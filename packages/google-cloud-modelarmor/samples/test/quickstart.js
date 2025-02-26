d4835578e76185e2ae9412228d41fdcd6f24d1f9
Daniel Bruce <djbruce@google.com>
2025-02-26 10:23:06 -0500

    Change client

--- packages/google-cloud-modelarmor/samples/test/quickstart.js
+++ packages/google-cloud-modelarmor/samples/test/quickstart.js
@@ -18,8 +18,8 @@ const assert = require('assert');
 const path = require('path');
 const cp = require('child_process');
 const {describe, it, before} = require('mocha');
-const {Client} = require('@google-cloud/modelarmor').v1;
-const modelarmorClient = new Client();
+const {ModelArmorClient} = require('@google-cloud/modelarmor').v1;
+const modelarmorClient = new ModelArmorClient();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
