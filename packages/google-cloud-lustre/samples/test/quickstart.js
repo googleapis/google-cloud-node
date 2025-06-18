ee0bc33794b411a969e0ecd8e358c205e04887e4
sofisl <55454395+sofisl@users.noreply.github.com>
2025-06-18 11:07:27 -0700

    Update quickstart.js

--- packages/google-cloud-lustre/samples/test/quickstart.js
+++ packages/google-cloud-lustre/samples/test/quickstart.js
@@ -38,7 +38,7 @@ describe('Quickstart', () => {
 
   it('should run quickstart', async () => {
     const output = execSync(
-      `node ./quickstart.js projects/${projectId}/locations/us-central1-a`,
+      `node ./quickstart.js projects/${projectId}/locations/-`,
       {cwd}
     );
     assert(output !== null);
