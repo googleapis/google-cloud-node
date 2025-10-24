b2eb141795362eeb2fac5ecb7241b008507877c7
Sofia Leon <sofialeon@google.com>
2025-10-24 19:53:57 +0000

    chore: setup test fixtures correctly

--- packages/gapic-node-processing/test/fixtures/combined-library/google-cloud-speech/system-test/fixtures/sample/src/index.js
+++ packages/gapic-node-processing/test/fixtures/combined-library/google-cloud-speech/system-test/fixtures/sample/src/index.js
@@ -21,7 +21,6 @@
 const speech = require('@google-cloud/speech');
 
 function main() {
-  const adaptationClient = new speech.AdaptationClient();
   const speechClient = new speech.SpeechClient();
 }
 
