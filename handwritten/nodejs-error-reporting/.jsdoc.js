d81020069dbb9ef53ebde8d09db32c25bc786c18
Gautam Sharda <gautamsharda@google.com>
2026-01-19 22:00:20 +0000

    chore: global dependency overrides and internal license strings
    - Move pnpm overrides to root package.json to ensure CI coverage.
    - Update internal copyright strings in .jsdoc.js, .OwlBot.yaml, and owlbot.py.

--- handwritten/nodejs-error-reporting/.jsdoc.js
+++ handwritten/nodejs-error-reporting/.jsdoc.js
@@ -34,7 +34,7 @@ module.exports = {
     includePattern: '\\.js$'
   },
   templates: {
-    copyright: 'Copyright 2019 Google, LLC.',
+    copyright: 'Copyright 2026 Google LLC',
     includeDate: false,
     sourceFiles: false,
     systemName: '@google-cloud/error-reporting',
