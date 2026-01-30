d979239cec23de887d0388612f31cd9dd98ba668
sofisl <55454395+sofisl@users.noreply.github.com>
2026-01-30 00:38:59 -0800

    Update samples-test.sh

--- handwritten/cloud-profiler/.kokoro/samples-test.sh
+++ handwritten/cloud-profiler/.kokoro/samples-test.sh
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eo pipefail
-
 export NPM_CONFIG_PREFIX=${HOME}/.npm-global
 
 # Setup service account credentials.
