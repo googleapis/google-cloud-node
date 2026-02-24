62c20bd2ab34c8246d9be900f68bf983fcdf7912
Sofia Leon <sofialeon@google.com>
2026-02-24 15:26:18 -0800

    build: add release-please config, fix owlbot-config

--- handwritten/firestore/.kokoro/pre-samples-test.sh
+++ handwritten/firestore/.kokoro/pre-samples-test.sh
@@ -14,5 +14,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export GCLOUD_PROJECT=nodejs-firestore-ci
-export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/secret_manager/nodejs-firestore-ci-samples-71b5f8aee66e
+export GCLOUD_PROJECT=nodejs-firestore/handwritten/firestore-ci
+export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/secret_manager/nodejs-firestore/handwritten/firestore-ci-samples-71b5f8aee66e
--- handwritten/firestore/owlbot.py
+++ handwritten/firestore/owlbot.py
@@ -1,6 +1,6 @@
 import synthtool as s
 import synthtool.gcp as gcp
-import synthtool.languages.node as node
+import synthtool.languages.node_mono_repo as node
 import logging
 import os
 import subprocess
