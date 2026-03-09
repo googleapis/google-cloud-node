b58e68521fd5f78f2690ab9eb1dafe6f3e91cc7d
Gabe Pearhill <gabepearhill@google.com>
2026-03-09 09:52:39 -0700

    chore: update package unit tests to install dependencies with bun

--- ci/run_conditional_tests.sh
+++ ci/run_conditional_tests.sh
@@ -30,6 +30,9 @@ fi
 # A script file for running the test in a sub project.
 test_script="${PROJECT_ROOT}/ci/run_single_test.sh"
 
+# A script file for running a GAPIC package test.
+test_package_script="${PROJECT_ROOT}/ci/run_single_package_test.sh"
+
 
 if [ ${BUILD_TYPE} == "presubmit" ]; then
     # For presubmit build, we want to know the difference from the
@@ -90,6 +93,9 @@ tests_with_credentials="packages/google-analytics-admin/ packages/google-area120
 # Until these packages can be updated to be OS agnostic, we will skip them on Windows.
 windows_exempt_tests=".github/scripts/fixtures/ .github/scripts/tests/ packages/gapic-node-processing/ packages/typeless-sample-bot/"
 
+# Install all root and package/* dependencies at once (allowing for dedpulication).
+bun install --ignore-scripts
+
 for subdir in ${subdirs[@]}; do
     for d in `ls -d ${subdir}/*/`; do
         if [ -f "ignore.json" ] && jq -e ".ignored[] | select(. == \"$d\")" ignore.json > /dev/null; then
@@ -148,7 +154,11 @@ for subdir in ${subdirs[@]}; do
             pushd ${d}
             # Temporarily allow failure.
             set +e
-            ${test_script}
+            if [[ "${subdir}" == "packages" ]]; then
+                ${test_package_script}
+            else
+                ${test_script}
+            fi
             ret=$?
             set -e
             if [ ${ret} -ne 0 ]; then
---
+++ ci/run_single_package_test.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+export REGION_ID='uc'
+export PROJECT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/..)
+export NODE_OPTIONS=--max_old_space_size=4096
+
+if [ -z "${BUILD_TYPE}" ]; then
+    echo "missing BUILD_TYPE env var"
+    exit 1
+fi
+
+if [ -z "${TEST_TYPE}" ]; then
+    TEST_TYPE="units"
+fi
+
+d=$(pwd)
+PROJECT=$(basename ${d})
+
+if [ ${BUILD_TYPE} != "presubmit" ]; then
+    # Activate mocha config
+    export MOCHA_REPORTER_OUTPUT=${PROJECT}_sponge_log.xml
+    export MOCHA_REPORTER_SUITENAME=${PROJECT}
+    export MOCHA_REPORTER=xunit
+fi
+
+# Install dependencies
+echo "Running `bun prepare` in ${d}"
+bun run "prepare"
+
+
+retval=0
+
+# Use NPM run to respece the currently installed node version.s
+set +e
+case ${TEST_TYPE} ins
+lint)
+    npm run prelint
+    npm run lint
+    retval=$?
+    ;;
+samples)
+    npm run samples-test
+    retval=$?
+    ;;
+system)
+    npm run system-test
+    retval=$?
+    ;;
+units)
+    npm run test
+    retval=$?
+    ;;
+*)
+    ;;
+esac
+set -e
+
+exit ${retval}
