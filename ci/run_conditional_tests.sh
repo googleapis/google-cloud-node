13f823eb6e1f60d746837f7bc1788b02bc81a953
Gabe Pearhill <gabepearhill@google.com>
2026-03-09 16:42:38 -0700

    chore: try running turbo with bun x to see if that uses it's compatibility shell

--- ci/run_conditional_tests.sh
+++ ci/run_conditional_tests.sh
@@ -94,20 +94,12 @@ for subdir in ${subdirs[@]}; do
     if [[ "${subdir}" == "packages" && "${TEST_TYPE}" == "units" ]]; then
         cd ${PROJECT_ROOT}
 
-        if [[ "$OSTYPE" == "msys" ]]; then
-            echo "setting script-shell to bash for Windows for compatibility"
-            npm config set script-shell "C:\\Program Files\\Git\\bin\\bash.exe"
-        fi
-
-        echo "installing turbo . . ."
-        bun install -g turbo
-
         echo "installing dependencies . . ."
         bun install --ignore-scripts
 
         echo "running all unit tests for packages"
         set +e
-        turbo run test
+        bun x turbo run test
         ret=$?
         set -e
 
