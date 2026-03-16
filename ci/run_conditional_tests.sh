bc602d53d285d3822e32ff7da232a199be92265e
Sofia Leon <sofialeon@google.com>
2026-03-16 18:58:29 +0000

    chore: exit early if one test fails

--- ci/run_conditional_tests.sh
+++ ci/run_conditional_tests.sh
@@ -170,7 +170,7 @@ for subdir in ${subdirs[@]}; do
             if [ ${ret} -ne 0 ]; then
                 RETVAL=${ret}
                 # Since there are so many APIs, we should exit early if there's an error
-                break
+                exit ${RETVAL}
             fi
             popd
         fi
