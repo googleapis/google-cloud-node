61f0fbaa693adc2b6caf70d901cdad6ca56113c6
Sofia Leon <sofialeon@google.com>
2026-03-16 21:49:00 +0000

    chore: add pnpm and remove tsbuild, and add to gitignore

--- ci/run_conditional_tests.sh
+++ ci/run_conditional_tests.sh
@@ -92,7 +92,7 @@ tests_with_credentials="packages/google-analytics-admin/ packages/google-area120
 # on Windows due to incompatible npm scripts.
 # 
 # Until these packages can be updated to be OS agnostic, we will skip them on Windows.
-windows_exempt_tests="core/ .github/scripts/fixtures/ .github/scripts/tests/ packages/gapic-node-processing/ packages/typeless-sample-bot/"
+windows_exempt_tests="core/ core/packages core/dev-packages .github/scripts/fixtures/ .github/scripts/tests/ packages/gapic-node-processing/ packages/typeless-sample-bot/"
 
 for subdir in ${subdirs[@]}; do
     for d in `ls -d ${subdir}/*/`; do
