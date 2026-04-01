bdba4ddf8b4455c29d1d025e12902e7a56f9e9c9
Daniel Bruce <djbruce@google.com>
2026-04-01 16:28:20 -0400

    listParams add

--- handwritten/bigquery/src/bigquery.ts
+++ handwritten/bigquery/src/bigquery.ts
@@ -2294,6 +2294,7 @@ export class BigQuery extends Service {
               rows = BigQuery.mergeSchemaWithRows_(res.schema, res.rows, {
                 wrapIntegers: options.wrapIntegers || false,
                 parseJSON: options.parseJSON,
+                listParams,
               });
               delete res.rows;
             }
