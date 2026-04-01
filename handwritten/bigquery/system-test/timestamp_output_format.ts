a959b3ac379029012124e404c71423d48dc1253c
Daniel Bruce <djbruce@google.com>
2026-04-01 16:59:52 -0400

    remove only

--- handwritten/bigquery/system-test/timestamp_output_format.ts
+++ handwritten/bigquery/system-test/timestamp_output_format.ts
@@ -28,7 +28,7 @@ interface TestCase {
   expectedTsValue?: string;
 }
 
-describe.only('Timestamp Output Format System Tests', () => {
+describe('Timestamp Output Format System Tests', () => {
   const datasetId = `timestamp_test_${randomUUID().replace(/-/g, '_')}`;
   const tableId = `timestamp_table_${randomUUID().replace(/-/g, '_')}`;
   const dataset = bigquery.dataset(datasetId);
