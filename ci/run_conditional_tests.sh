a076c1722b816f97a1ba8557bda7f0ba220972b8
Gabe Pearhill <gabepearhill@google.com>
2026-04-07 15:04:38 -0700

    chore: disable system tests in packages to unblock PRs pending fix

--- ci/run_conditional_tests.sh
+++ ci/run_conditional_tests.sh
@@ -113,6 +113,11 @@ for subdir in ${subdirs[@]}; do
             continue
         fi
 
+        if [[ "${subdir}" == "packages" && "${TEST_TYPE}" == "system" ]]; then
+            echo "Skipping ${TEST_TYPE} test for packages: ${d}"
+            continue
+        fi
+
         # Our CI uses Git Bash on Windows to execute this script, which returns "msys" for OSTYPE.
         if [[ "$OSTYPE" == "msys" ]]; then
             if [[ "${windows_exempt_tests}" =~ "${d}" ]]; then
