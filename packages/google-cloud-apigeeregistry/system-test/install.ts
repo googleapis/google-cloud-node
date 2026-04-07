99855489d0752af3e5fdebbaa492f186ced9ab2a
Gabe Pearhill <gabepearhill@google.com>
2026-04-07 13:58:57 -0700

    revert change to google-cloud-apigeeregistry to work around DEADLINE_EXCEEDED error in service

--- packages/google-cloud-apigeeregistry/system-test/install.ts
+++ packages/google-cloud-apigeeregistry/system-test/install.ts
@@ -40,7 +40,7 @@ describe('📦 pack-n-play test', () => {
       packageDir: process.cwd(),
       sample: {
         description: 'JavaScript user can use the library',
-        cjs: readFileSync('./system-test/fixtures/sample/src/index.js').toString()
+        ts: readFileSync('./system-test/fixtures/sample/src/index.js').toString()
       }
     };
     await packNTest(options);
