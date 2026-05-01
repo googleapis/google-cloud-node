884a703f63930b61e029643821d0e18f83ce5995
feywind <57276408+feywind@users.noreply.github.com>
2026-05-01 18:32:31 -0400

    chore: re-add deleted avro-js

--- handwritten/pubsub/system-test/avro-js.d.ts
+++ handwritten/pubsub/system-test/avro-js.d.ts
@@ -1,4 +1,4 @@
-// Copyright 2026 Google LLC
+// Copyright 2021 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// This one doesn't seem to have typings.
 declare module 'avro-js' {
   function parse(def: string): Parser;
 
