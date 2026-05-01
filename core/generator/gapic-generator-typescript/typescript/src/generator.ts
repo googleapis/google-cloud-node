90e95ad2a2ff9edd97542c83de54ee1cff098ec5
Daniel Bruce <djbruce@google.com>
2026-05-01 14:26:27 -0400

    Add typescript_packing_test back in.

--- core/generator/gapic-generator-typescript/typescript/src/generator.ts
+++ core/generator/gapic-generator-typescript/typescript/src/generator.ts
@@ -56,7 +56,11 @@ const templatesDirectory = fs.existsSync(
 )
   ? '../../gapic_generator_typescript/templates'
   : path.join(__dirname, '..', '..', 'templates');
-const defaultTemplates = ['typescript_gapic', 'typescript_samples'];
+const defaultTemplates = [
+  'typescript_gapic',
+  'typescript_packing_test',
+  'typescript_samples',
+];
 const metadataTemplate = 'typescript_gapic_metadata';
 
 export class Generator {
