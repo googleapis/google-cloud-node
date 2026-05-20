5b540042522bf85cbe49cc86bdacc2b40f14281f
Sakthivel Subramanian <sakthivelmani@google.com>
2026-05-20 10:17:44 +0000

    fix(build): evaluate diff check prior to changing directories in trampoline scripts

--- handwritten/spanner/.kokoro/trampoline_v2.sh
+++ handwritten/spanner/.kokoro/trampoline_v2.sh
@@ -254,9 +254,6 @@ else
     PROJECT_ROOT="$(repo_root $(pwd))/handwritten/spanner"
 fi
 
-log_yellow "Changing to the project root: ${PROJECT_ROOT}."
-cd "${PROJECT_ROOT}"
-
 # Auto-injected conditional check
 # Check if the package directory has changes. If not, skip tests.
 if [[ "${RUNNING_IN_CI:-}" == "true" ]]; then
@@ -276,6 +273,9 @@ if [[ "${RUNNING_IN_CI:-}" == "true" ]]; then
         echo "Changes detected in ${RELATIVE_PKG_PATH}. Proceeding with tests."
     fi
 fi
+
+log_yellow "Changing to the project root: ${PROJECT_ROOT}."
+cd "${PROJECT_ROOT}"
 
 # To support relative path for `TRAMPOLINE_SERVICE_ACCOUNT`, we need
 # to use this environment variable in `PROJECT_ROOT`.
@@ -511,3 +511,4 @@ if [[ "${update_cache}" == "true" ]] && \
 fi
 
 exit "${test_retval}"
+
