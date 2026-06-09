090127cd2682e46eabbafa6b4a5c234f208f1e8a
Gabe Pearhill <gabepearhill@google.com>
2026-06-09 10:50:07 -0700

    chore: remove unecessary formatting changes

--- packages/google-devtools-containeranalysis/librarian.js
+++ packages/google-devtools-containeranalysis/librarian.js
@@ -71,18 +71,18 @@ v1beta1Files.forEach(file => {
 const containerAnalysisClientFile = path.resolve('packages/google-devtools-containeranalysis/src/v1/container_analysis_client.ts');
 const grafeasClientMethod = `
     /**
-     * Returns an instance of a @google-cloud/grafeas client, configured to
-     * connect to Google Cloud's Container Analysis API. For documentation
-     * on this client, see:
-     * <a href="https://googleapis.dev/nodejs/grafeas/latest/index.html">https://googleapis.dev/nodejs/grafeas/latest/index.html</a>
-     *
-     * @returns {GrafeasClient} - An instance of a Grafeas client.
-     * 
-     */
+    * Returns an instance of a @google-cloud/grafeas client, configured to
+    * connect to Google Cloud's Container Analysis API. For documentation
+    * on this client, see:
+    * <a href="https://googleapis.dev/nodejs/grafeas/latest/index.html">https://googleapis.dev/nodejs/grafeas/latest/index.html</a>
+    *
+    * @returns {GrafeasClient} - An instance of a Grafeas client.
+    *
+    */
     getGrafeasClient() {
         return new GrafeasClient(this._opts as {});
     }
-}`;
+    }`;
 
 try {
   const clientContent = fs.readFileSync(containerAnalysisClientFile, 'utf8');
