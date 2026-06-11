a896bdfaf1a5d54c6d6ed1ee0adc5e53331ac22b
Tomo Suzuki <suztomo@gmail.com>
2026-06-11 15:01:20 +0000

    chore: remove GrafeasClient from top-level exports to match main

--- packages/google-devtools-containeranalysis/system-test/fixtures/sample/src/index.js
+++ packages/google-devtools-containeranalysis/system-test/fixtures/sample/src/index.js
@@ -16,13 +16,12 @@
 // ** https://github.com/googleapis/gapic-generator-typescript **
 // ** All changes to this file may be overwritten. **
 
-
 /* eslint-disable node/no-missing-require, no-unused-vars */
 const containeranalysis = require('@google-cloud/containeranalysis');
 
 function main() {
-  const containerAnalysisClient = new containeranalysis.ContainerAnalysisClient();
-  const grafeasClient = new containeranalysis.GrafeasClient();
+  const containerAnalysisClient =
+    new containeranalysis.ContainerAnalysisClient();
 }
 
 main();
--- packages/google-devtools-containeranalysis/system-test/fixtures/sample/src/index.ts
+++ packages/google-devtools-containeranalysis/system-test/fixtures/sample/src/index.ts
@@ -16,16 +16,10 @@
 // ** https://github.com/googleapis/gapic-generator-typescript **
 // ** All changes to this file may be overwritten. **
 
-import {
-  ContainerAnalysisClient,
-  GrafeasClient,
-} from '@google-cloud/containeranalysis';
+import {ContainerAnalysisClient} from '@google-cloud/containeranalysis';
 
 // check that the client class type name can be used
 function doStuffWithContainerAnalysisClient(client: ContainerAnalysisClient) {
-  client.close();
-}
-function doStuffWithGrafeasClient(client: GrafeasClient) {
   client.close();
 }
 
@@ -33,9 +27,6 @@ function main() {
   // check that the client instance can be created
   const containerAnalysisClient = new ContainerAnalysisClient();
   doStuffWithContainerAnalysisClient(containerAnalysisClient);
-  // check that the client instance can be created
-  const grafeasClient = new GrafeasClient();
-  doStuffWithGrafeasClient(grafeasClient);
 }
 
 main();
