8de37766271683ed44e98edab4af339b42a075d6
Jon Skeet <jonskeet@google.com>
2026-06-12 17:39:28 +0000

    chore: migrate google-cloud-securitycenter to librarian

--- packages/google-cloud-securitycenter/samples/generated/v2/security_center.create_finding.js
+++ packages/google-cloud-securitycenter/samples/generated/v2/security_center.create_finding.js
@@ -50,7 +50,7 @@ function main(parent, findingId, finding) {
   // const finding = {}
 
   // Imports the Securitycenter library
-  const {SecurityCenterClient} = require('@google-cloud/securitycenter').v2;
+  const {SecurityCenterClient} = require('@google-cloud/security-center').v2;
 
   // Instantiates a client
   const securitycenterClient = new SecurityCenterClient();
