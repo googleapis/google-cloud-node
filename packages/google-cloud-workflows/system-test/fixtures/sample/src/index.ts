5f0085a454143dc349e036d4e1854a09896bc738
Jon Skeet <jonskeet@google.com>
2026-06-12 10:52:27 +0000

    feat: regenerated google-cloud-workflows

--- packages/google-cloud-workflows/system-test/fixtures/sample/src/index.ts
+++ packages/google-cloud-workflows/system-test/fixtures/sample/src/index.ts
@@ -16,7 +16,7 @@
 // ** https://github.com/googleapis/gapic-generator-typescript **
 // ** All changes to this file may be overwritten. **
 
-import {ExecutionsClient} from '@google-cloud/workflows';
+import { ExecutionsClient } from '@google-cloud/workflows';
 
 // check that the client class type name can be used
 function doStuffWithExecutionsClient(client: ExecutionsClient) {
