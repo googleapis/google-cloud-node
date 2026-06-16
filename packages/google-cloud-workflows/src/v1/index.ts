2406548b35d9906d27e61833564f8153390390c1
Jon Skeet <jonskeet@google.com>
2026-06-16 10:20:39 +0000

    fix!: regenerate google-cloud-workflows
    This removes the workflows-executions client, which will be moved to a
    new package.

--- packages/google-cloud-workflows/src/v1/index.ts
+++ packages/google-cloud-workflows/src/v1/index.ts
@@ -16,4 +16,4 @@
 // ** https://github.com/googleapis/gapic-generator-typescript **
 // ** All changes to this file may be overwritten. **
 
-export {ExecutionsClient} from './executions_client';
+export { WorkflowsClient } from './workflows_client';
