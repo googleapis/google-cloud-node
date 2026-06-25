ff27f7727b16dcb09ecbe207ebbb12de12cbc6f7
Daniel Bruce <djbruce@google.com>
2026-06-25 15:57:37 -0400

    Add more kokoro system test files to the excludes

--- handwritten/pubsub/owlbot.py
+++ handwritten/pubsub/owlbot.py
@@ -24,5 +24,8 @@ node.owlbot_main(relative_dir="handwritten/pubsub",templates_excludes=[
     '.eslintignore',
     '.OwlBot.yaml',
     'renovate.json',
-    "README.md"
+    "README.md",
+    ".kokoro/system-test.sh",
+    ".kokoro/continuous/node18/system-test.cfg",
+    ".kokoro/presubmit/node18/system-test.cfg",
 ])
