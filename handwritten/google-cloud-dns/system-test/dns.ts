8b13ca4e8fd7c0f5ff90a32f05a59e084084a4f7
Daniel Bruce <djbruce@google.com>
2026-07-03 15:47:17 -0400

    Add a comment about skipping the dns tests

--- handwritten/google-cloud-dns/system-test/dns.ts
+++ handwritten/google-cloud-dns/system-test/dns.ts
@@ -34,8 +34,8 @@ const delayMs = async (ms = 1000) => {
   });
 };
 
-// Only run the tests if there is a domain to test with.
-// This test suite was skipped because it failed due to the migration from kokoro to GCB.
+// This suite was skipped since it failed when migrating from kokoro to GCB.
+// We should apply the proper permissions later to turn this test back on.
 describe.skip('dns', () => {
   const ZONE_NAME = 'test-zone-' + crypto.randomUUID().substr(0, 18);
   const ZONE = dns.zone(ZONE_NAME);
