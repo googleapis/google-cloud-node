4bbc21dfe1afff801970ccca4955ffbb11150da3
JoeWang1127 <joewa@google.com>
2026-07-04 19:39:44 +0000

    regenerate

--- packages/google-cloud-workflows/src/index.ts
+++ packages/google-cloud-workflows/src/index.ts
@@ -19,10 +19,10 @@
 import * as v1 from './v1';
 import * as v1beta from './v1beta';
 
-const WorkflowsClient = v1.WorkflowsClient;
-type WorkflowsClient = v1.WorkflowsClient;
+const ExecutionsClient = v1.ExecutionsClient;
+type ExecutionsClient = v1.ExecutionsClient;
 
-export {v1, v1beta, WorkflowsClient};
-export default {v1, v1beta, WorkflowsClient};
+export {v1, v1beta, ExecutionsClient};
+export default {v1, v1beta, ExecutionsClient};
 import * as protos from '../protos/protos';
 export {protos};
