f0ad7416070e2d74e05e86aecb35e76159acec55
Santiago Quiroga <santiquiroga@google.com>
2026-07-16 22:21:47 -0700

    fix(generator): support explicit directory option and use in package.json/README templates

--- core/generator/gapic-generator-typescript/typescript/src/generator.ts
+++ core/generator/gapic-generator-typescript/typescript/src/generator.ts
@@ -71,6 +71,7 @@ export class Generator {
   paramMap: OptionsMap;
   // This field is for users passing proper publish package name like @google-cloud/text-to-speech.
   publishName?: string;
+  directory?: string;
   // For historical reasons, Webpack library name matches "the main" service of the client library.
   // Sometimes it's hard to figure out automatically, so making this an option.
   mainServiceName?: string;
@@ -186,6 +187,7 @@ export class Generator {
 
   private readPublishPackageName() {
     this.publishName = this.paramMap['package-name'];
+    this.directory = this.paramMap['directory'];
   }
 
   private readMainServiceName() {
@@ -326,6 +328,7 @@ export class Generator {
       grpcServiceConfig: this.grpcServiceConfig,
       bundleConfigs: this.bundleConfigs,
       publishName: this.publishName,
+      directory: this.directory,
       mainServiceName: this.mainServiceName,
       serviceYaml: this.serviceYaml,
       rest: this.rest,
