0d9eb53e27a124a66332e25ef63ce2a597dfa222
Brent Shaffer <betterbrent@google.com>
2026-07-29 20:57:58 +0000

    chore(ci): reduce unit-test output due to log truncation

--- ci/run_single_test.sh
+++ ci/run_single_test.sh
@@ -37,6 +37,8 @@ if [ ${BUILD_TYPE} != "presubmit" ]; then
     export MOCHA_REPORTER_OUTPUT=${PROJECT}_sponge_log.xml
     export MOCHA_REPORTER_SUITENAME=${PROJECT}
     export MOCHA_REPORTER=xunit
+else
+    export MOCHA_REPORTER=dot
 fi
 
 # Install dependencies
@@ -47,8 +49,8 @@ if command -v cygpath >/dev/null 2>&1; then
     PNPMFILE_PATH=$(cygpath -m "${PNPMFILE_PATH}")
 fi
 
-echo "pnpm install --ignore-scripts --engine-strict --prod --pnpmfile \"${PNPMFILE_PATH}\"; pnpm install --pnpmfile \"${PNPMFILE_PATH}\""
-pnpm install --ignore-scripts --engine-strict --prod --pnpmfile "${PNPMFILE_PATH}"; pnpm install --pnpmfile "${PNPMFILE_PATH}"
+echo "pnpm install --reporter=silent --ignore-scripts --engine-strict --prod --pnpmfile \"${PNPMFILE_PATH}\"; pnpm install --reporter=silent --pnpmfile \"${PNPMFILE_PATH}\""
+pnpm install --reporter=silent --ignore-scripts --engine-strict --prod --pnpmfile "${PNPMFILE_PATH}"; pnpm install --reporter=silent --pnpmfile "${PNPMFILE_PATH}"
 
 
 retval=0
