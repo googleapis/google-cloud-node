3ab78530788fb026a8da32cb7c31d57bf792cedb
Brent Shaffer <betterbrent@google.com>
2026-07-30 17:48:48 +0000

    fix(bigtable): fix testproxy invocation and remove redundant install in conformance scripts

--- handwritten/bigtable/.kokoro/conformance.sh
+++ handwritten/bigtable/.kokoro/conformance.sh
@@ -16,23 +16,21 @@
 
 #set -eo pipefail
 
-export NPM_CONFIG_PREFIX=${HOME}/.npm-global
-
-## cd to the parent directory, i.e. the root of the git repo
+## cd to the parent directory, i.e. handwritten/bigtable
 cd $(dirname $0)/..
 
 # Stop the testbench & cleanup environment variables
 function cleanup() {
     echo "Cleanup testbench"
     # Stop the proxy
-    kill $proxyPID
+    if [ -n "$proxyPID" ]; then
+        kill $proxyPID || true
+    fi
 }
 trap cleanup EXIT
 
-# Build and start the proxy in a separate process
+# Start the proxy in a separate process
 pushd .
-npm install -g pnpm@9
-pnpm install
 nohup pnpm run testproxy &
 proxyPID=$!
 popd
--- handwritten/bigtable/.kokoro/mandatory-conformance.sh
+++ handwritten/bigtable/.kokoro/mandatory-conformance.sh
@@ -16,26 +16,22 @@
 
 set -eo pipefail
 
-mkdir -p ${HOME}/.npm-global
-export NPM_CONFIG_PREFIX=${HOME}/.npm-global
-
-## cd to the parent directory, i.e. the root of the git repo
+## cd to the parent directory, i.e. handwritten/bigtable
 cd $(dirname $0)/..
 
 # Stop the testbench & cleanup environment variables
 function cleanup() {
     echo "Cleanup testbench"
     # Stop the proxy
-    kill $proxyPID
+    if [ -n "$proxyPID" ]; then
+        kill $proxyPID || true
+    fi
 }
 trap cleanup EXIT
 
-# Build and start the proxy in a separate process
+# Start the proxy in a separate process
 pushd .
-cd ../.. && corepack enable pnpm && pnpm install -g pnpm@9
-pnpm install --filter "...{./handwritten/bigtable}"
-cd handwritten/bigtable
-nohup ppnpm run testproxy &
+nohup pnpm run testproxy &
 proxyPID=$!
 popd
 
