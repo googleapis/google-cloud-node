b1f43e9fd72901795256cd4ffd7308472bb14ae5
Knut Olav Løite <koloite@gmail.com>
2026-09-14 10:16:30 +0200

    chore(spanner): clean up leader routing headers and fix commonHeaders_ mutation
    - Precomputes 'x-goog-spanner-route-to-leader: true' once in Transaction and
      PartitionedDml constructors when routeToLeaderEnabled is true.
    - Removes redundant per-request header mutations and option checks across
      Snapshot and Transaction methods (begin, read, runStream, batchUpdate, commit, rollback).
    - Uses safe navigation in Snapshot.prototype._getSpanner to support lightweight
      sessions without full parent chains.
    - Fixes latent in-place mutation in Database.prototype.batchCreateSessions where
      database.commonHeaders_ was mutated, which caused subsequent read-only snapshots
      to inadvertently route to the leader.
    - Passes a shallow copy of commonHeaders_ in Transaction.prototype.rollback to protect
      against downstream in-place header modifications.

--- handwritten/spanner/src/transaction.ts
+++ handwritten/spanner/src/transaction.ts
@@ -697,15 +697,6 @@ export class Snapshot extends EventEmitter {
       reqOpts.requestOptions = this.requestOptions;
     }
 
-    const headers = this.commonHeaders_;
-    if (
-      this._getSpanner().routeToLeaderEnabled &&
-      (this._options.readWrite !== undefined ||
-        this._options.partitionedDml !== undefined)
-    ) {
-      addLeaderAwareRoutingHeader(headers);
-    }
-
     return startTrace(
       'Snapshot.begin',
       {
@@ -721,7 +712,10 @@ export class Snapshot extends EventEmitter {
             method: 'beginTransaction',
             reqOpts,
             gaxOpts,
-            headers: injectRequestIDIntoHeaders(headers, this.session),
+            headers: injectRequestIDIntoHeaders(
+              this.commonHeaders_,
+              this.session,
+            ),
           },
           (
             err: null | grpc.ServiceError,
@@ -974,15 +968,6 @@ export class Snapshot extends EventEmitter {
       },
     );
 
-    const headers = this.commonHeaders_;
-    if (
-      this._getSpanner().routeToLeaderEnabled &&
-      (this._options.readWrite !== undefined ||
-        this._options.partitionedDml !== undefined)
-    ) {
-      addLeaderAwareRoutingHeader(headers);
-    }
-
     const traceConfig: traceConfig = {
       ...this._traceConfig,
       tableName: table,
@@ -1021,7 +1006,7 @@ export class Snapshot extends EventEmitter {
           reqOpts: Object.assign({}, reqOpts, {resumeToken}),
           gaxOpts: gaxOptions,
           headers: injectRequestIDIntoHeaders(
-            headers,
+            this.commonHeaders_,
             this.session,
             nthRequest,
             attempt,
@@ -1525,15 +1510,6 @@ export class Snapshot extends EventEmitter {
       });
     };
 
-    const headers = Object.assign({}, this.commonHeaders_);
-    if (
-      this._getSpanner().routeToLeaderEnabled &&
-      (this._options.readWrite !== undefined ||
-        this._options.partitionedDml !== undefined)
-    ) {
-      addLeaderAwareRoutingHeader(headers);
-    }
-
     const traceConfig = {
       transactionTag: this.requestOptions?.transactionTag,
       requestTag: requestOptions?.requestTag,
@@ -1623,7 +1599,7 @@ export class Snapshot extends EventEmitter {
         }
 
         const injectedHeaders = injectRequestIDIntoHeaders(
-          headers,
+          this.commonHeaders_,
           this.session,
           nthRequest,
           attempt,
@@ -1983,15 +1959,6 @@ export class Snapshot extends EventEmitter {
       });
     };
 
-    const headers = this.commonHeaders_;
-    if (
-      this._getSpanner().routeToLeaderEnabled &&
-      (this._options.readWrite !== undefined ||
-        this._options.partitionedDml !== undefined)
-    ) {
-      addLeaderAwareRoutingHeader(headers);
-    }
-
     const traceConfig: traceConfig = {
       transactionTag: this.requestOptions?.transactionTag,
       requestTag: requestOptions?.requestTag,
@@ -2036,7 +2003,7 @@ export class Snapshot extends EventEmitter {
           reqOpts: Object.assign({}, reqOpts, {resumeToken}),
           gaxOpts: gaxOptions,
           headers: injectRequestIDIntoHeaders(
-            headers,
+            this.commonHeaders_,
             this.session,
             nthRequest,
             attempt,
@@ -2349,7 +2316,7 @@ export class Snapshot extends EventEmitter {
    * @returns {Spanner}
    */
   protected _getSpanner(): Spanner {
-    return this.session.parent.parent.parent as Spanner;
+    return (this.session?.parent as Database)?.parent?.parent as Spanner;
   }
 }
 
@@ -2553,6 +2520,9 @@ export class Transaction extends Dml {
     this._options.isolationLevel = IsolationLevel.ISOLATION_LEVEL_UNSPECIFIED;
     this.requestOptions = requestOptions;
     this._retryCommit = false;
+    if (this._getSpanner()?.routeToLeaderEnabled) {
+      addLeaderAwareRoutingHeader(this.commonHeaders_);
+    }
   }
 
   /**
@@ -2707,9 +2677,6 @@ export class Transaction extends Dml {
       nextNthRequest(database),
       1,
     );
-    if (this._getSpanner().routeToLeaderEnabled) {
-      addLeaderAwareRoutingHeader(headers);
-    }
 
     const traceConfig: traceConfig = {
       ...this._traceConfig,
@@ -2946,11 +2913,6 @@ export class Transaction extends Dml {
           this.requestOptions,
         );
 
-        const headers = this.commonHeaders_;
-        if (this._getSpanner().routeToLeaderEnabled) {
-          addLeaderAwareRoutingHeader(headers);
-        }
-
         span.addEvent('Starting Commit');
 
         const database = this.session.parent as Database;
@@ -2969,7 +2931,7 @@ export class Transaction extends Dml {
             reqOpts,
             gaxOpts,
             headers: injectRequestIDIntoHeaders(
-              headers,
+              this.commonHeaders_,
               this.session,
               nextNthRequest(database),
               1,
@@ -3341,11 +3303,6 @@ export class Transaction extends Dml {
         transactionId,
       };
 
-      const headers = this.commonHeaders_;
-      if (this._getSpanner().routeToLeaderEnabled) {
-        addLeaderAwareRoutingHeader(headers);
-      }
-
       if (this._affinityKey) {
         if (!gaxOpts || Object.keys(gaxOpts).length === 0) {
           gaxOpts = this._unbindGaxOpts as any;
@@ -3360,7 +3317,7 @@ export class Transaction extends Dml {
           method: 'rollback',
           reqOpts,
           gaxOpts,
-          headers: headers,
+          headers: {...this.commonHeaders_},
         },
         (err: null | ServiceError) => {
           if (err) {
@@ -3894,6 +3851,9 @@ export class PartitionedDml extends Dml {
   ) {
     super(session);
     this._options = {partitionedDml: options};
+    if (this._getSpanner()?.routeToLeaderEnabled) {
+      addLeaderAwareRoutingHeader(this.commonHeaders_);
+    }
   }
   /**
    * Use option excludeTxnFromChangeStreams to exclude partitionedDml
