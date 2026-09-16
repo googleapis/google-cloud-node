14bb06ff41a9154f657f61740eb16456189adc7e
feywind <57276408+feywind@users.noreply.github.com>
2026-09-16 21:34:38 +0000

    fix: fix several docs generation issues

--- core/dev-packages/jsdoc-region-tag/src/index.js
+++ core/dev-packages/jsdoc-region-tag/src/index.js
@@ -25,9 +25,10 @@ const REGION_END_REGEX = /\[END/;
 
 const sampleCache = new Map();
 exports.loadSampleCache = function () {
-  const sampleCandidates = glob.sync(`${SAMPLES_DIRECTORY}/**/*.{js,ts}`, {
-    ignore: ['node_modules'],
-  });
+  const sampleCandidates = [
+    ...glob.sync(`${SAMPLES_DIRECTORY}/**/*.js`, {ignore: ['node_modules']}),
+    ...glob.sync(`${SAMPLES_DIRECTORY}/**/*.ts`, {ignore: ['node_modules']}),
+  ];
   for (const candidate of sampleCandidates) {
     const stat = statSync(candidate);
     if (!stat.isFile()) continue;
