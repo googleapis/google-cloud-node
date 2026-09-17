dee73022fd6140b2be6cd4567d59b15510b86cc9
Shivanee Persaud <shivaneep@google.com>
2026-09-17 16:19:29 -0700

    chore: fix prettier formatting in gapic-generator-typescript

--- core/generator/gapic-generator-typescript/typescript/src/gapic-generator-typescript.ts
+++ core/generator/gapic-generator-typescript/typescript/src/gapic-generator-typescript.ts
@@ -165,8 +165,7 @@ async function main(processArgv: string[]) {
   const restNumericEnums = argv.restNumericEnums as boolean | undefined;
   const mixins = argv.mixins as string | undefined;
   const enableTelemetryTracing = argv.enableTelemetryTracing as
-    | boolean
-    | undefined;
+    boolean | undefined;
 
   // --protoc can be taken from environment or from the command line
   let protocParameter = argv.protoc as string | string[] | undefined;
