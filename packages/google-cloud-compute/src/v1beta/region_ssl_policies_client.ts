1b9aa430700d934f23f8d8104cae74ba98d5baee
Min Zhu <zhumin@google.com>
2026-09-17 14:59:59 -0400

    feat: update API sources and regenerate

--- packages/google-cloud-compute/src/v1beta/region_ssl_policies_client.ts
+++ packages/google-cloud-compute/src/v1beta/region_ssl_policies_client.ts
@@ -261,10 +261,12 @@ export class RegionSslPoliciesClient {
     const regionSslPoliciesStubMethods = [
       'delete',
       'get',
+      'getIamPolicy',
       'insert',
       'list',
       'listAvailableFeatures',
       'patch',
+      'setIamPolicy',
       'testIamPermissions',
     ];
     for (const methodName of regionSslPoliciesStubMethods) {
@@ -678,6 +680,158 @@ export class RegionSslPoliciesClient {
           {} | undefined,
         ]) => {
           this._log.info('get response %j', response);
+          return [response, options, rawResponse];
+        },
+      )
+      .catch((error: any) => {
+        if (
+          error &&
+          'statusDetails' in error &&
+          error.statusDetails instanceof Array
+        ) {
+          const protos = this._gaxModule.protobuf.Root.fromJSON(
+            jsonProtos,
+          ) as unknown as gax.protobuf.Type;
+          error.statusDetails = decodeAnyProtosInArray(
+            error.statusDetails,
+            protos,
+          );
+        }
+        throw error;
+      });
+  }
+  /**
+   * Gets the access control policy for a resource. May be empty if no such
+   * policy or resource exists.
+   *
+   * @param {Object} request
+   *   The request object that will be sent.
+   * @param {number} request.optionsRequestedPolicyVersion
+   *   Requested IAM Policy version.
+   * @param {string} request.project
+   *   Project ID for this request.
+   * @param {string} request.region
+   *   The name of the region for this request.
+   * @param {string} request.resource
+   *   Name or id of the resource for this request.
+   * @param {object} [options]
+   *   Call options. See {@link https://googleapis.dev/nodejs/google-gax/latest/interfaces/CallOptions.html|CallOptions} for more details.
+   * @returns {Promise} - The promise which resolves to an array.
+   *   The first element of the array is an object representing {@link protos.google.cloud.compute.v1beta.Policy|Policy}.
+   *   Please see the {@link https://github.com/googleapis/gax-nodejs/blob/master/client-libraries.md#regular-methods | documentation }
+   *   for more details and examples.
+   * @example <caption>include:samples/generated/v1beta/region_ssl_policies.get_iam_policy.js</caption>
+   * region_tag:compute_v1beta_generated_RegionSslPolicies_GetIamPolicy_async
+   */
+  getIamPolicy(
+    request?: protos.google.cloud.compute.v1beta.IGetIamPolicyRegionSslPolicyRequest,
+    options?: CallOptions,
+  ): Promise<
+    [
+      protos.google.cloud.compute.v1beta.IPolicy,
+      (
+        | protos.google.cloud.compute.v1beta.IGetIamPolicyRegionSslPolicyRequest
+        | undefined
+      ),
+      {} | undefined,
+    ]
+  >;
+  getIamPolicy(
+    request: protos.google.cloud.compute.v1beta.IGetIamPolicyRegionSslPolicyRequest,
+    options: CallOptions,
+    callback: Callback<
+      protos.google.cloud.compute.v1beta.IPolicy,
+      | protos.google.cloud.compute.v1beta.IGetIamPolicyRegionSslPolicyRequest
+      | null
+      | undefined,
+      {} | null | undefined
+    >,
+  ): void;
+  getIamPolicy(
+    request: protos.google.cloud.compute.v1beta.IGetIamPolicyRegionSslPolicyRequest,
+    callback: Callback<
+      protos.google.cloud.compute.v1beta.IPolicy,
+      | protos.google.cloud.compute.v1beta.IGetIamPolicyRegionSslPolicyRequest
+      | null
+      | undefined,
+      {} | null | undefined
+    >,
+  ): void;
+  getIamPolicy(
+    request?: protos.google.cloud.compute.v1beta.IGetIamPolicyRegionSslPolicyRequest,
+    optionsOrCallback?:
+      | CallOptions
+      | Callback<
+          protos.google.cloud.compute.v1beta.IPolicy,
+          | protos.google.cloud.compute.v1beta.IGetIamPolicyRegionSslPolicyRequest
+          | null
+          | undefined,
+          {} | null | undefined
+        >,
+    callback?: Callback<
+      protos.google.cloud.compute.v1beta.IPolicy,
+      | protos.google.cloud.compute.v1beta.IGetIamPolicyRegionSslPolicyRequest
+      | null
+      | undefined,
+      {} | null | undefined
+    >,
+  ): Promise<
+    [
+      protos.google.cloud.compute.v1beta.IPolicy,
+      (
+        | protos.google.cloud.compute.v1beta.IGetIamPolicyRegionSslPolicyRequest
+        | undefined
+      ),
+      {} | undefined,
+    ]
+  > | void {
+    request = request || {};
+    let options: CallOptions;
+    if (typeof optionsOrCallback === 'function' && callback === undefined) {
+      callback = optionsOrCallback;
+      options = {};
+    } else {
+      options = optionsOrCallback as CallOptions;
+    }
+    options = options || {};
+    options.otherArgs = options.otherArgs || {};
+    options.otherArgs.headers = options.otherArgs.headers || {};
+    options.otherArgs.headers['x-goog-request-params'] =
+      this._gaxModule.routingHeader.fromParams({
+        project: request.project ?? '',
+        region: request.region ?? '',
+        resource: request.resource ?? '',
+      });
+    this.initialize().catch(err => {
+      throw err;
+    });
+    this._log.info('getIamPolicy request %j', request);
+    const wrappedCallback:
+      | Callback<
+          protos.google.cloud.compute.v1beta.IPolicy,
+          | protos.google.cloud.compute.v1beta.IGetIamPolicyRegionSslPolicyRequest
+          | null
+          | undefined,
+          {} | null | undefined
+        >
+      | undefined = callback
+      ? (error, response, options, rawResponse) => {
+          this._log.info('getIamPolicy response %j', response);
+          callback!(error, response, options, rawResponse); // We verified callback above.
+        }
+      : undefined;
+    return this.innerApiCalls
+      .getIamPolicy(request, options, wrappedCallback)
+      ?.then(
+        ([response, options, rawResponse]: [
+          protos.google.cloud.compute.v1beta.IPolicy,
+          (
+            | protos.google.cloud.compute.v1beta.IGetIamPolicyRegionSslPolicyRequest
+            | undefined
+          ),
+          {} | undefined,
+        ]) => {
+          this._log.info('getIamPolicy response %j', response);
           return [response, options, rawResponse];
         },
       )
@@ -1253,6 +1407,158 @@ export class RegionSslPoliciesClient {
             operation,
             rawResponse,
           ];
+        },
+      )
+      .catch((error: any) => {
+        if (
+          error &&
+          'statusDetails' in error &&
+          error.statusDetails instanceof Array
+        ) {
+          const protos = this._gaxModule.protobuf.Root.fromJSON(
+            jsonProtos,
+          ) as unknown as gax.protobuf.Type;
+          error.statusDetails = decodeAnyProtosInArray(
+            error.statusDetails,
+            protos,
+          );
+        }
+        throw error;
+      });
+  }
+  /**
+   * Sets the access control policy on the specified resource.
+   * Replaces any existing policy.
+   *
+   * @param {Object} request
+   *   The request object that will be sent.
+   * @param {string} request.project
+   *   Project ID for this request.
+   * @param {string} request.region
+   *   The name of the region for this request.
+   * @param {google.cloud.compute.v1beta.RegionSetPolicyRequest} request.regionSetPolicyRequestResource
+   *   The body resource for this request
+   * @param {string} request.resource
+   *   Name or id of the resource for this request.
+   * @param {object} [options]
+   *   Call options. See {@link https://googleapis.dev/nodejs/google-gax/latest/interfaces/CallOptions.html|CallOptions} for more details.
+   * @returns {Promise} - The promise which resolves to an array.
+   *   The first element of the array is an object representing {@link protos.google.cloud.compute.v1beta.Policy|Policy}.
+   *   Please see the {@link https://github.com/googleapis/gax-nodejs/blob/master/client-libraries.md#regular-methods | documentation }
+   *   for more details and examples.
+   * @example <caption>include:samples/generated/v1beta/region_ssl_policies.set_iam_policy.js</caption>
+   * region_tag:compute_v1beta_generated_RegionSslPolicies_SetIamPolicy_async
+   */
+  setIamPolicy(
+    request?: protos.google.cloud.compute.v1beta.ISetIamPolicyRegionSslPolicyRequest,
+    options?: CallOptions,
+  ): Promise<
+    [
+      protos.google.cloud.compute.v1beta.IPolicy,
+      (
+        | protos.google.cloud.compute.v1beta.ISetIamPolicyRegionSslPolicyRequest
+        | undefined
+      ),
+      {} | undefined,
+    ]
+  >;
+  setIamPolicy(
+    request: protos.google.cloud.compute.v1beta.ISetIamPolicyRegionSslPolicyRequest,
+    options: CallOptions,
+    callback: Callback<
+      protos.google.cloud.compute.v1beta.IPolicy,
+      | protos.google.cloud.compute.v1beta.ISetIamPolicyRegionSslPolicyRequest
+      | null
+      | undefined,
+      {} | null | undefined
+    >,
+  ): void;
+  setIamPolicy(
+    request: protos.google.cloud.compute.v1beta.ISetIamPolicyRegionSslPolicyRequest,
+    callback: Callback<
+      protos.google.cloud.compute.v1beta.IPolicy,
+      | protos.google.cloud.compute.v1beta.ISetIamPolicyRegionSslPolicyRequest
+      | null
+      | undefined,
+      {} | null | undefined
+    >,
+  ): void;
+  setIamPolicy(
+    request?: protos.google.cloud.compute.v1beta.ISetIamPolicyRegionSslPolicyRequest,
+    optionsOrCallback?:
+      | CallOptions
+      | Callback<
+          protos.google.cloud.compute.v1beta.IPolicy,
+          | protos.google.cloud.compute.v1beta.ISetIamPolicyRegionSslPolicyRequest
+          | null
+          | undefined,
+          {} | null | undefined
+        >,
+    callback?: Callback<
+      protos.google.cloud.compute.v1beta.IPolicy,
+      | protos.google.cloud.compute.v1beta.ISetIamPolicyRegionSslPolicyRequest
+      | null
+      | undefined,
+      {} | null | undefined
+    >,
+  ): Promise<
+    [
+      protos.google.cloud.compute.v1beta.IPolicy,
+      (
+        | protos.google.cloud.compute.v1beta.ISetIamPolicyRegionSslPolicyRequest
+        | undefined
+      ),
+      {} | undefined,
+    ]
+  > | void {
+    request = request || {};
+    let options: CallOptions;
+    if (typeof optionsOrCallback === 'function' && callback === undefined) {
+      callback = optionsOrCallback;
+      options = {};
+    } else {
+      options = optionsOrCallback as CallOptions;
+    }
+    options = options || {};
+    options.otherArgs = options.otherArgs || {};
+    options.otherArgs.headers = options.otherArgs.headers || {};
+    options.otherArgs.headers['x-goog-request-params'] =
+      this._gaxModule.routingHeader.fromParams({
+        project: request.project ?? '',
+        region: request.region ?? '',
+        resource: request.resource ?? '',
+      });
+    this.initialize().catch(err => {
+      throw err;
+    });
+    this._log.info('setIamPolicy request %j', request);
+    const wrappedCallback:
+      | Callback<
+          protos.google.cloud.compute.v1beta.IPolicy,
+          | protos.google.cloud.compute.v1beta.ISetIamPolicyRegionSslPolicyRequest
+          | null
+          | undefined,
+          {} | null | undefined
+        >
+      | undefined = callback
+      ? (error, response, options, rawResponse) => {
+          this._log.info('setIamPolicy response %j', response);
+          callback!(error, response, options, rawResponse); // We verified callback above.
+        }
+      : undefined;
+    return this.innerApiCalls
+      .setIamPolicy(request, options, wrappedCallback)
+      ?.then(
+        ([response, options, rawResponse]: [
+          protos.google.cloud.compute.v1beta.IPolicy,
+          (
+            | protos.google.cloud.compute.v1beta.ISetIamPolicyRegionSslPolicyRequest
+            | undefined
+          ),
+          {} | undefined,
+        ]) => {
+          this._log.info('setIamPolicy response %j', response);
+          return [response, options, rawResponse];
         },
       )
       .catch((error: any) => {
