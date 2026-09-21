5909839afb610e2c17e52bc45cb2eddb25d3d4e3
Shivanee Persaud <shivaneep@google.com>
2026-09-21 16:26:41 -0700

    chore(gax): fix linter and prettier formatting in tracerHelper test

--- core/packages/gax/test/unit/tracerHelper.ts
+++ core/packages/gax/test/unit/tracerHelper.ts
@@ -936,19 +936,13 @@ describe('TracerHelper', () => {
         }
         cancel(): void {}
         then<TResult1 = string, TResult2 = never>(
-          onfulfilled?:
-            | ((value: string) => TResult1 | PromiseLike<TResult1>)
-            | null,
-          onrejected?:
-            | ((reason: unknown) => TResult2 | PromiseLike<TResult2>)
-            | null,
+          onfulfilled?: (value: string) => TResult1 | PromiseLike<TResult1>,
+          onrejected?: (reason: unknown) => TResult2 | PromiseLike<TResult2>,
         ): Promise<TResult1 | TResult2> {
           return this.promise.then(onfulfilled, onrejected);
         }
         catch<TResult = never>(
-          onrejected?:
-            | ((reason: unknown) => TResult | PromiseLike<TResult>)
-            | null,
+          onrejected?: (reason: unknown) => TResult | PromiseLike<TResult>,
         ): Promise<string | TResult> {
           return this.promise.catch(onrejected);
         }
