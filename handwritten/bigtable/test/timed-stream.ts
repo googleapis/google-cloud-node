3943390550725fd1fc34dfb517217f30725b4605
Daniel Bruce <djbruce@google.com>
2026-09-22 16:54:37 -0400

    Run bigtable with the new test runner

--- handwritten/bigtable/test/timed-stream.ts
+++ handwritten/bigtable/test/timed-stream.ts
@@ -34,7 +34,7 @@ class UserStream extends TimedStream {
   }
 }
 
-describe('Bigtable/TimedStream', () => {
+describe.only('Bigtable/TimedStream', () => {
   describe('with handlers', () => {
     describe('with no delay from server', () => {
       it('should measure the total time accurately for a series of 30 rows with a synchronous call', function (done) {
