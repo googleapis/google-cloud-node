b6e6db11ab9fd0b53c11cd82d5a4ff6bf45a9770
Daniel Bruce <djbruce@google.com>
2026-09-24 11:40:00 -0400

    Resolve compiler errors with datastore test

--- handwritten/datastore/src/request.ts
+++ handwritten/datastore/src/request.ts
@@ -61,7 +61,7 @@ import {
   RunQueryResponse,
   RunQueryCallback,
 } from './query';
-import {Datastore, Transaction} from '.';
+import type {Datastore, Transaction} from '.';
 import ITimestamp = google.protobuf.ITimestamp;
 import {AggregateQuery} from './aggregate';
 import {RunOptions} from './transaction';
@@ -1362,7 +1362,10 @@ class DatastoreRequest {
  *
  */
 function isTransaction(request: DatastoreRequest): request is Transaction {
-  return request instanceof Transaction;
+  return (
+    (request as any).state !== TransactionState.NOT_TRANSACTION ||
+    request.constructor?.name === 'Transaction'
+  );
 }
 
 /**
